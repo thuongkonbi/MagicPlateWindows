b31412d3fc414d605d7d6652374605bce2edc428
Dzung Cao <dzung@konbinisg.com>
2018-03-28 16:23:07 +0700

    test 1

--- BackEnd/Api/Models/ProductSnapshot.cs
+++ BackEnd/Api/Models/ProductSnapshot.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:435c63ab3e69fd1b0300c9fd48446e0f968a180a794066bd5c7b724456b3550f
-size 372
+oid sha256:66ac422c9b8903c318ffc2e7f799d53ab55085198f51073c787f49b0bc2f0422
+size 363
