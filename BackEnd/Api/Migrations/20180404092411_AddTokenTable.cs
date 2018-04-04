a6a98f77aff9352c6b858da7b98ceb106a18897e
Dzung Cao <dzung@konbinisg.com>
2018-04-04 17:55:21 +0700

    update_api_table

--- BackEnd/Api/Migrations/20180404092411_AddTokenTable.cs
+++ BackEnd/Api/Migrations/20180404092411_AddTokenTable.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ba6e1e0a4f1dbac1a9d650e03be3b4e5db0ce36d5127b838cdfb945044f729a
-size 1359
+oid sha256:82fb8da382162dbc4a4d739d562fe16d244d97d8ea64aa61fa1b7f271a4f796b
+size 1418
