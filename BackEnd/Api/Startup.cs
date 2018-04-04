344babcfa077d36cdb4c867badb45460c4534107
Dzung Cao <dzung@konbinisg.com>
2018-04-04 11:08:32 +0700

    update cors

--- BackEnd/Api/Startup.cs
+++ BackEnd/Api/Startup.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fbb6a0561df5883cf8721a11af2c1cf5dc280b8df14be4895e748c5f32098f60
-size 2185
+oid sha256:9a70d64dd070248d5da62f3b5aab093a3196238efa56a48e6da59c9fd4a89bad
+size 2195
