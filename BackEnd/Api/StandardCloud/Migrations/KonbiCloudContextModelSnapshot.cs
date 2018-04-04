dbed99592b19b96ce768d3d59996e3003b736120
Dzung Cao <dzung@konbinisg.com>
2018-04-04 17:37:57 +0700

    update

--- BackEnd/Api/StandardCloud/Migrations/KonbiCloudContextModelSnapshot.cs
+++ BackEnd/Api/StandardCloud/Migrations/KonbiCloudContextModelSnapshot.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6d81624766f520c777dec566b0b75213fb8234805a4d0ac7b45c32c32585de6
-size 10881
+oid sha256:fd46cdb0eb663d57bf4c38a706e007d67731837aaf557a521d63e3360b12c8e4
+size 10883
