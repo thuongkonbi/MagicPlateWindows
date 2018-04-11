76407df3b86a0014aab7553f7ff745e2be159f84
Dzung Cao <dzung@konbinisg.com>
2018-04-11 16:18:19 +0700

    table name is case sensitive in ubuntu

--- BackEnd/Api/Migrations/20170816151242_InitialCreate.cs
+++ BackEnd/Api/Migrations/20170816151242_InitialCreate.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dbf23d5c01c3fcd75e3316a99d6849c0ed5e560eb219b8cdc889db8604305ba3
+oid sha256:bc2a5c07219f0fd1624fa4141895680db27ff46430f0e605da6f31ebbcab59bd
 size 16692
