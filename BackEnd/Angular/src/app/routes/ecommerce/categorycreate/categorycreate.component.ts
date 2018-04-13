82ada60f4d582a73cfa408b85e151a4808a07258
Dzung Cao <dzung@konbinisg.com>
2018-04-13 14:14:52 +0700

    fix angular typos

--- BackEnd/Angular/src/app/routes/ecommerce/categorycreate/categorycreate.component.ts
+++ BackEnd/Angular/src/app/routes/ecommerce/categorycreate/categorycreate.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3821fcd0748b2367e4a924e59111d46ccb7b44daed3a9c8bfac5843a5dfa90e
-size 1741
+oid sha256:1bf98b043ace700572459bf1b5c1d208f16ff24f45c2578116d4d23fb76923ae
+size 2014
