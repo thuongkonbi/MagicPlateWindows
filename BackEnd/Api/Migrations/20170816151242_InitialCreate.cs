199c470d7d14044901f07e862cd3cf9ea005146e
Dzung Cao <dzung@konbinisg.com>
2018-04-13 17:24:05 +0700

    update

--- BackEnd/Api/Migrations/20170816151242_InitialCreate.cs
+++ BackEnd/Api/Migrations/20170816151242_InitialCreate.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7eaa141a27a5102b6137495f17db0c9e3fd37a6532640f18ceb111e244ba762
-size 18101
+oid sha256:075d7130a5e04763aee850dc2d8b92520888f8ff2553f26f7ed1d71d48dc483d
+size 18099
