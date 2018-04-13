fd88791e675d6783c2fb7f9819ed04f89339bec3
Dzung Cao <dzung@konbinisg.com>
2018-04-13 15:25:30 +0700

    fix logic on while command

--- BackEnd/Api/Controllers/SyncController.cs
+++ BackEnd/Api/Controllers/SyncController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3bc90e4469dce79492ef664b8048ed645a30c694974edd0b87ce1b6c3e481e0c
-size 5920
+oid sha256:7ca01673cbec606a700c99bc35aeb4bd53c7b95647088870608bfbac6371eb3b
+size 5926
