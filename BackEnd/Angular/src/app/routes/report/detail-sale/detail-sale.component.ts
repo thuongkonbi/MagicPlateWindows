644abd20bb7549d35d5e10e0ea264843cb2fe3dc
Dzung Cao <dzung@konbinisg.com>
2018-04-26 15:28:33 +0700

    fix production buid for angular app

--- BackEnd/Angular/src/app/routes/report/detail-sale/detail-sale.component.ts
+++ BackEnd/Angular/src/app/routes/report/detail-sale/detail-sale.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58c938bdc0de8991d09d8fc58f55dc6bced22391e283f3589b0fda37481c949b
-size 978
+oid sha256:200e39217ad52ed6622f959327f592b1b381252d60b01a1f0a70ad50d6f4c25e
+size 1052
