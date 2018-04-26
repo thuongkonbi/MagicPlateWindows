a98f64ca67ffc3246c0114320751a0df965ce9d5
Dzung Cao <dzung@konbinisg.com>
2018-04-26 18:24:23 +0700

    update split logic for linux path

--- BackEnd/Api/Data/DbInitializer.cs
+++ BackEnd/Api/Data/DbInitializer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6bac095be35a0baf406c94464d074f63030b193aa70ffd19cc0e3e15846f5ebb
-size 4646
+oid sha256:b33af44fde96324d5f1e1ac97b84bfd18a0fd5260e41eaee98d55af01e847dcc
+size 4705
