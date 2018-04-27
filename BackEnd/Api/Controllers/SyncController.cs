b8601465979f8dfb7df4656e058a3540ce24720d
Dzung Cao <dzung@konbinisg.com>
2018-04-27 17:46:22 +0700

    add error log

--- BackEnd/Api/Controllers/SyncController.cs
+++ BackEnd/Api/Controllers/SyncController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0532b1176046d0aab70b54a07ef48538a92c6bbd6886fad275ff1678aa7bc19
-size 6720
+oid sha256:169c7e35144046909ef6b03b0dc6db280f9ffe727f6c07fcf0bad4e0e90d19b7
+size 6754
