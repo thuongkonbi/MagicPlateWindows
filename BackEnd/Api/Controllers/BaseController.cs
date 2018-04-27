cf514a028303688e4fc142e1ed064599f9a57a65
Dzung Cao <dzung@konbinisg.com>
2018-04-27 17:40:46 +0700

    add nlog

--- BackEnd/Api/Controllers/BaseController.cs
+++ BackEnd/Api/Controllers/BaseController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff5acdef8c84a4f0b47e5896ebb75d8236a9d9047e2f3ce59f0e3a0290f8ad0c
-size 1245
+oid sha256:7ed03326dc6fd4455fff0b730b56465ee2df7a9950a1d0c60b99d39b4a54d13a
+size 1383
--- BackEnd/Api/Controllers/SyncController.cs
+++ BackEnd/Api/Controllers/SyncController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7fecf265005d2fe17a388ad2a1bf460e1bba9d4c46c93d837fb8dc33c4ebfcae
-size 6685
+oid sha256:b0532b1176046d0aab70b54a07ef48538a92c6bbd6886fad275ff1678aa7bc19
+size 6720
