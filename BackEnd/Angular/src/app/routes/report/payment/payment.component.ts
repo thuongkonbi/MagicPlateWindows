f063d43c1194c9be69905536f4b0b3cd27aa9a36
Dzung Cao <dzung@konbinisg.com>
2018-04-27 15:12:22 +0700

    re-commit

--- BackEnd/Angular/src/app/routes/report/payment/payment.component.ts
+++ BackEnd/Angular/src/app/routes/report/payment/payment.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42293310b4c66078a0190ab450c08df89324e4e212c2f644005a59d71d7011b4
-size 843
+oid sha256:4d4ccc42f02aa31fb9bfbac08f6eb9aaa433d451153f85c32665e10e8629baa9
+size 853
