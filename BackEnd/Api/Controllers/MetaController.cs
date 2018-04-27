20f475a942faeb94ffd891ba1f125ebfc2b9beae
Dzung Cao <dzung@konbinisg.com>
2018-04-27 16:02:28 +0700

    update mapping file location

--- BackEnd/Api/Controllers/MetaController.cs
+++ BackEnd/Api/Controllers/MetaController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0816468049222dca97895df766ec091dc6830ee881db2e7060f33ad4020c3da6
-size 1014
+oid sha256:a31ceca592a6b9fe896fab5f94daf9fbb8241420c7aafcc2f03b0c89c02e7cad
+size 995
