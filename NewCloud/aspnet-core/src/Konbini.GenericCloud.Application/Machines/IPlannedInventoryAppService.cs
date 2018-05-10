d3765f1243bc8573b26167afbd1012a052b4137a
ha <ha@konbinisg.com>
2018-05-10 17:28:04 +0800

    machine create ok

---
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Machines/IPlannedInventoryAppService.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:51d451031f13f5c5e291aa0caf1ff1a7b8a95ed7433c6a93a338a39e7dbb2b49
+size 377
--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Core/Machines/PlannedSessionInventory.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Core/Machines/PlannedSessionInventory.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac71f5d6bc575c1c8424a14af2f510c1b27e66350db99e0a87578437c60a120c
-size 581
+oid sha256:137f2f4237bb14fd27ec76f4af2d6f2ea13a1cfe922dc669c54aaacf16fb328a
+size 537
