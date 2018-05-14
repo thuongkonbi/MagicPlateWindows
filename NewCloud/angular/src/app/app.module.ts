add146556a1507b945d58b4e0fab1054c5af642b
ha <ha@konbinisg.com>
2018-05-14 19:05:07 +0800

    temporary commit

--- NewCloud/angular/src/app/app.module.ts
+++ NewCloud/angular/src/app/app.module.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fcae935d64d49caf516fc1b603c7d435bab945f0ec073672760e974777a22e3
-size 4122
+oid sha256:1bb00d6e969cf3c6fc9cc649385a5020b3b122e3b8eca10349210ca6e6741bf7
+size 4128
