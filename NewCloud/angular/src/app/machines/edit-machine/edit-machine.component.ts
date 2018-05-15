c966bb4f44ce5a76084f6ec76a772da965030605
Le Duy Nam <namld@groupcar.com.vn>
2018-05-15 11:19:04 +0700

    machines fix

--- NewCloud/angular/src/app/machines/edit-machine/edit-machine.component.ts
+++ NewCloud/angular/src/app/machines/edit-machine/edit-machine.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31105aae970acc96f0bb2c8cdbaef247616cb0afc522098b7dc21e6dd9f77390
-size 2506
+oid sha256:ac09fa44b476bd2e7f45ebf82ee2f2ca721e676901fdb80ba3702e2a9ff5d98a
+size 2507
