df1c2b7e694691406206dc9e2fbb8c9759e5c2fd
ha <ha@konbinisg.com>
2018-05-16 16:41:18 +0800

    ok

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Products/ProductAppService.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Products/ProductAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:acc313b749db6ebae038c1df3210a878e9b51d61098cf628c056a3a3d61370f0
-size 3516
+oid sha256:c610f2949b7d7f1e7556f31f4caa59470b177b83dfcef7ed09a1fdab540f22a6
+size 3935
