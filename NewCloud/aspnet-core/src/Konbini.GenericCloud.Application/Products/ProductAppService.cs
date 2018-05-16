22d7ad863605f46290914bd0f47c50d8719145f0
phuong <phuong@konbi.cloud>
2018-05-16 15:32:57 +0700

    filter product list with category

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Products/ProductAppService.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Products/ProductAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:acc313b749db6ebae038c1df3210a878e9b51d61098cf628c056a3a3d61370f0
-size 3516
+oid sha256:caef92ad60646ea30a9be1e84612b93c486077b00b412f90a9d4e1c3d8d01a6e
+size 3438
