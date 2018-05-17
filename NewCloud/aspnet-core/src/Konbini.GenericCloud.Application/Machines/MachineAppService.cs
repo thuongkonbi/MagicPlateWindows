3bcd1f04ed8e43d78e5fedf5e1aec61759587257
Le Duy Nam <namld@groupcar.com.vn>
2018-05-17 15:22:19 +0700

    Services update

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Machines/MachineAppService.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Machines/MachineAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e44efac66dd3e970587c2514295f20e11dcf2eade807b1c1bdb650896f9df43
-size 8079
+oid sha256:378d2cc65d0768fd4656fdc5ed94c09a94edd39cbdd3a57d62e4bbeaeed50691
+size 8506
