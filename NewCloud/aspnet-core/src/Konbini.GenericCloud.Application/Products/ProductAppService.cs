cd774ccbf8a37651b13514ce515b3ea1e1d28d4a
phuong <phuong@konbi.cloud>
2018-05-17 21:47:15 +0700

    merge update product

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Products/ProductAppService.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Products/ProductAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1367221fe13fe265c484c641df2ec64ebdb1b34104b8ab6cbe770e068292b0ea
-size 5742
+oid sha256:2f2a21ed2c2fb24d32fee0aff85d2753cb78919ad731ffd5433acd55927ff2ac
+size 5739
