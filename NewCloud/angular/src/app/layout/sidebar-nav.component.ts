65852ecf18b61976bbab33a2a4dbfc4acb748ad7
Le Duy Nam <namld@groupcar.com.vn>
2018-05-25 14:05:01 +0700

    menu update

--- NewCloud/angular/src/app/layout/sidebar-nav.component.ts
+++ NewCloud/angular/src/app/layout/sidebar-nav.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2117ae29193d3b438f68cf39a1cdd0e74cc7b2310d95d10ce74411d1136a8d6a
-size 4323
+oid sha256:f7aa37fe38d952193b226a9ab4b57583ed2a1faed6d5afb1a0826234ffd29831
+size 1992
