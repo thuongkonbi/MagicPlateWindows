921db7da8e37909131780351440aa7f27d556627
Le Duy Nam <namld@groupcar.com.vn>
2018-05-28 17:42:09 +0700

    fix transaction tenantID sync

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Machines/TransactionAppService.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Machines/TransactionAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa4511a0acd4c41cc63799f71d6bb011a9cc235251a400dbb6b3222e505c1cf0
-size 1929
+oid sha256:cc4b4c1e17c98109d0c7abdbe8390c1d8d19e54d3abb23226d0dfc4934a7246e
+size 1971
