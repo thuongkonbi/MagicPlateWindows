82f388494c570db131bb808730114313d21fc353
Le Duy Nam <namld@groupcar.com.vn>
2018-05-29 18:04:34 +0700

    backend add Machine Loadout for dashboard

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Dashboard/DashboardAppService.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Dashboard/DashboardAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a02bed4b35ffe05996293854b4f53f9ab1d9c4bd258f6300ac72b2ed4ac636ed
-size 6995
+oid sha256:4e2f0e8619fccb40d4790316d3330cb484a7f6532c2585dea72538ac95b9e500
+size 7075
--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Dashboard/Dtos/DashboardDto.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Dashboard/Dtos/DashboardDto.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83ab13d15f97a5cc67c20ddbbcc47ae108c4023ae95607bd2ce6bc6a518d358e
-size 1332
+oid sha256:c5b7b6bfbff541893ddaeaa18e04d393f9a46a7a349fd3ffcfcf89c1e45f1ddd
+size 1408
