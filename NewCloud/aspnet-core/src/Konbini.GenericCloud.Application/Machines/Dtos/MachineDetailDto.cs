671c9e0c95584a1736f2e2855b559f5ffb5d2e8d
Le Duy Nam <namld@groupcar.com.vn>
2018-05-30 19:29:15 +0700

    backend process machine loadout status

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Machines/Dtos/MachineDetailDto.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Machines/Dtos/MachineDetailDto.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d777d6df3dc8eb092431a282a6e6872c76d1f967e9e57e88a8797728986894c3
-size 500
+oid sha256:e68cfc29764991055656070e0a76316f9c306bceec85f2df0acdae98ed5e0a8e
+size 1205
