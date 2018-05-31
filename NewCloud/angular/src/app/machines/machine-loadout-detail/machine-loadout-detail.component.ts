a74f115836eb2dbe11b76cc31490141cdb2e60b1
Le Duy Nam <namld@groupcar.com.vn>
2018-05-31 17:37:36 +0700

    process loadout detail + minor fix

--- NewCloud/angular/src/app/machines/machine-loadout-detail/machine-loadout-detail.component.ts
+++ NewCloud/angular/src/app/machines/machine-loadout-detail/machine-loadout-detail.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca0f4f3881c11f92ea4ae52b7c90ae8c78b26a154325d16e3cc7084958d478e9
-size 2061
+oid sha256:e5161fdf6e7cddaa0ff83789c445484b4624908cc8a0245a1eb889371ba091a1
+size 2436
--- NewCloud/angular/src/shared/service-proxies/machine-service-proxies.ts
+++ NewCloud/angular/src/shared/service-proxies/machine-service-proxies.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3289ddc24ff9cc0a13b859dd16f03cca42e824e2e0d6e4493eef9af9899ea18a
-size 37517
+oid sha256:4ab2e6c24898bdf407faf81c758f996792dd2b153cac4d85c6eaeb25479481fe
+size 38064
