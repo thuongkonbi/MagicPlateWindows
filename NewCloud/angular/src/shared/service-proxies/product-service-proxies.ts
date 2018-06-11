0506ee5224425f3cd2ca568d7a38d8f8784738bb
Le Nam <nam@konbinisg.com>
2018-06-11 18:27:16 +0700

    add product price

--- NewCloud/angular/src/shared/service-proxies/product-service-proxies.ts
+++ NewCloud/angular/src/shared/service-proxies/product-service-proxies.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8d9edee49926cde17b0146fd20a83ac1f2a89021d190157e78623d489334a54
-size 25111
+oid sha256:00724be5cd8566ebba5534efeb911a484db1ca01b80cfdc85330124cdba99587
+size 25320
