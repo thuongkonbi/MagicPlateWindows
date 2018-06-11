560fae8bf00d8348baf67f1d9be12e786c479e82
Le Nam <nam@konbinisg.com>
2018-06-11 19:21:13 +0700

    update product price when change selected product loadout

--- NewCloud/angular/src/app/machines/configure-machine-detail/configure-machine-detail.component.ts
+++ NewCloud/angular/src/app/machines/configure-machine-detail/configure-machine-detail.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c47c0fa4fa4fb7cd3eb347b20113bc0d6fb27ddb936537854b67afc7673c8ea4
-size 7633
+oid sha256:ec7ba056f5dee3f4fc8a8ecfa9a9062ffb673f06790dbe1201542d6841b28bfe
+size 8071
