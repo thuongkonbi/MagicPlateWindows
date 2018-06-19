90607a5d2827379b5d45ec7823fdff36c6dc3aab
ha <ha@konbinisg.com>
2018-06-19 12:15:50 +0800

    add alert table

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.EntityFrameworkCore/EntityFrameworkCore/GenericCloudDbContext.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.EntityFrameworkCore/EntityFrameworkCore/GenericCloudDbContext.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55fc06174cd62bba8d47ac1afaa3a06206d49a90fe019153d1402d75a9753555
-size 1551
+oid sha256:747d25882aeaa7d9a58eedf0a50f082684280c1a461b2ccc224dc4056daed9d8
+size 1664
---
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.EntityFrameworkCore/Migrations/20180619041420_AddAlertTables.Designer.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b06aa52c6e6b4efbaa5a93f30131f68c3793567982241a8ff20a04cb9660be09
+size 58032
