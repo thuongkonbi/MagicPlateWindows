ecdd920b3111e77320db6e7065ba09b53c848562
Le Nam <nam@konbinisg.com>
2018-06-23 16:26:07 +0800

    machine staus intergration and UI

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Machines/MachineAppService.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Machines/MachineAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e57cce38270676e1f312821931bf76a1a9dac7ab84ac134273b6d4a37bbb43bb
-size 17996
+oid sha256:7e66eb853ab1f5f4dbf2a2917f22612912321da5062f803d50eebe5885bb44e9
+size 20499
