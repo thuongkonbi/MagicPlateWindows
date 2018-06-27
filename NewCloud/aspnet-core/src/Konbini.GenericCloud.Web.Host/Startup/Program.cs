3348243dca16bfeedf466e5c6d7f0952883a5d64
ha <ha@konbinisg.com>
2018-06-27 15:01:26 +0800

    a

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Web.Host/Startup/Program.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Web.Host/Startup/Program.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:914c60ac57d65b9952e7786971dadab3b9cc629272efdc3e3d0f78ffa66bf581
-size 461
+oid sha256:3490f8f0084df552b2582451b820826aeeecbd2827ce66d2a83a204373001b47
+size 512
