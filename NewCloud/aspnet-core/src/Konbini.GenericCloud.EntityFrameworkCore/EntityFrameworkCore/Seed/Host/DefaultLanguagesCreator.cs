8bf124b334d452b03776f66cb7abfa1661dd95cb
ha <ha@konbinisg.com>
2018-06-27 14:01:09 +0800

    ok

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.EntityFrameworkCore/EntityFrameworkCore/Seed/Host/DefaultLanguagesCreator.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.EntityFrameworkCore/EntityFrameworkCore/Seed/Host/DefaultLanguagesCreator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f010ff838e872c75207e60029d6aa73c74acec3ddc619a579efbbec0f98dfdd
-size 2387
+oid sha256:414d11d44e3e7a04f194279a138c6299c755b57d7df675ecc00dcb33f40eedf2
+size 2409
