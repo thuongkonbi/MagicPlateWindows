e15d1391930bf8d140cfcd8384b9d56fa88a0c74
Le Nam <nam@konbinisg.com>
2018-07-02 16:26:55 +0700

    label for VMC Y

--- NewCloud/angular/src/app/vending-diagnostics/vending-diagnostic.component.ts
+++ NewCloud/angular/src/app/vending-diagnostics/vending-diagnostic.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bddce5c6a48d5ae7b3dfb00607aa02852dc3bc8d27ca0cda84146ea291178da0
-size 10221
+oid sha256:ec16f9679d841356cbd35b43938ed1466c75de7372675ba4c805d398505c9be7
+size 10363
