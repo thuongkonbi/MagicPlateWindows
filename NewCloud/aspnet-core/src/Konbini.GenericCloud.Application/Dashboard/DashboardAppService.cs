284426517bdcc819bced1bfb97eb14dab6581f00
Le Nam <nam@konbinisg.com>
2018-07-03 17:06:46 +0700

    dashboad fix label and tran time

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Dashboard/DashboardAppService.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Dashboard/DashboardAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:345442156b0023321268c45d3e5fe7b882712bdcdfd1cf17e879dca1d7e4f682
-size 13470
+oid sha256:31a4d1ada85cbb92625e9258a33c5a35d868d8935a613f9ffdb5ee7008b1f300
+size 13522
