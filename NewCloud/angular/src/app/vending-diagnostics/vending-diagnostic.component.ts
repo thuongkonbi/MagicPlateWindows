6c55f9f9c1a2d16efdec2c083babbc140450d34c
Le Nam <nam@konbinisg.com>
2018-07-04 13:53:57 +0700

    remove angular comipilor error shit

--- NewCloud/angular/src/app/vending-diagnostics/vending-diagnostic.component.ts
+++ NewCloud/angular/src/app/vending-diagnostics/vending-diagnostic.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90ff2664b9a87b55b732b765e32f38a28b104e45cf6748e85ff18cd86204f9b6
-size 11453
+oid sha256:637d10a8cc7cd7eb9798eb316f334572c7912d0be4c7ff1530526ca826c1360e
+size 11589
