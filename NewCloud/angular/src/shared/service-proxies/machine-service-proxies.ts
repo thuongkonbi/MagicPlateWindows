c92b218774e2fdef30b2346cbbdb13a99f78a4a9
Le Nam <nam@konbinisg.com>
2018-07-09 13:28:46 +0700

    machine status upgrade

--- NewCloud/angular/src/shared/service-proxies/machine-service-proxies.ts
+++ NewCloud/angular/src/shared/service-proxies/machine-service-proxies.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:830a4acd538dbd9663b89e8a098b1a31430b0c1ab6eee34ea5d9fe398e60e073
-size 52990
+oid sha256:49a1d077d84e4de023b83a3d2eb5f507c88ebd13c1e7bc648cf964fef6ffbd62
+size 53481
--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Machines/MachineAppService.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Machines/MachineAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36a03ed01b4da10b47335120b2a6c11203be7285f3a3aba5b8838938e8862562
-size 20498
+oid sha256:c4f5de0f17574fd888aaf42242450989c6dbddd9a43f5152e9e886fa6e78ec7b
+size 22299
