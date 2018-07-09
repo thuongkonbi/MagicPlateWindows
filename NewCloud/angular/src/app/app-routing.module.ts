17f1479fadd6357f69310fa19ad5eb719849b02f
Le Nam <nam@konbinisg.com>
2018-07-09 13:27:51 +0700

    fix rule color

--- NewCloud/angular/src/app/app-routing.module.ts
+++ NewCloud/angular/src/app/app-routing.module.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:150536c942ab1ded26c3b31aa55ec4b81ddaed57e66a428b8f0958f8483e4e88
-size 4397
+oid sha256:31ca27bd6bd2201a938f93e2c60421106076f7db743ef121f90550d6d937493e
+size 4403
