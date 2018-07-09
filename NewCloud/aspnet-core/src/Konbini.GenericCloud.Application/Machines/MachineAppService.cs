60852c50d11433944b0345a2a1c42d60cf8d8a30
Le Nam <nam@konbinisg.com>
2018-07-09 15:16:27 +0700

    passwork cloud and get VMC level and temperature from vending history

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Machines/MachineAppService.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Machines/MachineAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cdb5f7ef16e8272d4987385da0fc27efb8c4ef28e6cbc7969f95116742af3797
-size 21380
+oid sha256:0c4cb9d4adec9274f536da96276a284a91adc8f047fee76aed807c13edde5f2f
+size 21594
