a6814b02cd14b356556d8b01d9b8901adf68af2b
Le Nam <nam@konbinisg.com>
2018-07-10 10:15:36 +0700

    fix status vremia

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Utils/TimeUtils.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Utils/TimeUtils.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b9e6222d3d7875aa007aee49c52a43f8fffa0f06b86479ae15610fd02c5e73a
-size 511
+oid sha256:979e8cef7196e8f99d95fbcb4d8dda04761b0411f04590704478778129a0c87b
+size 499
