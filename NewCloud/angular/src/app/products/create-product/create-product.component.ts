a2c547e9b61651d499ee2117bb0af824c14ac678
Le Nam <nam@konbinisg.com>
2018-07-10 14:26:59 +0700

    remove render3 error

--- NewCloud/angular/src/app/products/create-product/create-product.component.ts
+++ NewCloud/angular/src/app/products/create-product/create-product.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21df4a984c728156fddfc75114b456176555f660685bc13c86e3534c5d56f7c8
-size 2840
+oid sha256:1635fc1e907787f0c4c1b32b460a98a720ad2e848953ef9a77cfbfa8a2011c9e
+size 2778
