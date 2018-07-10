a61e5444dd72072c466482e1af58758ce12b1331
Le Nam <nam@konbinisg.com>
2018-07-10 10:08:24 +0700

    process current local time is SIngtime

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Utils/TimeUtils.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Utils/TimeUtils.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44f6845d8ea00d3518c3301cd58a607270380ced86a190522c30e735df455b4e
-size 536
+oid sha256:8b9e6222d3d7875aa007aee49c52a43f8fffa0f06b86479ae15610fd02c5e73a
+size 511
