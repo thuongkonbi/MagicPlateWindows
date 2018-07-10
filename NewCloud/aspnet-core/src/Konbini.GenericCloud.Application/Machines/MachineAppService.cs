082e3311deb6ad4c649d2efb244f14fc004e3f9d
Le Nam <nam@konbinisg.com>
2018-07-10 18:02:55 +0700

    remove lish loadout and others

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Machines/MachineAppService.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Machines/MachineAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a80ce232b87af0df4087fb4871b0c297a9a640b8908428132392fab45f18e092
-size 21882
+oid sha256:d59aa0686c63a47366f9d73ab50bfd1e91e9d436865ac6fe532dc9ac90bd59a1
+size 22577
