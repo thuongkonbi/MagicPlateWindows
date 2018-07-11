caf3dd75c0a0a5a143b932b15139bafd4c2b95b3
Le Nam <nam@konbinisg.com>
2018-07-11 13:49:17 +0700

    default authenticate processing

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Web.Core/Controllers/TokenAuthController.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Web.Core/Controllers/TokenAuthController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9664d520fff91e156db8faa6d95fc72462e2eaecd76c2c9237164b52cfb450a
-size 10323
+oid sha256:95d154938d82e12fce090c0ed40873793ecb9f99fbcc7d15c954dd1fce8ba4ea
+size 10429
