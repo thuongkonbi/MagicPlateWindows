043621814ac25165b9f80d9e255ead1a4e676709
Le Nam <nam@konbinisg.com>
2018-07-12 18:01:47 +0700

    machine error order by decending time

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Diagnostic/MachineDiagnosticAppService.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Diagnostic/MachineDiagnosticAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe80203e4bccf4b6637a5c87f401242e31988fd561de976c32044b9909d96f76
-size 6795
+oid sha256:58cbc5a91abd079d3cacf5ee51c0597d2c1dd4faf4578b8080d7d5cfe5773c06
+size 6832
