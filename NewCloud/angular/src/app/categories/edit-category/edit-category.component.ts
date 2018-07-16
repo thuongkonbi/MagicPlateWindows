25fc0b6b1bf02c98c77d4a0e47dc477a73b4a2fb
Le Nam <nam@konbinisg.com>
2018-07-16 14:12:04 +0700

    edit category image and fix none image product

--- NewCloud/angular/src/app/categories/edit-category/edit-category.component.ts
+++ NewCloud/angular/src/app/categories/edit-category/edit-category.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f530a1fa75962ec6ee4044bbcec274bbcd2dc9f842166fbb16fcd8391f7f39c
-size 2499
+oid sha256:a1df243a97f2f63837a2b4b80b919f7e48fed84bcb2e8c8fef7a4ba5c787f549
+size 3055
--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Categories/Dtos/CategoryListDto.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Categories/Dtos/CategoryListDto.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47ddf1795eb99befa2d4ca84e85d87309a6de0dd473f40cb496e70ea853f4858
-size 456
+oid sha256:199052ccd4c08a49c5da74cf3af322e360c15956abfb9221f18d8dffee12035a
+size 509
