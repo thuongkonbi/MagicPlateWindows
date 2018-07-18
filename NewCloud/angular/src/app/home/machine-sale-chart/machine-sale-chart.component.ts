2e6369daf460853f9bcb6ea974f8bc408af36278
Le Nam <nam@konbinisg.com>
2018-07-18 16:50:53 +0700

    barchart transform label improvement

--- NewCloud/angular/src/app/home/machine-sale-chart/machine-sale-chart.component.ts
+++ NewCloud/angular/src/app/home/machine-sale-chart/machine-sale-chart.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e322a7237cccf1e3744c01dff95bb33063b52ebd18f92353d2ffc31dcaa03e2c
-size 2746
+oid sha256:e0447b97d5b3a107cb51aa56b5efb18a85af55d320e788430310e91ef10f5bc7
+size 3470
