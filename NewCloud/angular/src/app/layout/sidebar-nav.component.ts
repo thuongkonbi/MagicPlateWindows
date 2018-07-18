ba9a80fba72766c5fcd256107cf0e03aa3791a10
Le Nam <nam@konbinisg.com>
2018-07-18 16:08:10 +0700

    help menu

--- NewCloud/angular/src/app/layout/sidebar-nav.component.ts
+++ NewCloud/angular/src/app/layout/sidebar-nav.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e475851d1db58f02d367c8321fca37e42fc3da72704a6cca428daf9430653d3e
-size 2349
+oid sha256:2f1ef878ec2b5b7bd1c9c0de1ddab10fb7c45cd33353db8fe851044e8ff03d65
+size 2585
