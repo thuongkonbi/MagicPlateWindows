cadd3903da362f90209fab989a37a0f476328680
Le Nam <nam@konbinisg.com>
2018-07-19 10:57:06 +0700

    dashborad number transaction eror and success

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Dashboard/DashboardAppService.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Dashboard/DashboardAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd18cabb130f86b84da3e8d35f0593345576e7535736e2b7e1d91e99c00ea05e
-size 14099
+oid sha256:7dcd0b9616707d798ce365978e78f076ff208c320c3269edb2b21f6e14e8c81d
+size 14498
