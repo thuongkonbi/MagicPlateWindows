1395b986cd4febb1f46e6e40a54e2c85e4e464ae
Le Nam <nam@konbinisg.com>
2018-07-20 15:40:12 +0700

    error solution frontend

--- NewCloud/angular/src/app/machines/error-solution/error-solution.component.ts
+++ NewCloud/angular/src/app/machines/error-solution/error-solution.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:578d5aad442f67a5ffa76344f4b82e34de10e00345a3bb61ac99f652a36af183
-size 1687
+oid sha256:e9a114934181757f56de23324b5359299476cfec6e3ad9c5955377e73aba145c
+size 1753
