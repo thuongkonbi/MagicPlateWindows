174a11f17da782283322bf8a80f3df98710ba568
Le Nam <nam@konbinisg.com>
2018-07-20 17:47:50 +0700

    fix speed issue raise invalid list tran errror

--- NewCloud/angular/src/app/transactions/transactions.component.ts
+++ NewCloud/angular/src/app/transactions/transactions.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4cd230c08b502c36960d0c68c3e6d75e4995b8b087af7826873f5e105adce71e
-size 4860
+oid sha256:bab68a2939dbf511f16c902ca2c34c0591dd9a41fc33c0d2a50758000654b82b
+size 5172
