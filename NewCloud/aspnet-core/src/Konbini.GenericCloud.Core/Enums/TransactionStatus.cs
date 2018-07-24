be65054e1dc13a49dbad37b0415dbeeb628cf710
Le Nam <nam@konbinisg.com>
2018-07-24 09:25:49 +0700

    Transaction state

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Core/Enums/TransactionStatus.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Core/Enums/TransactionStatus.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:999292e377f5680826383f091310945e7fefeda50e06c677ad0dd78c8141904c
-size 161
+oid sha256:bfbb0ded6686d06e6faf3b5def7aa716fa5179be7a2a92f38e0f88c8292586cf
+size 212
