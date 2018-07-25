412e6eedf2bcd35f02ec995a3da512205b9001ee
Le Nam <nam@konbinisg.com>
2018-07-25 10:05:01 +0700

    backend

--- NewCloud/angular/src/app/layout/sidebar-nav.component.ts
+++ NewCloud/angular/src/app/layout/sidebar-nav.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2fc714a6b65fbbd98ffc9705cb39295283ef4fe13d3b12050221e972ae412068
-size 2673
+oid sha256:f1825bb8ca3b598ef5b76979d45889296fffc0c5ca4279a911341e42ceee5cf2
+size 2675
--- NewCloud/angular/src/app/layout/topbar-languageswitch.component.ts
+++ NewCloud/angular/src/app/layout/topbar-languageswitch.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d30236f58dd820e031083ea52f67d3a4e1efae26b0485550fb25a080044b1680
-size 1373
+oid sha256:b003ef611d7154a4e42fedc73382cf84fd31c8d2f0c89e44f5048233c96dbaaf
+size 1442
