de792075b6b3ee45e4c438ea0d785a0573a1e1f6
Le Nam <nam@konbinisg.com>
2018-08-06 11:40:48 +0700

    remove invalid temperature from chart

--- NewCloud/angular/src/app/vending-diagnostics/vending-diagnostic.component.ts
+++ NewCloud/angular/src/app/vending-diagnostics/vending-diagnostic.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1223cc0c95292b45f60aacd25356d4cb6774c9b321ecc35aa7a67b1f5ff763d0
-size 16547
+oid sha256:40695e40c36feca1769bc0b4b93995bf7a0e81a1b034b416d2836655f3d16f7a
+size 16701
