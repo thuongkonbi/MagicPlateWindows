a2bc778c54fe6b5d5be86b107987ab82bff46765
Le Nam <nam@konbinisg.com>
2018-09-07 16:55:17 +0700

    shtM

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Core/Configuration/AppSettingProvider.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Core/Configuration/AppSettingProvider.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:184454435e31dfc521634256da57f2b3ff80ffb458973fdc3c9b0059abb23f12
-size 1224
+oid sha256:96e33d7bbc43a3cfad935a0cf892002bd7e8910bc1eb69ad7383922804eb2445
+size 1392
