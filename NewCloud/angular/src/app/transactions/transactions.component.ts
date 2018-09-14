3d40e21d3d6347f23033cbc97e2ba449f3b11d40
Le Nam <nam@konbinisg.com>
2018-09-14 15:29:36 +0700

    display transaction state

--- NewCloud/angular/src/app/transactions/transactions.component.ts
+++ NewCloud/angular/src/app/transactions/transactions.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72ecf7c3d2f40bea100caf76c513076cb205ae48b0d20a41a58ad257005b50aa
-size 5835
+oid sha256:89add733f4d3ab15ed4b01a6912410fada6f2e10af687f732b32be7b29ba98c2
+size 6198
--- NewCloud/angular/src/shared/service-proxies/transaction-service-proxies.ts
+++ NewCloud/angular/src/shared/service-proxies/transaction-service-proxies.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64f44edc2e43bbacc76983b3197fcf726e65a744d13c52412451784973057ac1
-size 16200
+oid sha256:4373ba9da3a1e320922140b162ea01a80eca83b9ecc6f862d570a706c351cc92
+size 16240
