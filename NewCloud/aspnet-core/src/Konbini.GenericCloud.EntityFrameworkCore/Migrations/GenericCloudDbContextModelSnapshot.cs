d2db0647bf0fafd15cd4b9848f277baf8157003a
Le Nam <nam@konbinisg.com>
2018-09-21 14:48:14 +0700

    addBankTransactionInfo cloud

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.EntityFrameworkCore/Migrations/GenericCloudDbContextModelSnapshot.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.EntityFrameworkCore/Migrations/GenericCloudDbContextModelSnapshot.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:910684003866ac0eae2306d5c2a9e05725c7966b580ee76e127b01f6b7b14e1a
-size 60553
+oid sha256:1f7c2f86aa83f2e1c7eecd281767dbc54640a2ae04dee673d456a0f62b814998
+size 60812
