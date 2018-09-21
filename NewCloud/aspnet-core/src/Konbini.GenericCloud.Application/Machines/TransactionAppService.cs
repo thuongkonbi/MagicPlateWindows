3584c1ea33bb9e7cb9e5563af5a901eb1de608f7
Le Nam <nam@konbinisg.com>
2018-09-21 15:50:19 +0700

    add to report transaction

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Machines/TransactionAppService.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Machines/TransactionAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fcd6a075b137b99f655c84b6f29d7ab03c5305614f241c37a83d88d0de555886
-size 10001
+oid sha256:9f2e4cfe8b24df2be81fdd7ae454a05ee8cb79b0218d90008b189ede72eb22f1
+size 10206
