33ae4635950bf5af2360d0c46816b2d77574df20
Le Nam <nam@konbinisg.com>
2018-09-24 17:36:24 +0700

    inventory data for report

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Core/Machines/LoadoutItemStatus.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Core/Machines/LoadoutItemStatus.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14b5c321ee45e93dc1c101fd57866fc010001f4eb6e7c3dbed426b20dc445c52
-size 463
+oid sha256:a85e74c2b264209a061fc2de1720f33c39ce3de388bd7bda2843ccef82aa2212
+size 562
