3b150d06be324725337dda36545c7d704aa8e1f2
Le Nam <nam@konbinisg.com>
2018-09-25 10:32:57 +0700

    inventory report

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.EntityFrameworkCore/Migrations/GenericCloudDbContextModelSnapshot.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.EntityFrameworkCore/Migrations/GenericCloudDbContextModelSnapshot.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f7c2f86aa83f2e1c7eecd281767dbc54640a2ae04dee673d456a0f62b814998
-size 60812
+oid sha256:715f6ea597225e3efb302f72c5cb5c0e256e4ec359b90b79aedfaecc7f0e06f7
+size 60976
