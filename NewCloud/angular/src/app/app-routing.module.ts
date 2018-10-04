9bbbade458bd64b3ce06f2dd173073591f04f5e0
Do Ngoc Tu <Tu@konbinisg.com>
2018-10-04 16:07:27 +0700

    Add black vmc diagnostic screen

--- NewCloud/angular/src/app/app-routing.module.ts
+++ NewCloud/angular/src/app/app-routing.module.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63f07f5024bcc2fdd2758f71e3ad7d4c9b0b240c1ec40c94eb723376e63387a0
-size 4647
+oid sha256:7558274e6ee68f7cb675c8da8f16251cc34823faa6b251ee603f5a7a9d3e6af5
+size 4851
--- NewCloud/aspnet-core/src/Konbini.GenericCloud.EntityFrameworkCore/EntityFrameworkCore/GenericCloudDbContext.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.EntityFrameworkCore/EntityFrameworkCore/GenericCloudDbContext.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:770de51ea9d564a4299d08a22cd6cfb890658c90e5b980548e8e8e626fd9ff10
-size 1809
+oid sha256:92b8473056a78e722f41218a8cdb34be0efcd6bdcb1edc41555827df4aa73079
+size 1989
