85a24a1af6e68ed22b1e776b1621a2d4dd817218
Nam <nam@konbinisg.com>
2018-10-04 11:00:50 +0700

    cancel transaction change issue

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Core/Machines/DetailTransaction.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Core/Machines/DetailTransaction.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01a4b3b175456d7ee6a194d55d940fa8f3f933c6dc0a4c14f54e63f93c358a47
-size 3658
+oid sha256:74e823a1f4aeccb39ff3486146b37075b16558080da08e0ecd3a5680cf4add92
+size 3660
