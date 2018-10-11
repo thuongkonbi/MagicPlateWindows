7854f53e690aa731953d61c7255b451add2ab08f
Ta Tuan <tuan@konbinisg.com>
2018-10-11 11:18:23 +0700

    add interval get machine status

--- NewCloud/angular/src/app/machines/machine-statuses/machine-statuses.component.ts
+++ NewCloud/angular/src/app/machines/machine-statuses/machine-statuses.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f83bec9066dd05e24300daec09260ead59cd21f20e5fc61220a837eecdd5491
-size 1597
+oid sha256:bc18583af3fa4b3fe97308725b413624ba22331731c0f44e8c50cba46770451f
+size 2328
