8d7d6ac1d2d737b2078fe31920171571c7912244
Ta Tuan <tuan@konbinisg.com>
2018-10-15 09:57:35 +0700

    remove testing code

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Machines/MachineAppService.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Machines/MachineAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99998f41d26060cd7bfd000e95d3a421c825d546b8d15350b4ab7c590b089a6c
-size 21349
+oid sha256:f3dfcb0729148d5be6c67346554ba86b216b262541d71d2ff8bcee863cc8585a
+size 21099
