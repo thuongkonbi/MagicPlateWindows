efba9ed4b60808515b26b2e60118f8f810fcb4b2
Do Ngoc Tu <Tu@konbinisg.com>
2018-10-15 09:11:05 +0700

    Fix edit / create

--- NewCloud/angular/src/app/employees/create-employee/create-employee.component.ts
+++ NewCloud/angular/src/app/employees/create-employee/create-employee.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:362d91fd18bd4fedf27e6f5bcaf1a2190763e4f2f9a8b989e912bfe496158d16
-size 2729
+oid sha256:7fead679f4c5018a04e4b1b51e5cc7f21410a072fe53293758f0bf8af837ec3e
+size 2933
