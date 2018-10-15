b116d7dfe4517bacbb8ed736e3ec46e4c2c4a217
ha <ha@konbinisg.com>
2018-10-15 14:32:24 +0800

    add mockup apis

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Employees/EmployeeAppService.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Employees/EmployeeAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b53ae811172bd3eb63d3d3b0c17433d7347ed4bc7d05e799ed5578658d4250cf
-size 8278
+oid sha256:ceeaa9954025ad63dd73b794bcdba6f9a3d0c50283f9d47b260e86da42925a7f
+size 9240
