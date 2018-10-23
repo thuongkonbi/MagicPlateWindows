9021383eae45a607f68cd21d0eff7cd21cf59762
Ta Tuan <tuan@konbinisg.com>
2018-10-23 11:27:32 +0700

    fix slack duplicate msg

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/AzureIoT/MachineOfflineCheckingJob.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/AzureIoT/MachineOfflineCheckingJob.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3f4f6f758245d360e170d384b944e7a6da3d34e2525cd6ca7027a0610d32fbd
-size 3154
+oid sha256:0f1bbb521c5e28175490949e55a66c086f580d684b83fa178de3d9c19554ed0b
+size 2963
