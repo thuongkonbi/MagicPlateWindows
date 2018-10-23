9f0100d146150c46742783d229b496e485b60e79
Do Ngoc Tu <Tu@konbinisg.com>
2018-10-23 14:44:49 +0700

    Fix bug

--- NewCloud/angular/src/app/machines/configure-machine-detail/configure-machine-detail.component.ts
+++ NewCloud/angular/src/app/machines/configure-machine-detail/configure-machine-detail.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7d95f41d0ea83e11d7aac485be1dab06a005f5d382313073c8ae0dae837fcde
-size 11065
+oid sha256:ad8a26e58512ee87a65446177be455d174b000b37f551d1904d676744d2ff05c
+size 11205
