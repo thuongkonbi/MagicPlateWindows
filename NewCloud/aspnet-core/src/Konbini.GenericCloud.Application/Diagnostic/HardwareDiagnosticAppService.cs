57f0d8295805d9532b74dd5cfc92e4a3a870ad42
Ta Tuan <tuan@konbinisg.com>
2018-10-23 15:58:12 +0700

    disable cache for GetAllHardwareDiagnostic

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Diagnostic/HardwareDiagnosticAppService.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/Diagnostic/HardwareDiagnosticAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3116a0eb8410b4622bbf6e040dc267b82f0f151c3b7929064739c5195071e0aa
-size 4982
+oid sha256:c7cc211a8ed699d283f4cca24a5e8dd0206e57dd0d0fa66cfc3c41af63fbc65a
+size 4810
