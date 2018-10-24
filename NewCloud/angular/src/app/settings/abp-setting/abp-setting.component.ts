f54afe4baee95f7a67e0cfb14f6cca6af0699a93
phuong <phuong@konbi.cloud>
2018-10-24 17:24:13 +0700

    add setting screen

--- NewCloud/angular/src/app/settings/abp-setting/abp-setting.component.ts
+++ NewCloud/angular/src/app/settings/abp-setting/abp-setting.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ff6b1732f12600ca82675bd67bd8f6997a11083cb659b76132a58536b7b775c
-size 2203
+oid sha256:695c79d347e13106604ce2a6c64c442aed8636a9d8e6b7a3862bcdcf8ea2b672
+size 2211
