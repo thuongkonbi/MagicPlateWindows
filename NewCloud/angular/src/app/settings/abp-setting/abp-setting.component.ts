6eaecc0a1b72a9b9b7686350a8fb30ee5bf09775
phuong <phuong@konbi.cloud>
2018-10-25 14:18:23 +0700

    add filter on setting screen

--- NewCloud/angular/src/app/settings/abp-setting/abp-setting.component.ts
+++ NewCloud/angular/src/app/settings/abp-setting/abp-setting.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:695c79d347e13106604ce2a6c64c442aed8636a9d8e6b7a3862bcdcf8ea2b672
-size 2211
+oid sha256:00b4a6b33e5df0614c71dffb3e0966a9aac0923a28cc8afe88cd0e6823cfb987
+size 2494
