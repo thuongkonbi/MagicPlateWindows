9cfa39c4795617b9dc6ea3a70ad1c19d7f21b3b2
phuong <phuong@konbi.cloud>
2018-11-01 13:58:15 +0700

    fix update setting

--- NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/SystemConfig/SystemConfigService.cs
+++ NewCloud/aspnet-core/src/Konbini.GenericCloud.Application/SystemConfig/SystemConfigService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f2d3c33606ec8df972eea7fc02358e8a03c97b496d760cebb8a38ceb83a738d
-size 3936
+oid sha256:36a245883f41488007d8799ee00df522557b7f0aa702e4774ae62864736254ea
+size 3983
