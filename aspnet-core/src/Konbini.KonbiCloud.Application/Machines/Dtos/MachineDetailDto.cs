b0514f3d52377d526cd027e5a0da94cad7afcdd2
ha <ha@konbinisg.com>
2018-11-08 17:06:32 +0800

    refactor rediscache

--- aspnet-core/src/Konbini.KonbiCloud.Application/Machines/Dtos/MachineDetailDto.cs
+++ aspnet-core/src/Konbini.KonbiCloud.Application/Machines/Dtos/MachineDetailDto.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0738bd998ac9bbca69d93c060a2bbecf1eb1d13bbdf100dfafad75b5b35216c
-size 2848
+oid sha256:2068c734b64e7ebcc6038e020548d91cd14ab9c3d8828f1a1f4d054060a3e5c6
+size 2062
