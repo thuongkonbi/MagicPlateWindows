f5b36ad53fa08c6abd074a52be5bc02a72dd5d87
phuong <phuong@konbi.cloud>
2018-11-12 15:34:06 +0700

    fix bug display item in overview homepage

--- aspnet-core/src/Konbini.KonbiCloud.Application/Dashboard/DashboardAppService.cs
+++ aspnet-core/src/Konbini.KonbiCloud.Application/Dashboard/DashboardAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c98634d54f488d73bb362fae838caf6a4ea20a07eacadf7eb949b4bc58281cd
-size 15615
+oid sha256:91be195a1b260d14385fa17a616c01bd55ecf904b3e277794cdf854594e79163
+size 15742
