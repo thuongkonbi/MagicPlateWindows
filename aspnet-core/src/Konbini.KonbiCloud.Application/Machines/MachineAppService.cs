17561d785650e6379a41a13790fb330d0d026d6d
Do Ngoc Tu <Tu@konbinisg.com>
2018-11-14 08:34:16 +0700

    Fix bug

--- aspnet-core/src/Konbini.KonbiCloud.Application/Machines/MachineAppService.cs
+++ aspnet-core/src/Konbini.KonbiCloud.Application/Machines/MachineAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b1470c95c8117b1d8cca6cd76ac78513ac856c33b8e1628c9639e2c4d7c4e50
-size 24797
+oid sha256:811596b3fb8217edf51f83ec90bfa9cbce6eccdc865d296b548278677cd5a8d6
+size 24785
