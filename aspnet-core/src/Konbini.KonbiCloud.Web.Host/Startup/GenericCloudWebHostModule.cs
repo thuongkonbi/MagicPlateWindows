5100b34c470690c009477989672b07550e5d6bdf
ha <ha@konbinisg.com>
2018-11-14 15:55:30 +0800

    deployment_cp

--- aspnet-core/src/Konbini.KonbiCloud.Web.Host/Startup/GenericCloudWebHostModule.cs
+++ aspnet-core/src/Konbini.KonbiCloud.Web.Host/Startup/GenericCloudWebHostModule.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6385eb1819e3f65e28006c612b6c19266f68467b4eef0203d0129afb91feeb8
-size 791
+oid sha256:ffbd54ce26f9e32569de1aef5e352a69c99395b7e9cf97070d32b84af87d9066
+size 919
