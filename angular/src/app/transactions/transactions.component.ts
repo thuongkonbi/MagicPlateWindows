86de7df89b1cc2a8e1098b41832b866ed6e95426
Do Ngoc Tu <Tu@konbinisg.com>
2018-11-15 15:04:25 +0700

    Fix bug

--- angular/src/app/transactions/transactions.component.ts
+++ angular/src/app/transactions/transactions.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2211613fe60fd2c421e3af726621b5ec3b7a3621f44e7454fb74b991b7a76be
-size 6253
+oid sha256:b88fb75d23f967c78e72066291bc2ea5b50101f80229a2c03b24aef58a77c5d8
+size 6269
--- aspnet-core/src/Konbini.KonbiCloud.Application/Machines/TransactionAppService.cs
+++ aspnet-core/src/Konbini.KonbiCloud.Application/Machines/TransactionAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:320705c09b99a6f55da47eaf2e79f147d16ede91df264d8865775a5d985e5f19
-size 15031
+oid sha256:6ad8c5e5ce9bcccb88ecd49db37ead73951cadad9ba630ea97339b670f381468
+size 15042
