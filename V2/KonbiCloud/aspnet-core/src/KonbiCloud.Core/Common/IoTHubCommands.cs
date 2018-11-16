bfd5e009ee81f45cb63f0eebbce449212f07de23
ha <ha@konbinisg.com>
2018-11-16 11:07:28 +0800

    add entity

---
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Core/Common/IoTHubCommands.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e06629ff1f63f756b86a885f9296d2a9a49aaced822fcb2148d33dd6e9f37374
+size 252
---
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Core/Diagnostic/VendingStatus.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:11b696f9494a55621d96c55ac13ce5152fe8729461270d0cde9b834ed271b6ae
+size 653
