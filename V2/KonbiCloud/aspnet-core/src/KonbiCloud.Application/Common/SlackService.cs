768ab14d773bfbe872b52da9a6d70114677a7ee5
phuong <phuong@konbi.cloud>
2018-11-16 17:34:15 +0700

    Add api

---
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Common/SlackService.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:98973eee7278b9ab3c1db367e72c7afd92aba46cac3473f74e75ae1067405aca
+size 1565
---
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Diagnostic/MachineDiagnosticAppService.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:256bf14e81a437bf44a031ef6b629f60ff4c7c64a4ffb72016ec4dd73b5b976e
+size 6739
