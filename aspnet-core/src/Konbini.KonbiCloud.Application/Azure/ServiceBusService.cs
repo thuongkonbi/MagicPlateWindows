e5a02c95fed9ef0a2ea2fa535a30ced4c2eb38e7
ha <ha@konbinisg.com>
2018-11-19 19:11:30 +0800

    ok

--- aspnet-core/src/Konbini.KonbiCloud.Application/Azure/ServiceBusService.cs
+++ aspnet-core/src/Konbini.KonbiCloud.Application/Azure/ServiceBusService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b51e4aa0f2b2d6b325191141d03e7d3fe6843ffa9cda7ff734a060694feae632
-size 3227
+oid sha256:b2d768b6fa1c558947806982e8cb7c10cc33ab4feb9b3d504bec594e9e934d7f
+size 3175
