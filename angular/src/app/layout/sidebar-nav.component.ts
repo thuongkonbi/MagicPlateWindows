f4f0e8d723034ffdeb53a451305d68f1cc71d677
ha <ha@konbinisg.com>
2018-11-20 16:52:59 +0800

    ok

--- angular/src/app/layout/sidebar-nav.component.ts
+++ angular/src/app/layout/sidebar-nav.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fac61947fea2db74c0e10299322412d3232e8e589408fea353c0271eff292c1b
-size 3621
+oid sha256:435eb223a5b9135318cc0ca5e78034611ec0caae4a5945734d38f62d246355f5
+size 3619
