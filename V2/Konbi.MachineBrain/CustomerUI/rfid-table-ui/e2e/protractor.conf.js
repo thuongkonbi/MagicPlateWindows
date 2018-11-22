b53cfdd5616d51a88739db0c04e790665aea1b10
ha <ha@konbinisg.com>
2018-11-22 13:24:24 +0800

    add customer ui project

---
+++ V2/Konbi.MachineBrain/CustomerUI/rfid-table-ui/e2e/protractor.conf.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f9408b90f14de03fdeb1e6b60b3724b3860e42390fab22f8cc6660d20ae6e66e
+size 752
---
+++ V2/Konbi.MachineBrain/CustomerUI/rfid-table-ui/e2e/src/app.po.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fc75ee22c4d602f057aef0c8b34d3ef16194eb485051d0abb10e1a50a4ac49a8
+size 204
