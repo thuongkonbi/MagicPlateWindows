4591f40d600d37153fc57d8c403af4895bb11063
phuong <phuong@konbi.cloud>
2018-11-29 15:47:01 +0700

    fix create plate

--- V2/KonbiCloud/angular/src/app/main/plate/plates/create-or-edit-plate-modal.component.ts
+++ V2/KonbiCloud/angular/src/app/main/plate/plates/create-or-edit-plate-modal.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c00d4727f8dd7ac7ac0f3bfa8091388214c2c2ccc463713a83c8a14df8edcb0c
-size 3449
+oid sha256:9bd9c33c6285aa4041e2c4e0d68cc57ed2d28580c9346176087fa31e1c509073
+size 3424
