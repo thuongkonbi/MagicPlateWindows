e00e6cc8c3868e081ec899b02c9afc7eeb10cc25
phuong <phuong@konbi.cloud>
2018-11-30 16:20:36 +0700

    add disc service proxy

--- V2/KonbiCloud/angular/src/app/app.component.spec.ts
+++ V2/KonbiCloud/angular/src/app/app.component.spec.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c4e88bc225483d16db7ed3bab998d492a81ad926801095a6320b845073a231a
+oid sha256:738e233978f6115856ad5305e0e161668819e503beaf8be48fc35fdfe04b9cb6
 size 1011
