5a647679508e0eb420867cc551790c83614c9577
phuong <phuong@konbi.cloud>
2018-11-30 16:38:19 +0700

    fix export function

--- V2/KonbiCloud/angular/src/app/shared/layout/nav/app-navigation.service.ts
+++ V2/KonbiCloud/angular/src/app/shared/layout/nav/app-navigation.service.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f148651898300a5e8c9fadd54aa08167e307c2b8fa20f953f45b5e39707dff9
-size 5046
+oid sha256:01645af963a98c4470b7f3a8724d8ec21eda162e4d485ce31f8304c9702678aa
+size 5038
--- V2/KonbiCloud/angular/src/shared/service-proxies/service-proxies.ts
+++ V2/KonbiCloud/angular/src/shared/service-proxies/service-proxies.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cef06f069126c6b6c8a3e2e2e07e73f3e728bb1a303a9fd8fc2ef7ce38bc8680
-size 1199648
+oid sha256:3ecd6ce9c99893d9c5897220a0c55fa1db0990badc388a127b4517886a41c83e
+size 1199662
