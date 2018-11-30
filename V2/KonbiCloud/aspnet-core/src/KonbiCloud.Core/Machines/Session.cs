fd860108f5a89fdfd07c0a55b109f194aeda1016
ha <ha@konbinisg.com>
2018-11-30 16:50:28 +0800

    optimize dashboard service

--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Core/Machines/Session.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Core/Machines/Session.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a18a42069521bfeb125687567b6becf343482c414b5f6fb9dd46100291bd8b5
-size 361
+oid sha256:8eff8ba7472a736906c67c7a68be551afd433c1b704ab31062162b0c1e662a85
+size 519
