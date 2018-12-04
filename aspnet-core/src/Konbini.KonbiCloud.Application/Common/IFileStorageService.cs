e85ccdcaee0b96a6fe29daf54e8cfcacbad35c08
ha <ha@konbinisg.com>
2018-12-04 15:32:26 +0800

    add file existed function

--- aspnet-core/src/Konbini.KonbiCloud.Application/Common/IFileStorageService.cs
+++ aspnet-core/src/Konbini.KonbiCloud.Application/Common/IFileStorageService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6449a3b476f8c94bed18c957b16b0fcd570a02c7194204aeb24900879efd840a
-size 299
+oid sha256:eca7d1a8982b2be627e8f3dea8a10ab06ef02f4939bbf960e261cc3c344ab968
+size 365
