514a0a73f7884761c79436fba4fd69f5db94c40a
Ha <ha@konbinisg.com>
2018-12-05 12:41:36 +0800

    modify machine log logic

--- aspnet-core/src/Konbini.KonbiCloud.Application/Machines/MachineLogAppService.cs
+++ aspnet-core/src/Konbini.KonbiCloud.Application/Machines/MachineLogAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ef0d24c95abed9b95db31458868a72c1df2fbf663b0dbb1082bd859c016982f
-size 6903
+oid sha256:a59b5e3d12e1c30b40e608d17406ce8a3e5f55919d7156ab9b43510cf2e288d3
+size 6911
