4bf23677348631ae61e4c709e92cddad5d8477b9
Do Ngoc Tu <Tu@konbinisg.com>
2018-12-06 10:54:13 +0700

    Order session

--- aspnet-core/src/Konbini.KonbiCloud.Application/MachineSessions/MachineSessionServices.cs
+++ aspnet-core/src/Konbini.KonbiCloud.Application/MachineSessions/MachineSessionServices.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da8b015c571c6c9b0de18db1bb6211a9cfd7dfe78c62ee21ddd83dea5532e8fd
-size 5897
+oid sha256:0ea2e518d6633f83f3fc1c2c0932e29d67c2e41da18e62e2ebe0aefacac4d2a2
+size 5634
