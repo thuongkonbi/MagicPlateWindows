b73ffa6d3f85047c37a03468fd1e9f0b639a75fb
phuong <phuong@konbi.cloud>
2018-12-06 14:25:41 +0700

    fix

--- V2/KonbiCloud/angular/src/customer/customer-routing.module.ts
+++ V2/KonbiCloud/angular/src/customer/customer-routing.module.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e01ef1f0b5167c82d142a5261dbe18af07208ac29d90cfb09a537bb01a80f79
-size 540
+oid sha256:781b88ed01efd27313b50398da8c3fe85a7e1e0c355013d2364ce4e86df4e5fd
+size 499
