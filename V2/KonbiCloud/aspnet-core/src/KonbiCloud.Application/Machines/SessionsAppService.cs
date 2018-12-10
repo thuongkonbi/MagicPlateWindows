87b52568e148f45152262cddc03ee5d782765b8a
phuong <phuong@konbi.cloud>
2018-12-10 17:30:45 +0700

    sorting session

--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Machines/SessionsAppService.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Machines/SessionsAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b61dec3d7674d4d9bfc6160b07cf7b1f64118fc7c6bdc12f3873c9eb388916fc
-size 4654
+oid sha256:fc68521f79e45c712f9d39f6f83da24e0a698b8b06c36f75ed85bdad2efdedd1
+size 4659
