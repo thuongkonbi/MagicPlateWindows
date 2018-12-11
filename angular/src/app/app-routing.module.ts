741ba0ce16321798ab0e9b4a872e7c168eb644f6
ha <ha@konbinisg.com>
2018-12-11 15:07:29 +0800

    fix bugs machine logs

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74506373e30c7f8c83834c520655aa634fd548e65e34cbbede107603cac25a99
-size 5959
+oid sha256:c505b0c8abd703ebe6a47b1141e54dfbcc27dfc0ca03a44f5aa6d87281e7dc36
+size 5954
