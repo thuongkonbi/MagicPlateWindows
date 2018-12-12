a4bc5cbf9885c93ab2ea3232ccacf30aa2b4b525
ha <ha@konbinisg.com>
2018-12-12 15:59:10 +0800

    ok

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.Common/Services/LogService.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.Common/Services/LogService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8f6c67bdb0bda4750b72f0db60e7476dcaaad0d3a6b069dbd8f3371d63df48a
-size 24109
+oid sha256:32c86c170c3dfc5228c85c4896c74dc5057f0cdab1fe460597f5116137a783fe
+size 23797
