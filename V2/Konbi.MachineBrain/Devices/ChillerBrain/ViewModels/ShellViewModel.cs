006a6a0f74ff3486a2ea48cfddaa55eae2ca504c
ha <ha@konbinisg.com>
2018-12-12 15:48:44 +0800

    build konbi.devices ok

--- V2/Konbi.MachineBrain/Devices/ChillerBrain/ViewModels/ShellViewModel.cs
+++ V2/Konbi.MachineBrain/Devices/ChillerBrain/ViewModels/ShellViewModel.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e8bbc92098a178fa0bbf102446fe3f4392ed02c8fbe91556f679d56f2407cbb
-size 6551
+oid sha256:c06bec400e792e9f1ea2e96bb3136bdb70f3353214b7ce7a2395da10011bae91
+size 6494
