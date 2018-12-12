188f57032a6f9158e32d7322c1188e5db51e95ae
Do Ngoc Tu <Tu@konbinisg.com>
2018-12-12 10:44:40 +0700

    Fix bug

--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Core.Shared/Plate/PlateConsts.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Core.Shared/Plate/PlateConsts.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cccce4aac72a8bd7817d1ccf6506db202dac2e160b74e18c316ce3c740106757
-size 107
+oid sha256:8ff32cea49a01b8405638432e70e9b4d1b0500214b7d7aefdcf94eb12799c258
+size 126
