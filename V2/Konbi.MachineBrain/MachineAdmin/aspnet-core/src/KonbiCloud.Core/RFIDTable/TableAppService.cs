87f6021069a1b8f0d230ad92be7470369faf152f
phuong <phuong@konbi.cloud>
2018-12-13 16:31:37 +0700

    fix bug

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Core/RFIDTable/TableAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Core/RFIDTable/TableAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee307134dbda1308eff1334c0b9b6954db86170756e97ccde0a14a4431d789d6
-size 2157
+oid sha256:ab951036644f9b08a6699b6affaa73270b275c6e4e90e871cf658b961ce07b52
+size 2184
