67e30baed99f3d25247d3ce67c6cb2fbc692109b
ha <ha@konbinisg.com>
2018-12-13 19:03:47 +0800

    change class name

---
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/BackgroundJobs/PaymentResponseNsqIncomingMessageService.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:db0f43427feb746b22fd9f1faa10987d17404a5e8f3675cdd19e4965471a03ff
+size 748
--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Web.Host/Startup/KonbiCloudWebHostModule.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Web.Host/Startup/KonbiCloudWebHostModule.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1049c56aebe2f22787bc5841e73a1df9dcd8dbf4e73ea05ec354f2177e1cde41
+oid sha256:aa55622393861511052acde3e00d3c24c089b72c4146c59886a0ed127df8365c
 size 5681
