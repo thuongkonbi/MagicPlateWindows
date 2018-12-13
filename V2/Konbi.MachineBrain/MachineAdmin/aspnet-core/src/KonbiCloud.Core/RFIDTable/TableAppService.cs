1a843de36ecac97853d08f48c4706d08ef89c067
ha <ha@konbinisg.com>
2018-12-13 17:31:35 +0800

    modify

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Core/RFIDTable/TableAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Core/RFIDTable/TableAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee307134dbda1308eff1334c0b9b6954db86170756e97ccde0a14a4431d789d6
-size 2157
+oid sha256:eabde1fa46a280e4cf972a537a93df91c1067cc30de6d3b14ecb372933c43435
+size 2184
