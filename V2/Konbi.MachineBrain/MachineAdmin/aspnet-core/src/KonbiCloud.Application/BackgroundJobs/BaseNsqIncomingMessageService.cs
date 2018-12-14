c54fcf11f8442f09f24dea133dc4c8752cfed28b
Do Ngoc Tu <Tu@konbinisg.com>
2018-12-14 15:23:05 +0700

    Transaction

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/BackgroundJobs/BaseNsqIncomingMessageService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/BackgroundJobs/BaseNsqIncomingMessageService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f0f8dae3e322134253be881b28f8c76a5c51aca36a0ee50c051550b9a9b331e
-size 1665
+oid sha256:6b767678265fe51384da01011b4e73d124836fa47fe62241cea2272d9e3b1283
+size 1667
--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Core/Transactions/DetailTransaction.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Core/Transactions/DetailTransaction.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05ea68d2bbe1bb726a4eafabd78ee75ac5f59cc4e2d23ee8ada5f4d7975574c4
-size 1338
+oid sha256:c88186c2270b4b7601449ed87c46322f348405b4e84724bcf367c6857ceb85d8
+size 1420
