2a287b4b97ed19bcf24c7896d077aa7f5475a91e
Do Ngoc Tu <Tu@konbinisg.com>
2018-12-14 17:29:49 +0700

    Fix bug

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/PlateMenu/PlateMenusAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/PlateMenu/PlateMenusAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3c1b86f0d9abf7b259ca1036296bc5185117f5606d67e716e75d5d8619e10f2
-size 13565
+oid sha256:b793931d88bc998554261bad649e26fb4bf4f5818dffae1e0ff4ef436a08c977
+size 13924
