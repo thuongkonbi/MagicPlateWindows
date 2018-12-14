fcf0a1a5fad5916b1f18b20f7fac276e802286f6
Long Le Hoang <longdt@live.com>
2018-12-14 16:04:40 +0700

    resolve merged

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/BackgroundJobs/RfidTable/RfidTableNsqIncomingMessageService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/BackgroundJobs/RfidTable/RfidTableNsqIncomingMessageService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07d32ff4d8c4ca0486ae2e95b375ffd2e12d5dd39c8a2a566eac4d357579fc8e
-size 4731
+oid sha256:f680550bd5e8ae6e9d4bca6444cbe1f90ac32e65a70bccd76d7625bb7923a004
+size 4737
--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Core/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Core/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da3462b3ddf76d5ecab185778ed16f0e44c85aaec109e3ddc9d861b598fb1ced
-size 3254
+oid sha256:37a198664cfc23aa8e23431f8ce59df25726418acde8a8c7cab5918c0b154213
+size 3271
