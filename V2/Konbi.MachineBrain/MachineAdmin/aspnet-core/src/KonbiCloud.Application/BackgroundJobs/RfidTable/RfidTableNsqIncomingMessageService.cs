66fef19dd2fada55b1791616dd0f8083c92fb8eb
ha <ha@konbinisg.com>
2018-12-14 14:45:45 +0800

    ok

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/BackgroundJobs/RfidTable/RfidTableNsqIncomingMessageService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/BackgroundJobs/RfidTable/RfidTableNsqIncomingMessageService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92ffbb91e23a3f5fbc55be8d688a1b90f92903be59194a5f2ee70613f29b360f
-size 3555
+oid sha256:1ab4880846911f6b8cf322b9d40a2c8bc8b255436504da6a2983fec278f33732
+size 3385
