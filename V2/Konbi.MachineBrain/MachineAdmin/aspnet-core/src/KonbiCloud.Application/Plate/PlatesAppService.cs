356987e3d4736914f76e12b6c6f86d4a6c6aedc8
phuong <phuong@konbi.cloud>
2018-12-17 11:32:17 +0700

    import csv + check duplicate plate code

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Plate/PlatesAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Plate/PlatesAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9c624443e631ca5e6f998c7a940e37595eb13ce87e64327110c6d5ec5bfd0dc
-size 10212
+oid sha256:03763a7847348e5a11cd9768109de8805854c01cdb7ac5c2a8564c0cff1effd9
+size 11305
