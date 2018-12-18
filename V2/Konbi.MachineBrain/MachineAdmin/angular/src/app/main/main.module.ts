8e32d1e1f360a88e940e8622d8ffbc2a4007d4eb
phuong <phuong@konbi.cloud>
2018-12-18 11:48:15 +0700

    fix merge

--- V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/main.module.ts
+++ V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/main.module.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f67730e60fa3dbae4a5c9f9791e85f22b107126939ef5b36f1611b10b4158fd4
+oid sha256:7982a23879689d8fe2a00ec9a805bbbc53e1a421d295ae9c7e723d1629ba4231
 size 4759
--- V2/Konbi.MachineBrain/MachineAdmin/angular/src/shared/service-proxies/service-proxy.module.ts
+++ V2/Konbi.MachineBrain/MachineAdmin/angular/src/shared/service-proxies/service-proxy.module.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f4531f06dd9710b41ebffea2e731a5f69055002ff4e6122182081a008608b8a
-size 3196
+oid sha256:b9c7250f2d8c19fe2542f677583d549748ae76da2eca1b6f88e03c90fe4c9353
+size 3197
