8e065b848878a13e4d35260db45ba32412e07384
Do Ngoc Tu <Tu@konbinisg.com>
2018-12-20 11:51:03 +0700

    show emp card in transaction

--- angular/src/app/transactions/transactions.component.ts
+++ angular/src/app/transactions/transactions.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e72c217e56bfbbcf61723651664ae76966ff89aff3a19132b65d07ea763d684f
-size 9488
+oid sha256:d73ba5624770c230fcb4ba8c6e84a54792258ca0569c8bbe2fe08e1bd65bec6f
+size 10418
