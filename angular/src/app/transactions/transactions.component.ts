0db342fc9ffb4fe9860fe76647ce280198a172c7
Do Ngoc Tu <Tu@konbinisg.com>
2018-12-20 15:01:19 +0700

    Add column to transaction csv

--- angular/src/app/transactions/transactions.component.ts
+++ angular/src/app/transactions/transactions.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d73ba5624770c230fcb4ba8c6e84a54792258ca0569c8bbe2fe08e1bd65bec6f
-size 10418
+oid sha256:a66eda62a691ac0054150de17bb0b858cdd332afe74e8252851de086f1652c07
+size 10263
