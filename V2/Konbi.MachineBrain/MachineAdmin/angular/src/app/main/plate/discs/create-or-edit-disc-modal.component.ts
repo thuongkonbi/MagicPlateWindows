d025aa1d01078026a96f85295df07af85e1d265f
Long Le Hoang <longdt@live.com>
2018-12-26 15:05:04 +0700

    Resolved issue cannot parse Json when registering new plates

--- V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/plate/discs/create-or-edit-disc-modal.component.ts
+++ V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/plate/discs/create-or-edit-disc-modal.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:310d2656fe65010b39d9fa0a2d3235ccca9ecb663b67b62bde4d7726c7df058c
-size 6345
+oid sha256:d27e84356d8b549462e396dd76970b70d5f5ca111fbc107ee40206a496f6bf85
+size 6353
