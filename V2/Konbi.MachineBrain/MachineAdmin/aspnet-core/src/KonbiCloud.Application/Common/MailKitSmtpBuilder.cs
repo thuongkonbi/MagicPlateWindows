98cdc88513fbb1f6a42a5527d081a35453ad0840
Do Ngoc Tu <Tu@konbinisg.com>
2018-12-26 09:18:38 +0700

    Send email

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Common/MailKitSmtpBuilder.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Common/MailKitSmtpBuilder.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43875a39b5af8c56a8ba098541dddd8fcae165c6c0d5d899f9889253451c5ff5
-size 722
+oid sha256:053890b527e9cbe8fea06bd9ba16f3dcf3bc169de2432574df1b37bae7e57c0f
+size 1824
--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Common/MyMailKitEmailSender.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Common/MyMailKitEmailSender.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78f682a43e973acb878aa245a3150269c09ed2bc18ce23a5fb122af75b9b17b5
-size 2717
+oid sha256:19d98a5b9a2e799b269cc8e49791bdd4339e5fe5a1d5e83f0a37d0aa8d1ead72
+size 2669
