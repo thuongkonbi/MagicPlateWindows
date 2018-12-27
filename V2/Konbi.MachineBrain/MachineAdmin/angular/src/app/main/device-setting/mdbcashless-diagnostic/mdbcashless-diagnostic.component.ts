de57a5b6f3e64b052e847e3dfe54cf953fd83ec0
ha <ha@konbinisg.com>
2018-12-27 10:12:05 +0800

    add mdbs cashless

--- V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/device-setting/mdbcashless-diagnostic/mdbcashless-diagnostic.component.ts
+++ V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/device-setting/mdbcashless-diagnostic/mdbcashless-diagnostic.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16ee2fb494e6f883c92d9007ff6d40f204b09b2bb5360b7660dce3fdd5b776c1
-size 468
+oid sha256:d247fed416946ab39c1bdd0f72be8092a0072a93cc6afcfd59a1a046eb9cea5c
+size 531
---
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/DeviceSettings/IMdbCashlessSettingService.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:03cd52fc5a7d2f91e096ebaca17e02e4d32747603207563bf7ee6338b2dd1599
+size 172
