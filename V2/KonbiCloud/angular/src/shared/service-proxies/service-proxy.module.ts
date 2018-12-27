d78e7e76d1293c6099c52d9c4aca8a1d2d3baf65
phuong <phuong@konbi.cloud>
2018-12-27 17:21:19 +0700

    fix

--- V2/KonbiCloud/angular/src/shared/service-proxies/service-proxy.module.ts
+++ V2/KonbiCloud/angular/src/shared/service-proxies/service-proxy.module.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:164ef5fcd918512d290ee8f269cf4cd2034cf00a5180642fb595c5be3f791f86
-size 3061
+oid sha256:1b42fe3c3d7508f1453c27dafb4d9fd2f1ac88d1b18b1b18b63ace441ff00169
+size 3184
