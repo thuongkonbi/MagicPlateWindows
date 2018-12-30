50c882a8156c0a20c43dbb973c04fbaf66a6df67
Do Ngoc Tu <Tu@konbinisg.com>
2018-12-30 15:10:50 +0700

    Add change error state to success

--- aspnet-core/src/Konbini.KonbiCloud.Core/Machines/DetailTransaction.cs
+++ aspnet-core/src/Konbini.KonbiCloud.Core/Machines/DetailTransaction.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41f4c0d38cf505dd0bcf60a22cc0cbb188f1799a4d36fa5f43f7a77df235459c
-size 3844
+oid sha256:eb27b67a0fd60d2ee6995f9d8c48291ee480e67e842e02ce2fbdbae48b487a7c
+size 3886
