1d1c66d8d91d8330486abd21da7882ea4f4d1496
phuong <phuong@konbi.cloud>
2019-01-04 16:08:35 +0700

    add 3 images to cloud

--- V2/KonbiCloud/angular/src/shared/service-proxies/plate-service-proxies.ts
+++ V2/KonbiCloud/angular/src/shared/service-proxies/plate-service-proxies.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe22febb79831352a2d3fd0c17f42679f00f77829ecb9621f42dc6472008f5e8
-size 25918
+oid sha256:7658880d34d536e9e839b65cc1785cd4781f5b2b3c417bbbb56e04a97b8c2ff4
+size 26689
--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Plate/PlatesAppService.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Plate/PlatesAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:080ee578d4f40b72535671c38c266aaeee355a594899b81c5ed2ad60f30d3354
-size 15556
+oid sha256:1587228d3b9531b2f5c15337653864d7854bb8a0e528816daaacdf056d8ddbb8
+size 16745
