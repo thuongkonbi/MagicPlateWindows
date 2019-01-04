48d90ab535f233602f2b4530dda83c98b3fc50ac
phuong <phuong@konbi.cloud>
2019-01-04 14:43:44 +0700

    upload 3 images plate

--- V2/Konbi.MachineBrain/MachineAdmin/angular/src/shared/service-proxies/plate-service-proxies.ts
+++ V2/Konbi.MachineBrain/MachineAdmin/angular/src/shared/service-proxies/plate-service-proxies.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e018ba5c31140ecfad3d2a18db3cf3bd94790ba5c51ea830ceea0a0f1226ef2
-size 28568
+oid sha256:ce360fc73080a5d57fcce59dbb5c484869438568ad59e32723520a9837f3f60f
+size 29340
--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Plate/PlatesAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Plate/PlatesAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c968a8c7355daa0738beb28cd0a8aaaa1aa3f54aebb7249de930fdbc4c78a15e
-size 16136
+oid sha256:5debff797663b751fec4aa4495337982f205532488af07b15cdcf60ab7b66a70
+size 17641
