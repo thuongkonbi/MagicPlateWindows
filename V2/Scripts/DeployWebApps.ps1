5a5760505450cb415604327bd0cea7a09d55a214
Long Le Hoang <longdt@live.com>
2019-01-08 18:20:49 +0700

    Add auto deployment script

---
+++ V2/Scripts/DeployWebApps.ps1
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:958eaa646a4dce1c58cf1e042d31e6cd46ccc1110fe8253da9a61ed1c303a57c
+size 904
---
+++ V2/Scripts/Pack.ps1
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:047057dbd52523f1388cb011e2d8e652d3e3b2a45648a09e353d24228bd5bea1
+size 2239
