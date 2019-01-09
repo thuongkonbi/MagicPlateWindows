9ee35cd306b37e6cf7ecdd88f4f67de552f45820
phuong <phuong@konbi.cloud>
2019-01-09 11:44:49 +0700

    fix import plate

--- V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/plate/plates/plates.component.ts
+++ V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/plate/plates/plates.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9cce78974874f1ec297f56d19e1d0675c7cd58b328f3fa6f45af089b8b436cb
-size 11851
+oid sha256:28db6d37d6572da61199ce7c4f6e39743acbefee7642573273aff7f07f0b11f2
+size 11808
--- V2/KonbiCloud/angular/src/app/main/plate/plates/plates.component.ts
+++ V2/KonbiCloud/angular/src/app/main/plate/plates/plates.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f865070e54d8b649d0c60c3e05ecd988218f536617965d302d39fee001ade211
-size 11417
+oid sha256:2eb7e8fb2fd91f9b709dd10f63de42840b6228939ea1b7feee255f1b67814f7f
+size 11405
