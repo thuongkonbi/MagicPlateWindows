c71dbc86167e201e272b444acf6d461758412abd
Long Le Hoang <longdt@live.com>
2019-01-09 18:31:22 +0700

    Initialize TableSettingsManager on Startup to get NSQ listener ready

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Web.Host/Startup/Startup.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Web.Host/Startup/Startup.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2124b03976db28529d69afb7affa523605b657c615ad5f40f25759f6922f8e8c
-size 8455
+oid sha256:1244a81240d619e4f158dbe29d741f80b79884b89a824b429b51522cdebe2ace
+size 8639
