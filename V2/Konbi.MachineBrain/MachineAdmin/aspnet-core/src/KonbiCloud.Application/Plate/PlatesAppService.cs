932a544b72e780272c0944042ad2c16dc08ac685
ha <ha@konbinisg.com>
2019-01-11 10:32:14 +0800

    add log

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Plate/PlatesAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Plate/PlatesAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:106e37ee26de865b4e576879e969cf19ee167ff2a02be9a2d4f1d7778b42628f
-size 20253
+oid sha256:04a46bebd634f8a22ee3c766958d9b9e300d893db195277c2070f734d930cd5f
+size 20380
