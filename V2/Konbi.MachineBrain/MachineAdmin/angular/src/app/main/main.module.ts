e3787c37aeb6e92380abcdc5f5bb26ec58d6c9cf
phuong <phuong@konbi.cloud>
2019-01-14 11:25:14 +0700

    realtime search plate, show inventory menu cloud

--- V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/main.module.ts
+++ V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/main.module.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6faff0541ecf4f9f47ae13b2dea48bb59eace93b026ed6a783c850b78084e723
-size 5102
+oid sha256:8d919e0c3bd14dea79ae75dbcb753b7e1dcedcd1abd093d1042e7acc60134361
+size 5182
--- V2/KonbiCloud/angular/src/app/shared/layout/nav/app-navigation.service.ts
+++ V2/KonbiCloud/angular/src/app/shared/layout/nav/app-navigation.service.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:970c8a0387fc09db8773e0af3708fde3943bf62337f0c5097d55952c7c353840
-size 6702
+oid sha256:395ac6b06c88608a06aca09f514c4e52a1b852aa7dd034b5e83689b7d7c8138a
+size 6693
