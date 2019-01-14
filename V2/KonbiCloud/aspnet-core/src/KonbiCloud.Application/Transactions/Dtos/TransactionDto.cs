de7378c4d1593bf95f36c63ee369072d8e86ca0b
Long Le Hoang <long@konbi.cloud>
2019-01-14 14:41:39 +0700

    adjust GST calculation

--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Transactions/Dtos/TransactionDto.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Transactions/Dtos/TransactionDto.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f343d57f61a1c7cd8e384d4be23e6fe1289db184ba3291183aa7ee26bc39d57c
-size 1944
+oid sha256:f3506e1e1a2ce7fe81e9540120a2de084bcb199d664b7f4f9e973c913b59f5ac
+size 1972
