1a7d32366cdf56d8a2adb8c63131304cd2de03c0
ha <ha@konbinisg.com>
2019-01-16 14:49:55 +0800

    add columns to detail transaction

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.EntityFrameworkCore/Migrations/KonbiCloudDbContextModelSnapshot.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.EntityFrameworkCore/Migrations/KonbiCloudDbContextModelSnapshot.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:218a620d3dc6fff7ebde0dc0929bf85e775f9dad2c4eb7b98cc9b8613e7b162c
-size 68292
+oid sha256:63752a1707db211b6e5d51ab41e52e759ec174b113f3b0381f4ebee7706e4f3b
+size 68462
