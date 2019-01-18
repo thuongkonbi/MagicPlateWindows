c7babc3594d1746bb4b212dacbca2cfaffdbd524
ha <ha@konbinisg.com>
2019-01-18 16:16:07 +0800

    fix bug transactions + organize log

--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Transactions/TransactionAppService.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Transactions/TransactionAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4d0cad165ede5aed9ebfb2156639d3f759b4a3eeb4b7ac3d5d6e11f2a71200e
-size 8206
+oid sha256:5d4fb200813e3f11b87146f2b4156f35ede98dab14392383b1fc64aa234b66d5
+size 8306
