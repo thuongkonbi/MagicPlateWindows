f351dfef4aced2b0af9d63ecfe4c13e7be22b604
Do Ngoc Tu <Tu@konbinisg.com>
2019-01-18 09:16:00 +0700

    Add log

--- aspnet-core/src/Konbini.KonbiCloud.Application/Machines/TransactionAppService.cs
+++ aspnet-core/src/Konbini.KonbiCloud.Application/Machines/TransactionAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b32619898d9e2d98e0dea7a2cd0a5b9d579b44b734e741e5cc5559e43456ba55
-size 15003
+oid sha256:47f3927ae812518241c1a9c69eee5a42cfa4f20be9fcef0e166096d13f4a59db
+size 15046
