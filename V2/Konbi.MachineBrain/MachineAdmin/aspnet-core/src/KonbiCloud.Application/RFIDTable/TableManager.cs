68dcda5b728d145dd51948bc94c9d7e841159027
Long Le Hoang <longdt@live.com>
2019-01-18 16:21:26 +0700

    ok

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3aa2a0b5bb228866d4ea71ae9b2b81f88dfe6f6d30dcf36913ab3a25f534174b
-size 15203
+oid sha256:7f063c5c17b4901c7f8211ded270d2d8a549ecb825c36b0702fb749c476d35ed
+size 16803
