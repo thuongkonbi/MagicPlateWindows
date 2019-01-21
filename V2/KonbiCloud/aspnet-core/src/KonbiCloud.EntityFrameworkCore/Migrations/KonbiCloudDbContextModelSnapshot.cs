f38ffd8098d9c2e236196ec1c172ea2faf9dce59
Do Ngoc Tu <Tu@konbinisg.com>
2019-01-21 11:08:10 +0700

    Add image to cloud transaction

--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.EntityFrameworkCore/Migrations/KonbiCloudDbContextModelSnapshot.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.EntityFrameworkCore/Migrations/KonbiCloudDbContextModelSnapshot.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9dc1a092d7cc6f50e807343978707db481a4e1319671e7bd949c54565dad18e
-size 86327
+oid sha256:7a3eb1582c8dc651bee15235ec11ede07bf88a31c92e258faa875b2d9b17a4fa
+size 86497
