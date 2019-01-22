41700720719f3da5933f5b97dfde82a9afb195a6
phuong <phuong@konbi.cloud>
2019-01-22 16:27:18 +0700

    fix dashboard for machine

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Core/KonbiCloudCoreModule.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Core/KonbiCloudCoreModule.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e9c731018d78789b2d278ee25a6a1d91fc0760243e349584d1f8a184f629f31
-size 5123
+oid sha256:560375f7dacfc7afd1a0feab69d4e45f7e014e266a5ebea204eb4028a05f59ad
+size 5132
