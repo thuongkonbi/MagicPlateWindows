f3673dfe9a28aad9ff1953e0b5cad7ba1c94bc04
phuong <phuong@konbi.cloud>
2019-01-22 17:51:59 +0700

    fix dashboard on cloud

--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Dashboard/DashboardAppService.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Dashboard/DashboardAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86da6dd4ba2cc30b5f4d99600cba1304456333b5cb2c5dbeb2ef957e6d6f4d5d
-size 30037
+oid sha256:7ccf0db5931c4fc8d50b3925c875bd55f67bd760af9e60348dbb2072a94b3217
+size 30122
--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Core/KonbiCloudCoreModule.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Core/KonbiCloudCoreModule.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87846d37ef8eb090d4fe4c6bac94259e7adbad0a339e0bf0ad71663d17563074
-size 5135
+oid sha256:e2116cd73d21d8caeafdda0807d09352022520f491328faf615e4217edd40b21
+size 5754
