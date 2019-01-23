bbff5bf68b309e96672dfd1d4f9ee11d2929154a
Do Ngoc Tu <Tu@konbinisg.com>
2019-01-23 09:13:04 +0700

    Modify transaction image

--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Transactions/TransactionAppService.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Transactions/TransactionAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb0f7f91f9789ecc07be70dc86ca31fea51a5efe08c85b33beea894e0cad3e4d
-size 9523
+oid sha256:84ec379ce2e11b728faa395258e36797bb5407dfe937c7c19a5c89b34ee89482
+size 10384
