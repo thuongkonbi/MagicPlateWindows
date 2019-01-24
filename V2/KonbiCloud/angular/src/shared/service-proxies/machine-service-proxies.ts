ad9dd53bb587eec732bef0b39661e789634afc1d
phuong <phuong@konbi.cloud>
2019-01-24 15:18:05 +0700

    add permission for machine

--- V2/KonbiCloud/angular/src/shared/service-proxies/machine-service-proxies.ts
+++ V2/KonbiCloud/angular/src/shared/service-proxies/machine-service-proxies.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa9da221b6c1941001a544fcc3eed688aa9a816725052b4aea905aa521b982fb
-size 60772
+oid sha256:d0a0ee52db63524a4d70f874aac058be96d2598087e90c9cc09466cdf51aa350
+size 60779
--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Machines/MachineAppService.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Machines/MachineAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41efca5e7405680dccacc1897e3d4e562c87e046750aaf4237c3998f1d68a83f
-size 25174
+oid sha256:0886adc4fea989d52679e4b928dea1390002284618023e307d627ab01f544c53
+size 25170
