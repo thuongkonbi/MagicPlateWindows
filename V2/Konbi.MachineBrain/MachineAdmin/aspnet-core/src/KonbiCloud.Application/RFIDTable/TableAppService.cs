33b2f948b8b2df2cf4f4e17a2696a31e11e24c99
Do Ngoc Tu <Tu@konbinisg.com>
2019-01-25 10:29:55 +0700

    Uncomment code

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e74bedacaf660270d38b81fa6cf65c5809272de498d47aca3819349bcb8dc69
-size 11713
+oid sha256:0d5ed66aac743de2048dde1e9df8ca3855201dc910bebf89d011aef204c021fc
+size 11621
