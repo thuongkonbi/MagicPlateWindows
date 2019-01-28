936fb0414d0e428fc1d2d58a9418aa4752ad2214
Do Ngoc Tu <Tu@konbinisg.com>
2019-01-28 13:47:15 +0700

    Fix bug

--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/PlateMenu/PlateMenusAppService.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/PlateMenu/PlateMenusAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6638c5a920deada0a5f5b348a1ca34601044edfc415f96782032bdb368b7de5c
-size 35490
+oid sha256:c9246d1ad6ba905aff70195c9ea02903196d8a1a4e5e364a0fd56b74fddc45f9
+size 35497
