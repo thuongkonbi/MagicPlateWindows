907395518b814f96fc58d2868cc67d58dd9cf5a1
Do Ngoc Tu <Tu@konbinisg.com>
2019-01-28 16:47:32 +0700

    Modify transaction

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Transactions/TransactionAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Transactions/TransactionAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1131016ee59f55dcf306ef1ad8738496f43e5f33824904b1b9afc0b6dc5b119f
-size 6390
+oid sha256:05ada57511f70476d8c2259a57fbcc1be095ae2f0c5b6dc24dcb55b7f28a9463
+size 7022
--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Transactions/Dtos/TransactionInput.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Transactions/Dtos/TransactionInput.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:110cf0be3b54156b6d43dcc035c57d5e816de95180b95ba1332b6f16edc8e6a2
-size 407
+oid sha256:008027152b030c3857ed93b903a735e36c429b9f41ca5680abca69dffd24a885
+size 494
