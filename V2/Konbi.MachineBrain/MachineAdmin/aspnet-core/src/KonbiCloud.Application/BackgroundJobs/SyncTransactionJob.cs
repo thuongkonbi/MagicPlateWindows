8864215574b84d2957f68d4a829ea624133fe681
Do Ngoc Tu <Tu@konbinisg.com>
2019-01-29 09:58:51 +0700

    Fix bug

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/BackgroundJobs/SyncTransactionJob.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/BackgroundJobs/SyncTransactionJob.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7666991abc49590eb485bc520c591dfea854cca4810e307e66a3224b7f2933f
-size 8259
+oid sha256:381d1a44b4c74e1ebde9421635e625568f0272001dfd9935c8a12edebf962e81
+size 6347
