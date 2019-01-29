1717049edcb1a60e59bf56edad8abef50d2379ac
ha <ha@konbinisg.com>
2019-01-29 18:03:21 +0800

    add rabbitmq listener to test

---
+++ V2/KonbiCloud - Demo/aspnet-core/src/KonbiCloud.Application/BackgroundJobs/RabbitMqListenerJob.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d9cd54b6b99dd9cc3975c3da45acf990ff0626d2b989db5ed802e1960481c76f
+size 2487
--- V2/KonbiCloud - Demo/aspnet-core/src/KonbiCloud.Web.Core/SignalR/SignalRMessageCommunicator.cs
+++ V2/KonbiCloud - Demo/aspnet-core/src/KonbiCloud.Web.Core/SignalR/SignalRMessageCommunicator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf33f7c997c83f96b618e552cccdee0f932d440f5663fd845b3b6d0931617586
-size 4877
+oid sha256:36d36cc8896d335e0b98d35765b83393a4bc73bca8f5882a72c04a6ccc74f4d6
+size 5285
