191e7203c0c750733e265f294e3543d11d5945ef
phuong <phuong@konbi.cloud>
2019-01-29 17:52:29 +0700

    getting started

--- V2/KonbiCloud/angular/src/app/main/get-started/get-started.component.ts
+++ V2/KonbiCloud/angular/src/app/main/get-started/get-started.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd1ae1f61639000935350720de114b18fc07045ff33fb91b81ac61cea588b240
-size 1183
+oid sha256:2e25252adaf1f87a839c6fa08743dffaafc5787b9533348bd8967992c06d5edb
+size 1693
--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/GetStarted/GetStartedAppService.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/GetStarted/GetStartedAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a54891a4c9fa18443f55458a4cdd469e0005927cc165adf8a7ae11f096a235f5
-size 4795
+oid sha256:84d7fad282150f40cd5abb9e7f59df93f8f6ebd54760448bfca74b4d279ce74a
+size 5170
