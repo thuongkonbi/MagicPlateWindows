38c0a308b72d75940d8f33e97cae178a59daaaa2
Do Ngoc Tu <Tu@konbinisg.com>
2019-01-30 11:32:04 +0700

    Fix bug

--- V2/Konbi.MachineBrain/Devices/Konbi.Camera/FormMain.cs
+++ V2/Konbi.MachineBrain/Devices/Konbi.Camera/FormMain.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:420d31f9df4c3a3ae7ced2b2aaf41130ac7b049caa92a4b34c1e25f60e847bf3
-size 7458
+oid sha256:efd61aa010e3c5843996992881efa77ca3e9d498baf21b79f688694e9efbb888
+size 7414
