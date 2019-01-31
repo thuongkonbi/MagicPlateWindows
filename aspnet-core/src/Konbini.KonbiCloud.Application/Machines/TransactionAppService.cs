9dae66ff09d607fa593c015cffe0af766c98868e
Do Ngoc Tu <Tu@konbinisg.com>
2019-01-31 11:03:51 +0700

    Try catch add transaction

--- aspnet-core/src/Konbini.KonbiCloud.Application/Machines/TransactionAppService.cs
+++ aspnet-core/src/Konbini.KonbiCloud.Application/Machines/TransactionAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bcb95a49f27c95043c7d0e55d34c104803fbfbb036360067e41d99e5ecb9169d
-size 15157
+oid sha256:3ed8811d10212a4773aa3345bbce8bb64ab420a663ad627dfe05814e68beedda
+size 15404
