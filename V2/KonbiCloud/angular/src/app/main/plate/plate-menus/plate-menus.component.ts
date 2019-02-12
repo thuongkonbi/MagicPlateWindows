dd464f91ccb7bb56bcca342c32ac65a364269d71
Do Ngoc Tu <Tu@konbinisg.com>
2019-02-12 14:38:45 +0700

    Fix bug

--- V2/KonbiCloud/angular/src/app/main/plate/plate-menus/plate-menus.component.ts
+++ V2/KonbiCloud/angular/src/app/main/plate/plate-menus/plate-menus.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0364d0e8d4e2b2db0b691b35ee1df66601a7bad7aac08723f28ea683ee9676c
-size 14217
+oid sha256:3384fe0a6c9921f1ca0894772ef1ddeb6382fd74e09effc26d400e9160881181
+size 14219
