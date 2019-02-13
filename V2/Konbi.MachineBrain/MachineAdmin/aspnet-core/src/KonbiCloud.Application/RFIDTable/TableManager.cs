8e5b19bcd7e9f49297eb94eea82d48915007c99e
Long Le Hoang <longdt@live.com>
2019-02-13 12:09:04 +0700

    Fix issue regarding to  Customer Message is not auto resize properly

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:04a04012b65ffde841ea74b4110c5114781fffc00cd580cd996b4356d1d2091c
-size 18507
+oid sha256:256e627ce3f186afc06d2d871dcfeae307ec92daa98139f0c6e52efe66ec4fa3
+size 18508
