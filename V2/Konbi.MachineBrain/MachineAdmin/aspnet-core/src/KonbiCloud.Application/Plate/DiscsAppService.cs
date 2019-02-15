7ef8bcbb6977228abe8817400b1a23d36efb3ecb
Do Ngoc Tu <Tu@konbinisg.com>
2019-02-15 15:36:05 +0700

    Fix bug

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Plate/DiscsAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Plate/DiscsAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4db844eace3a9c237460712321cae2480b187a130019b69902fde5d88d5306d
-size 18124
+oid sha256:7d00a13eb7c6d6adae9d577d85c71d2560006d38e52c2a3479f1a845aabac8ee
+size 18470
--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Transactions/TransactionAppService.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Transactions/TransactionAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:763ebea9ffbeb54e663fc10c7989c5ff3d5158dc35ecdbb9d2b1e75c1288be9d
-size 11373
+oid sha256:306e0efdd5d3dc79ae6344f90aa8b1d2d42cc2a719a369963e41081329628dd4
+size 12209
