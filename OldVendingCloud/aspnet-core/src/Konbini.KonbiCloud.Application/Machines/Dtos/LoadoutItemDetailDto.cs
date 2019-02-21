181a22d02935a2d777615e9019fc665b217d17d7
phuong <phuong@konbi.cloud>
2019-02-21 17:05:20 +0700

    fix

--- OldVendingCloud/aspnet-core/src/Konbini.KonbiCloud.Application/Machines/Dtos/LoadoutItemDetailDto.cs
+++ OldVendingCloud/aspnet-core/src/Konbini.KonbiCloud.Application/Machines/Dtos/LoadoutItemDetailDto.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7df710e0fac8ac25680186a89b74b0417ea40a0f978e64ad2b07a72493682aca
-size 375
+oid sha256:1201add35dc2c2b9b282e9d26946363dfd95373d7e2a6dcc58638491ae5d58bc
+size 377
