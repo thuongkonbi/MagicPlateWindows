ada436725b3962170dccd36e91ab286364f1480d
TrungPQ <trungpq@konbinisg.com>
2019-02-22 17:11:46 +0700

    Edit coding conventions.

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ae2b64dcb18eb6b0af162d3d5399720daf07e5575ed34098e7a9dc1b06e3076
-size 25947
+oid sha256:60c376b4146783f2e4fd75d2df6be775b3c663a7e5b291f1bbe6a58cc4d6ae60
+size 25782
