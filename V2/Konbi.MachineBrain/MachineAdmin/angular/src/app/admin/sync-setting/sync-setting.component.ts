a09464c5e2b555f5ce1a84cd99f8c4adb919d0e0
TrungPQ <trungpq@konbinisg.com>
2019-02-26 16:52:24 +0700

    Should be numbered for sync steps.

--- V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/admin/sync-setting/sync-setting.component.ts
+++ V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/admin/sync-setting/sync-setting.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6e7912127689c3a559d3685b1e8da0515c4324adfdf4b2a78c96409b722445b
-size 3977
+oid sha256:92420453dc9088db7fb814d8176dc6671123c65c2464fa6ec4b3e4a2c2a622d8
+size 4285
