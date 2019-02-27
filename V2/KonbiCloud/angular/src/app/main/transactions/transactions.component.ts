5c4f0de36a5c368e5b462899b0dacc74485fa62a
TrungPQ <trungpq@konbinisg.com>
2019-02-27 15:27:20 +0700

    Add transaction export => for cloud

--- V2/KonbiCloud/angular/src/app/main/transactions/transactions.component.ts
+++ V2/KonbiCloud/angular/src/app/main/transactions/transactions.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d065a9f1a9e19b519758902f0801cdfc82efea5b2741b29fce83daee61f46828
-size 3955
+oid sha256:d81605cafc0ee5221fab9ac314e11f57ada15c5346e900d39bf2e1d05646335e
+size 6265
