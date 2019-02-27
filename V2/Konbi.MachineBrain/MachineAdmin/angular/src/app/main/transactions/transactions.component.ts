f62adec52087c0b4ed37b60f24e524317820fa55
TrungPQ <trungpq@konbinisg.com>
2019-02-27 15:07:21 +0700

    Add transaction export => reformat.

--- V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/transactions/transactions.component.ts
+++ V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/transactions/transactions.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2bcd1c174319bc3f7f871b83fe651e9e8271d8ae167d632901357c643849af86
-size 5589
+oid sha256:43f01369e31350a4164e853dcbc4df9cf363e7741ad4a5626c70b4c023baa51d
+size 5577
