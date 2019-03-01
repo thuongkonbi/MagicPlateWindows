46c45feedbb5e6293f32ee10c56b731a2734d5ae
TrungPQ <trungpq@konbinisg.com>
2019-03-01 12:08:07 +0700

    Fixed: error while import plate model.

--- V2/KonbiCloud/angular/src/app/main/plate/plates/import-plate-modal.component.ts
+++ V2/KonbiCloud/angular/src/app/main/plate/plates/import-plate-modal.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5d14533dd78c6c3dfc96d8c5fd1013242025c9c306dee6a90cfbeebd0c1aadb
-size 7486
+oid sha256:e15be10944dd62fea16e484af67839aa0f75e21af52c433580bc32929ed1c9dd
+size 8284
