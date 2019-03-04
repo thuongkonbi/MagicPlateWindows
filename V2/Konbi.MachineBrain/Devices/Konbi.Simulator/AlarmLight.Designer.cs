a4784ae8b0ab522b5d18c8fb5cf5136be1453468
phuong <phuong@konbi.cloud>
2019-03-04 17:24:01 +0700

    fix build

--- V2/Konbi.MachineBrain/Devices/Konbi.Simulator/AlarmLight.Designer.cs
+++ V2/Konbi.MachineBrain/Devices/Konbi.Simulator/AlarmLight.Designer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82f27698a9a8b591b7e3bc869ed27b1cb4cfa9dd68647f6216ab6e533cab8ab6
-size 12420
+oid sha256:5d60ef328513a9d01026b9ea05ed5542b4d5aa2170081cc3fe6631b6058c804e
+size 13809
--- V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowServices.AlarmLight/Services/SerialPortHandler.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowServices.AlarmLight/Services/SerialPortHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f777ff17c408a3cbce279f4d55a7aa16dce75a368cbadc2597737e302d4a421f
-size 5478
+oid sha256:d49e04b3da03827cd4e8518dd4f538410c94eb7ebbc6c8758b3dc71a6cc51a71
+size 5481
