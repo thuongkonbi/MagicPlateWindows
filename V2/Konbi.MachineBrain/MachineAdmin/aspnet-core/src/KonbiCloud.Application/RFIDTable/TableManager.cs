9d54b837911892c36b9a9e4f5a6074219df12691
phuong <phuong@konbi.cloud>
2019-03-05 17:17:23 +0700

    update error handler

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df71e92343dcd77190c42b73e44b734613e31c01962db66a936e8ad1c5f72340
-size 29565
+oid sha256:9c6ce8422f46e7dfb1d9f8051dc9acd5005a4c6553263f4fbc45164bd5ee5eac
+size 30376
