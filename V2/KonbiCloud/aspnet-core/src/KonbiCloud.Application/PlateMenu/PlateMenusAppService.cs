c6b1c6f0c0804296fd9d9ad9a78d32efdd33c2d8
TrungPQ <trungpq@konbinisg.com>
2019-03-06 17:04:24 +0700

    Fixed: display total wrong.(Cloud)

--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/PlateMenu/PlateMenusAppService.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/PlateMenu/PlateMenusAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5289f2e51d65909f05fdf5d58f3854c5f5bb35c5976d299c8bed0942e5a32afb
-size 36471
+oid sha256:62b99d43cfde142352273a2927da8cb22f555c69c2a62cca7f64a7c9f7c4c858
+size 36531
