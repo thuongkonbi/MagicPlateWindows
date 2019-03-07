f5bd12394f18611117a4c937b795df5071d83be2
ha <ha@konbinisg.com>
2019-03-07 13:04:47 +0800

    udpate code

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePrice.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePrice.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fce51cdc7aa68914b86a1bce068352704f5f1527e2996aa8d65e5b9ab6a54e7a
-size 27972
+oid sha256:98bfd8513fbb2901184c9cae4c66f1953ada00c6c051cc8d81a1bab814851847
+size 28119
