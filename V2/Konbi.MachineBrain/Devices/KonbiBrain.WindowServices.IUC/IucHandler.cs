ad8655f537d3310bc73ef8f25db4f5ef079c78a9
Long Le Hoang <longdt@live.com>
2019-03-07 17:40:01 +0700

    IUC: Override messsages for ressponse code: IC, IN, CO, PL

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowServices.IUC/IucHandler.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowServices.IUC/IucHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa4ec0635f65bc82dcca5b33ef6795edbe1b904cad4a902fb08e4c9dbc80c202
-size 9528
+oid sha256:e6cda861f444a208ba3ba0c87668a67b814ed35d6a0e680242a268acef6b9e5c
+size 9809
