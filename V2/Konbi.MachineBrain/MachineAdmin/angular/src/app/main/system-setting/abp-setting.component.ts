675d59c59bc3705b0667dde4d6ddc763e9921e23
TrungPQ <trungpq@konbinisg.com>
2019-03-07 11:15:30 +0700

    Sync machine name.

--- V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/system-setting/abp-setting.component.ts
+++ V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/system-setting/abp-setting.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bbbdda537cd3d5184dd3cc7395467edadd0e7caa21afd5cfa2e9cfc1464e0ad9
-size 5561
+oid sha256:3ab3ede0c3af2e5cf4077a31f8834bb74b96234fa786de4846d057d582b46845
+size 5621
