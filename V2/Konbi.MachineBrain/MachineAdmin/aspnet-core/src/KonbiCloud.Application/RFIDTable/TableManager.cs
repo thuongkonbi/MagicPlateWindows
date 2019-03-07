52b0a52bc25c375eef41192c5578354b3099a265
phuong <phuong@konbi.cloud>
2019-03-07 11:54:13 +0700

    update light alarm

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a103df83c7c78dae367dc6449720c89af967bc584ba6bb3a8edeb4ec2965c4d9
-size 31568
+oid sha256:a5d7bd05b633e93a8be93ec21a132df0a4ac2e1d39f43c538fdc2b9ae6341ea1
+size 31480
