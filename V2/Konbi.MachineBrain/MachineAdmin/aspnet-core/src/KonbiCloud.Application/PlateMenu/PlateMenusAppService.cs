08b62ab1248adeb2514b400b4765dfe7ae956506
TrungPQ <trungpq@konbinisg.com>
2019-03-08 11:42:46 +0700

    Fixed: display total wrong.(Local Machine)

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/PlateMenu/PlateMenusAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/PlateMenu/PlateMenusAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66625042d8838d3af77320fa3e7872f2e3cbc99362bfafefaf92942336fc1b53
-size 33684
+oid sha256:452f0903a2e8475d081db38b335d3d0bb2c920dbfdbe520fc70ba250716ad091
+size 33744
