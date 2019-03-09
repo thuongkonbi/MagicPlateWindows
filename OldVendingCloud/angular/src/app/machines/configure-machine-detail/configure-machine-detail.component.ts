bd9956d3180e3a3b6cb08b1719ef501993871c16
Do Ngoc Tu <Tu@konbinisg.com>
2019-03-09 17:27:41 +0700

    Fix UI in machine loadout

--- OldVendingCloud/angular/src/app/machines/configure-machine-detail/configure-machine-detail.component.ts
+++ OldVendingCloud/angular/src/app/machines/configure-machine-detail/configure-machine-detail.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:316630f3cfcb6ef2b97e8fdfacd334d8edbd1fa61152ab0a8e2c831a44f6289c
-size 13772
+oid sha256:2ee98747685c511baad99b939acda4e3ff6e24e911e7180010da80dc0b28cf22
+size 14118
