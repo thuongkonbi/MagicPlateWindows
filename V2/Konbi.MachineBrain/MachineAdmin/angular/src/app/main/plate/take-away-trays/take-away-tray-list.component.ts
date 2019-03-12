d8755d90d56df2e2ba362292ee924ed434fca0fd
Bành Cao Quyền <quyen@konbinisg.com>
2019-03-12 23:37:42 +0700

    TAW tray feature

--- V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/plate/take-away-trays/take-away-tray-list.component.ts
+++ V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/plate/take-away-trays/take-away-tray-list.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02f1a2760f3c2e5cde31fe333e13a30d77288381f973e67f13110aa8d7edeb52
-size 1360
+oid sha256:b4729b5205a22171b3401c1a5b385fb3b34ef4906068991c1d0610e5f670cd3d
+size 3243
--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/CloudSync/SaveFromCloudService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/CloudSync/SaveFromCloudService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a37afd62f02d6301835511f8f4cc1eae41fb5e6daf419d725222c6738c06a0f0
-size 23444
+oid sha256:8e3a1c50e6a13f3a915c63bd98713a3f75b3d5efbc4202380bba93b70f21602c
+size 23324
