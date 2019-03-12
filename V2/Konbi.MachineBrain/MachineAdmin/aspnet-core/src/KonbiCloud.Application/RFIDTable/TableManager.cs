0a0fc9d222093c135065ec1510bc07b4c8ca5eee
phuong <phuong@konbi.cloud>
2019-03-12 16:37:27 +0700

    change ScanningStableAfterMiliseconds config

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96d5851348d328b54f73f4bca7a963042460518299a7c43f75aae27a9dad3fd2
-size 33418
+oid sha256:3a014f52289ee201fb8d3ba8722b5fea24954907d71947906e249b64e0ee0dc5
+size 33423
--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Core/Configuration/AppSettingProvider.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Core/Configuration/AppSettingProvider.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5ba526a2f5b8d83070fad14edc77da681467b59ee4f4fdc6ffb3232f6236c54
+oid sha256:e8e50ad40e01cd905fbf2b70b2b2bbd11d7a39f06a41bbe0d538c9d8d3d0b382
 size 10509
