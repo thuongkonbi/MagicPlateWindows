44268d4df1d7d415b71949c4aa0cce6abcd316b0
Long Le Hoang <longdt@live.com>
2019-03-12 10:19:50 +0700

    upper case for "please tap card"

--- V2/Konbi.MachineBrain/Devices/Konbi.Simulator/MdbCashless.cs
+++ V2/Konbi.MachineBrain/Devices/Konbi.Simulator/MdbCashless.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3759d8502ead84a29d10afcfcd856beaabe0eb208044780f93dee20a9e89947e
+oid sha256:d6680c2c0336960cce9c660ba82fb4b2ba6ea63ac4f6b47d2ad96833bfc4121a
 size 5799
