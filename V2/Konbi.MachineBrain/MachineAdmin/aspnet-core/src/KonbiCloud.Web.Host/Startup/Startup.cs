a1ec930f592569a512266dbdc79a4f19017db2d9
ha <ha@konbinisg.com>
2019-03-13 13:55:13 +0800

    add info to swagger

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Web.Host/Startup/Startup.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Web.Host/Startup/Startup.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c11b0b3b76cbda6a9b84bf761005a912de52945e6b7bd9f730ff342a309912a
-size 9093
+oid sha256:effcae2bcb62ec8ff240e00fbda518148235c065de30a51b5c56a4c443128bfb
+size 9627
