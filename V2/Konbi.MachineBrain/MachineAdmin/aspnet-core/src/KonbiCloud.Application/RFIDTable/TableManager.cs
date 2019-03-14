adddc9c6fd42f1606bfff3327554925c2f30ce80
Bành Cao Quyền <quyen@konbinisg.com>
2019-03-14 16:53:51 +0700

    refactoring code Take Away feature

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4776dd1d60e4cef772198a17f710f9c6e87ca3684f3a129b4f8e7994fa3e95ac
-size 35442
+oid sha256:972299d8d240defbadb24f4d727ca2a711394c28cf84b1e223bdd780c00fa034
+size 35506
