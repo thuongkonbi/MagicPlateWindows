32e8dd8fa957997e4e82062807ad36e6fd41cb7b
Do Ngoc Tu <Tu@konbinisg.com>
2019-03-14 13:02:57 +0700

    Fix loadout configuration

--- OldVendingCloud/aspnet-core/src/Konbini.KonbiCloud.Application/Machines/MachineAppService.cs
+++ OldVendingCloud/aspnet-core/src/Konbini.KonbiCloud.Application/Machines/MachineAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:509a8d8a11a531118b64de0da2924f06c38ccfa1030946944cbb91ff7daad42f
-size 33937
+oid sha256:f9531deb6b4becf85715ddcb575e56385399075485dd33f3f147ff01fabe8358
+size 33390
