0d214d02bf9b512ad480d8e6d4398aef881bec10
Bành Cao Quyền <quyen@konbinisg.com>
2019-03-14 15:03:04 +0700

    update appsettings

--- V2/KonbiCloud/angular/src/app/main/plate/take-away-tray/take-away-tray-create-or-update.component.ts
+++ V2/KonbiCloud/angular/src/app/main/plate/take-away-tray/take-away-tray-create-or-update.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dac128a72539ceddc07eb24c65acf8465e57a55f6d42fa2d928e949dad729c7d
-size 2261
+oid sha256:49ac5b782f6407a094eac9525be51fc8627297cb9a0d9ef90c10fd1457a95835
+size 2210
