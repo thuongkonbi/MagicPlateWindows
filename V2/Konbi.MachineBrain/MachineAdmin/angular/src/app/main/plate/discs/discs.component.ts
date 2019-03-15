5bca6f5c4535f155fd4121f2df26d648546e4155
TrungPQ <trungpq@konbinisg.com>
2019-03-15 18:02:54 +0700

    Add loading icon when sync discs.

--- V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/plate/discs/discs.component.ts
+++ V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/plate/discs/discs.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:358ad41c29b694934bfdd86055f727a3885c1dec15b471e197c96cb0bc211726
-size 6613
+oid sha256:d79a6ff4e8e49961abc15ec8695f4ff26fa5c892de84bbb67c78fe8d4aa57daf
+size 6669
