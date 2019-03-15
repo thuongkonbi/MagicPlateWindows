72b47aeb605b8077e77803f102fa193ea2ec0b02
phuong <phuong@konbi.cloud>
2019-03-15 11:07:33 +0700

    fix alarm light

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowServices.IUC/IucHandler.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowServices.IUC/IucHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6cda861f444a208ba3ba0c87668a67b814ed35d6a0e680242a268acef6b9e5c
+oid sha256:c8960439636c63e82581b91fc5979056dd45c6dea7eeef8f812e65a5aec096f1
 size 9809
--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08c07dc65699eacd4d7ed1b928515f01bc0f0c52301cec156e3dc0edbd7e53e2
-size 34566
+oid sha256:a1e98ef91b5df818e0aacb09c16ae2d75bb62c8886970d8e58d16345469b3529
+size 36791
