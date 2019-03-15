5c733b01e0f3e879c2de16fa86c1aa33b85f3e09
phuong <phuong@konbi.cloud>
2019-03-15 18:05:18 +0700

    fix tray

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd702c6da361f3766bf2e00d94c64a8c4bb5602a51133b66b3f6b0ab3a8238eb
-size 36736
+oid sha256:0e5f6b1b7120581b554f0ddb1368a3711a9dec17425b8c2fb022e7e10281de62
+size 37155
