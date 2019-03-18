7c2fb7899409d35e4f4b2153a6999f68509be064
Bành Cao Quyền <quyen@konbinisg.com>
2019-03-18 15:59:56 +0700

    add text-truncate

--- V2/Konbi.MachineBrain/CustomerUI/RFIDTable/js/abp.signalr-client.js
+++ V2/Konbi.MachineBrain/CustomerUI/RFIDTable/js/abp.signalr-client.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0015b9e16c4e6ff5a7ea1784afa504bd9a26506bc1a2797e0edbabb34f9c0e18
-size 9204
+oid sha256:d8a7a0e19477d2068f911970aaed03b0261ce02f662bfe575326e9ee594c321a
+size 9245
