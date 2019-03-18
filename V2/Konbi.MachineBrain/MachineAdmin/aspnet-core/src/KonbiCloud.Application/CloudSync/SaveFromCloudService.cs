eb2141f214fe0bddfd77a0fc1b919a85d9d19af8
TrungPQ <trungpq@konbinisg.com>
2019-03-18 18:17:52 +0700

    After sync data to client machine must auto delete the cache.

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/CloudSync/SaveFromCloudService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/CloudSync/SaveFromCloudService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35d1f72605bb10b6869e26bfcf019edbbd7d10b5062f90cc38a2f0e69576b897
-size 23342
+oid sha256:8b63ceaa3752df9152d19a81dfe4d46b6506f65920538065bad7aeb67d05fc58
+size 24259
