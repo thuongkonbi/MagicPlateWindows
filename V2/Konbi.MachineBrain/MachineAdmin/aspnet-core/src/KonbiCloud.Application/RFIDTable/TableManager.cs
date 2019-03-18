4f53093d945796f592a2a69a3391558a363e8c10
phuong <phuong@konbi.cloud>
2019-03-18 17:52:17 +0700

    fix change plate when lock transaction

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86498873ec99cfc7283eca6b98498cd95a409c501114fad92b4ff9a9e3d417f2
-size 38478
+oid sha256:cd13eed9014dfd75bc00cbd7b1ff7430fdf761c32bc74b7e92eb433b7d35ba5d
+size 38486
