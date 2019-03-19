fc2f92eaff28eb05b6e37fdbbcf11994ce52d521
Bành Cao Quyền <quyen@konbinisg.com>
2019-03-19 16:28:53 +0700

    add session info

--- V2/Konbi.MachineBrain/CustomerUI/RFIDTable/js/abp.signalr-client.js
+++ V2/Konbi.MachineBrain/CustomerUI/RFIDTable/js/abp.signalr-client.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:998daf61284fa7ce2cc16ec2207ff17453d1596c75ce6592ad150944944577db
-size 9547
+oid sha256:a719a621413feedc36d26b75a6900f662130041e158558ad26c24e8d2b04cb77
+size 9557
