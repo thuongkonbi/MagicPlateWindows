3da848a6d15b4bbf6bfbde03f69190db3dfe8edf
Bành Cao Quyền <quyen@konbinisg.com>
2019-03-19 16:26:34 +0700

    add count down & resolve customer UI issues

--- V2/Konbi.MachineBrain/CustomerUI/RFIDTable/js/abp.signalr-client.js
+++ V2/Konbi.MachineBrain/CustomerUI/RFIDTable/js/abp.signalr-client.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8a7a0e19477d2068f911970aaed03b0261ce02f662bfe575326e9ee594c321a
-size 9245
+oid sha256:998daf61284fa7ce2cc16ec2207ff17453d1596c75ce6592ad150944944577db
+size 9547
