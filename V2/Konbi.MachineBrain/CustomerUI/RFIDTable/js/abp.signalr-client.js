bd197dbab24c35ea96c7f46b8411ad556f1810e3
phuong <phuong@konbi.cloud>
2019-03-19 17:39:32 +0700

    update countdown UI

--- V2/Konbi.MachineBrain/CustomerUI/RFIDTable/js/abp.signalr-client.js
+++ V2/Konbi.MachineBrain/CustomerUI/RFIDTable/js/abp.signalr-client.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce25b66a1a8fb929aa06214d86006730563f103da575fe60292eae1323d5d458
-size 9553
+oid sha256:80a421a86ef1e14f9d096c9c77fcfbb97fffd566e8332447528f3ed5e6e56362
+size 9255
