aeb89e2771619a479349869fadc95a9a620381e5
Bành Cao Quyền <quyen@konbinisg.com>
2019-03-20 17:10:44 +0700

    pos mode

--- V2/Konbi.MachineBrain/CustomerUI/RFIDTable/js/app/app.js
+++ V2/Konbi.MachineBrain/CustomerUI/RFIDTable/js/app/app.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31d80647ee008faa3f083603f63bbab866075f6d7b86b90703615f82a9a49cdc
-size 1328
+oid sha256:8db8dca0b73293d4ae3a52b584aba37af093b6ac3b16a35449768b99f3774241
+size 2786
