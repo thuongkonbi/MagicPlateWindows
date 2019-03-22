2b3239d8bf14250719db49cb77c31a24ad148487
TrungPQ <trungpq@konbinisg.com>
2019-03-22 12:17:31 +0700

    Modify UI for Takeaway app.

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePriceVer2.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePriceVer2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8b52e2ee01227c127beb2e26a3270f83a7d499870ddc1e4e5efcd15020ccbb8
-size 41772
+oid sha256:c4226806afe08487fa566e27aaf33a9bfc10553e5c2884f47d5a3fd0b4f781ad
+size 42619
