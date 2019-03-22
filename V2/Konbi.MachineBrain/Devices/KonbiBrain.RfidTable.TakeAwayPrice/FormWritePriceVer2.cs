429272ac0b8659417985c06405ca5f14ce1d1b15
TrungPQ <trungpq@konbinisg.com>
2019-03-22 17:48:38 +0700

    Set disconect for card.

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePriceVer2.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePriceVer2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a3ffa3f8dc326f08360513637701d3ef9dd9741a1d927fb885ea5236ac1c7d0
-size 43286
+oid sha256:dd1cb70873d72bbcad397a6e35c0607e8136d1e91647186d7a46f3d677a36a37
+size 43393
