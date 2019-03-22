bf8bacf5c5a19dc44f0c8fb710f2df0347dc0a3d
TrungPQ <trungpq@konbinisg.com>
2019-03-22 16:41:34 +0700

    Check number decimal point.

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePriceVer2.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePriceVer2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6404afb9951921496bfff48f0ee950674013e9efab094ba0889164f880dde8cd
-size 42840
+oid sha256:6ad26c93e01649d68849a44e1ffacfee8a8ee2e80c6c8159fd578f5c53d708f8
+size 42908
