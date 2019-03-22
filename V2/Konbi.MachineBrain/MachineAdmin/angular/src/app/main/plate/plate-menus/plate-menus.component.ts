33c820b308301d2c9a9e7f2d094ea1ef4f4198be
TrungPQ <trungpq@konbinisg.com>
2019-03-22 16:26:21 +0700

    error while import menu scheduler on client machine.

--- V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/plate/plate-menus/plate-menus.component.ts
+++ V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/plate/plate-menus/plate-menus.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b373526bab84e18be204e9700a036489ab86d3b487f4a3ba536545eb309ce584
-size 15049
+oid sha256:500b64b2071f41046ac9dd37d0ac1342d64875cf26d76931beb2fdb38fb2da84
+size 15078
