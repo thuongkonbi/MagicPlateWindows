46f266d82990f28d694d6a3d62032b7d714da65e
TrungPQ <trungpq@konbinisg.com>
2019-03-22 11:37:19 +0700

    Modify UI for Takeaway app.Add UI messagebox.

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePriceVer2.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePriceVer2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a84acbda7a3e2dfbf68b22a31b172ff92fc86f0cf80b995ed0570046887c2dda
-size 39784
+oid sha256:a8b52e2ee01227c127beb2e26a3270f83a7d499870ddc1e4e5efcd15020ccbb8
+size 41772
