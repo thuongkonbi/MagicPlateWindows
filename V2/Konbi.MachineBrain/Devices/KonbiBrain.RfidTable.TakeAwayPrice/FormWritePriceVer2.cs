08a1bc61a096f012208d59d36a1e50407ed796bc
TrungPQ <trungpq@konbinisg.com>
2019-03-25 14:06:52 +0700

    set sound only new tag inventory

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePriceVer2.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePriceVer2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f19c103cff96b26c596bf45208b358f7178dd2e1b9026d99ed19df813ed021eb
-size 43861
+oid sha256:bb75e1c9204ecd1161e1643d48573328ea742acc6ba988b36ab0b0633ccab746
+size 43859
