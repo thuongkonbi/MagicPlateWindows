ecc55424a6ede6d0b5180cb7242c69f66317d56c
TrungPQ <trungpq@konbinisg.com>
2019-03-25 11:51:28 +0700

    - Remove button close form. - Set mode full HD. - auto set cent.

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePriceVer2.Designer.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePriceVer2.Designer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:568e562901b77dc76e5b339664c27a55cef11a176b8370697e5c1cf5686b0e2f
-size 36847
+oid sha256:0fb7c41dcad0dd2ef42511213d4b8ec36d32d913139603acad32c3e1565656a6
+size 38743
--- V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePriceVer2.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePriceVer2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd1cb70873d72bbcad397a6e35c0607e8136d1e91647186d7a46f3d677a36a37
-size 43393
+oid sha256:f19c103cff96b26c596bf45208b358f7178dd2e1b9026d99ed19df813ed021eb
+size 43861
