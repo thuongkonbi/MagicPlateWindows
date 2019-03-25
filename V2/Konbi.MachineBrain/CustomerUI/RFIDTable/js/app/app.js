cc00e97e57b033c409cc9ff9cf12aa7cb0ca0f85
Bành Cao Quyền <quyen@konbinisg.com>
2019-03-25 10:33:29 +0700

    Revert "fix issues on RFID table - POS Mode feature"
    This reverts commit 8e90a9e75dd0acfb2b6ae130f585c6bab220aa4a.

--- V2/Konbi.MachineBrain/CustomerUI/RFIDTable/js/app/app.js
+++ V2/Konbi.MachineBrain/CustomerUI/RFIDTable/js/app/app.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c7463e03e6e28fc58e4f47ae132232e512cb91bf60c138fdea26f3f9693ae91
-size 6219
+oid sha256:b87c8471eeddad6aec3594f5f5ecce2ea6e2c0462fd6cc4eb8d256f988b9b4d9
+size 6140
