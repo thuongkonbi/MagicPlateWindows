8aaba4130ff67d1da18c1bb258d6980ea8f20a70
TrungPQ <trungpq@konbinisg.com>
2019-03-26 12:37:09 +0700

    Fix issues with case sound a beep.

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePriceVer2.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePriceVer2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1cb6110bb8983d0188389edf8b2dfe641939fd2de3da4ccd5ae395479748e0df
-size 39670
+oid sha256:421bed56f2f15c8f2531bce714affa47099cc433da5ba0bb396b47debdbcac7b
+size 39917
