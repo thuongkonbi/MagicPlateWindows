e8d6a813fc328f086b82d5a519699cb67f6ffd28
Bành Cao Quyền <quyen@konbinisg.com>
2019-03-26 14:18:40 +0700

    extend input price popup in POS mode

--- V2/Konbi.MachineBrain/CustomerUI/RFIDTable/js/app/app.js
+++ V2/Konbi.MachineBrain/CustomerUI/RFIDTable/js/app/app.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:999024e99c833f14eaecfa4c98f019e3600d73dd0ac078c1e7a9a631684e2220
-size 6818
+oid sha256:2c7e62dd5ec1e4a19ed620cbe59202e9a9b76718452f26c09d368af387d14c99
+size 7240
