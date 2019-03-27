0420cdac61e35a43afce6a6ce1cb160722cbeb63
phuong <phuong@konbi.cloud>
2019-03-27 18:16:29 +0700

    add monitor windows applications (camera)

--- V2/Konbi.MachineBrain/Devices/Konbi.WatchDog/ConfigurationInfo.cs
+++ V2/Konbi.MachineBrain/Devices/Konbi.WatchDog/ConfigurationInfo.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e98a3e593909231273530a6ec135166e6d5fe85ef30723d8fce13e57d219d8aa
-size 833
+oid sha256:507aadb053dd9ceaacb28324b03dfec2338118efa39e754014cf62561f5a798d
+size 940
--- V2/Konbi.MachineBrain/Devices/Konbi.WatchDog/WatchDogService.cs
+++ V2/Konbi.MachineBrain/Devices/Konbi.WatchDog/WatchDogService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:04cad56aced430b73a9be2cbcbbd4d07ef8290c9cdae27a5622ec391bff96da6
-size 5328
+oid sha256:38d1e13801945204efc866693a9a8a25b4ffab3a994466d0ab795fed5f9fbff4
+size 6139
