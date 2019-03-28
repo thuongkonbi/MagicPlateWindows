a9def36865e3c94f7c1fa495154d8d37f72b2f65
TrungPQ <trungpq@konbinisg.com>
2019-03-28 15:07:09 +0700

    Fixed the error of unfiltered according to the color conditions on the plate model.

--- V2/KonbiCloud/angular/src/shared/service-proxies/plate-service-proxies.ts
+++ V2/KonbiCloud/angular/src/shared/service-proxies/plate-service-proxies.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d98eaa35558684078e553b4e268a7066ae7b15be47d02d77a59575c48445ca48
-size 28114
+oid sha256:1868b40a47dda39f754a07ca02d74d8bf91b1105b25cc4cc3f306dcdfc202556
+size 28234
