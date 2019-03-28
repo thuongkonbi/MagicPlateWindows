fd216cae0aa57091e8ec6013df83e84d11819694
TrungPQ <trungpq@konbinisg.com>
2019-03-28 14:02:16 +0700

    Create task for listen disconnect card reader on TakeAway UI.

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePriceVer2.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePriceVer2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb3186b0a11d53eee8667e64c2177eb913e5bb0f3b647fa954120b43b4e8f203
-size 41466
+oid sha256:5abadaccc1678ef4c56d783f377d572560617eb797169e425da358996e257e85
+size 42094
--- V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/Program.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/Program.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b5fdc3516cd294d8a091bbb986ef1bf0641bbed83a0e049530e035f1b85b21f
-size 840
+oid sha256:e6dd4583cdc8b1f2e92e119f514f7b2ccd91f5682fa2260e38f345ff282e015a
+size 853
