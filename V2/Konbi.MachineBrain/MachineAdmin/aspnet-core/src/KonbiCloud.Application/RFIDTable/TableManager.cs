4e30e1b58c46d98088398ec3a1d878ce78f07dce
TrungPQ <trungpq@konbinisg.com>
2019-03-28 13:40:07 +0700

    Fix show item when using POS mode in receipt.

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aeb70408bda9c06689c7136cd0fd86749fd75212d6e1941ef0b765328780a0c9
-size 41585
+oid sha256:8e0cf8e4140e67f8bd286e2d8dea833b96a360142af08fabf13dbb3531a5c64c
+size 42576
