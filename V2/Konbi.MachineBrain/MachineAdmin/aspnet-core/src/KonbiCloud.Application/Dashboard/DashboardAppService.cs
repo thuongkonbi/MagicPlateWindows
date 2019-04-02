282f5421569d35de4cc2bc6fd94a01c1417924ea
TrungPQ <trungpq@konbinisg.com>
2019-04-02 18:59:45 +0700

    Fixed show sale summary for today.

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Dashboard/DashboardAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Dashboard/DashboardAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b700b67d457a6a56ce2fb3ed3aa06678016ca4e65310b968fbe6424ca1d991f6
-size 15985
+oid sha256:243abe4e8dbdb178a7b7ba03b600bf0779a964d7785e4bd70ccd4336d8f52edd
+size 15988
