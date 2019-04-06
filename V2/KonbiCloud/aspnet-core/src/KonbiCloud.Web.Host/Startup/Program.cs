a94de1598cf13c2778d2d938e2ac9508b89bc38b
TrungPQ <trungpq@konbinisg.com>
2019-04-06 15:25:28 +0700

    Edit port 22743 for test office

--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Web.Host/Startup/Program.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Web.Host/Startup/Program.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19657b190248925a2dfc96615551ad5e34cbb3601f4bc8c76465824f420860fa
+oid sha256:5a4214dfb6b5511a406fa2d70f91fbba53acedfe781a8dfdd48bc3de80a0a535
 size 1301
