d7e5a0fe7ea27ebb2afc426aaa5a1916c77ceb57
TrungPQ <trungpq@konbinisg.com>
2019-04-10 18:28:01 +0700

    Update comment after review 131.

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9a900fbbce8588e15e07cb88a5cc3e3de774fdce184525fb2f46a77c9f35809
-size 44643
+oid sha256:0afaf8d2a7cd13ac37f3f664cfd081a8809500052d3936c50f3b2112e795d22f
+size 44604
