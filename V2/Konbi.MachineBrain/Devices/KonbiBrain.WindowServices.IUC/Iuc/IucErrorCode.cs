c1ea4b5043738c452e8e37c6619f051ff0a405fe
TrungPQ <trungpq@konbinisg.com>
2019-04-12 17:32:06 +0700

    Add new IUC error code.

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowServices.IUC/Iuc/IucErrorCode.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowServices.IUC/Iuc/IucErrorCode.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4a83309d4819e5323746128ed055ba676d2bd63619479fafee612294fdce67e
-size 3683
+oid sha256:208c7ec315ccbed8db90a8cad275343578461a4deeb76109ec3ba4c103b6cd99
+size 3867
