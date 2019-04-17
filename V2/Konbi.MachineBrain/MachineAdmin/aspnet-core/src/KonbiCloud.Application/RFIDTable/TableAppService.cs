1b2fe6ca335b38845f4e9aa56509a3df9465c240
TrungPQ <trungpq@konbinisg.com>
2019-04-17 11:09:02 +0700

    Edit logic save transaction and update images for trans when camera return.

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69b7723b1503009073643ed936366765344d4b45d754a564c30d06e98abd5c54
-size 14760
+oid sha256:45d72be3a525f6588aa809b0de87878a900af2aeee539ef6eac98d9dbbc67f2b
+size 18913
--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10a418a4537599ce9825d8af964de06a7ebcf2a369705bff2858cd0c600d8a47
-size 47301
+oid sha256:fd3106dc8b91014520576f5adee6e56d345703e9b83579efc773ecd9aba5394e
+size 45751
