e4beb1625e8aa879ff3163f8f39f5d9be2cd521f
TrungPQ <trungpq@konbinisg.com>
2019-04-22 21:03:29 +0700

    Fix code review with Long. Add machine in trans.

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:488df48379c36f9af8b7b2cc5abe1c9657129be95bf0ab1d23bead7a9777fae8
-size 15674
+oid sha256:34bd8f47e848ad40c53e71793aa9ccc8f11f0d472d9a6da86cfe64e4d1abcc9a
+size 16494
