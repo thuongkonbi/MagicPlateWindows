3ac40b913521d876c43c3e483b04b4bd88815da1
TrungPQ <trungpq@konbinisg.com>
2019-05-17 10:42:31 +0700

    Revert to source code for old CustomerUI.

--- V2/Konbi.MachineBrain/CustomerUI/RFIDTable/js/abp.signalr-client.js
+++ V2/Konbi.MachineBrain/CustomerUI/RFIDTable/js/abp.signalr-client.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1422ac9f302ceaedf93de987db032815ffbf948f330094e94d8d2a08681781db
-size 9998
+oid sha256:57f56c109812697894b422cf21943fb77f7511a766fc60245f5e5e51b870ec81
+size 9610
