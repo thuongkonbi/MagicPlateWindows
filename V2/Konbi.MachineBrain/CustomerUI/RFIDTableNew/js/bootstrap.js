35d70af42a1e7122352da96c3abbfa2b893456a0
TrungPQ <trungpq@konbinisg.com>
2019-05-17 10:39:17 +0700

    Add new folder StandardCloud\V2\Konbi.MachineBrain\CustomerUI\RFIDTableNew

---
+++ V2/Konbi.MachineBrain/CustomerUI/RFIDTableNew/js/bootstrap.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f364953a3675a8b76babc5549808ac15aa424aad5ba606afb5741a0c62cf0008
+size 123765
---
+++ V2/Konbi.MachineBrain/CustomerUI/RFIDTableNew/js/signalr.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:372dbe724797df21272e74556a7849f421a1415a91d8bebd85df73c993d0b8a6
+size 184863
