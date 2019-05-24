da34b61e7b051f1844548cfc6274c37e52373b41
TrungPQ <trungpq@konbinisg.com>
2019-05-24 16:19:08 +0700

    Fixed Contract Price in Menu Scheduler displays 0 on Cloud.

--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/PlateMenu/PlateMenusAppService.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/PlateMenu/PlateMenusAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16b5064283049f6ec3e276ff10da30574108bde4beafc98cd154da1b46f496c7
-size 38013
+oid sha256:8f4dec1eb43029e55c75caae88223a63c5344ca7449e4565a99690ab1bf8e92c
+size 38113
