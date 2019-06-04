e20f94388f520edc3625ad413cb4e575afc1b16e
TrungPQ <trungpq@konbinisg.com>
2019-06-04 17:45:04 +0700

    Add product for RFID TABLE.

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Products/ProductAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Products/ProductAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8ca47db322286b51307e7372c6fb85cc15758bb521347ff20dc4cff1579c72c
-size 19153
+oid sha256:84a9368d59693a35ca59123ff8599227bc7ddaa280ce59472fe8ad84cc238e2d
+size 19477
---
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Web.Core/Controllers/ProductImageUploadController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a3dd7f287b846a814e5a754a05a1918ae6417cf0121937db57f5b1dc4cde77b6
+size 5222
