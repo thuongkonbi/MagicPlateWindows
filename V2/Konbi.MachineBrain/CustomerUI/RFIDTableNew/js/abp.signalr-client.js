2557c8033448eb0b052135ada246feede00d350b
TrungPQ <trungpq@konbinisg.com>
2019-06-05 14:26:12 +0700

    Redo UI to match existing style- Black/White.

--- V2/Konbi.MachineBrain/CustomerUI/RFIDTableNew/js/abp.signalr-client.js
+++ V2/Konbi.MachineBrain/CustomerUI/RFIDTableNew/js/abp.signalr-client.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7c9f09a909d4f2506548740b1ac5abe61c550010ba8061b89f53fd897c24dda
+oid sha256:bc7f9171015c6bf0cf5526290c57378885d4b793a99a14763419d6d4be0c9b32
 size 12271
