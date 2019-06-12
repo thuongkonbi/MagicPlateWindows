467a899626cb7ce98841a6fbd5cdfa7b239e7912
TrungPQ <trungpq@konbinisg.com>
2019-06-12 14:17:32 +0700

    Return alone screen.

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0000a3299a1068eb6781604f7bfbfb6fb00675af306d4e753027664692a970af
-size 44592
+oid sha256:b3d4c6aad5ae8705cd8d6cf7536520fb8d9c53bbe3603ee5646a3d6a32c3b73d
+size 44305
