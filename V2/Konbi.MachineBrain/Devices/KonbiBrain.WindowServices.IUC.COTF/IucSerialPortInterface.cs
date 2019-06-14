a89a0ad1821d4e3c9b9127dbcee7010c5bc8c614
TrungPQ <trungpq@konbinisg.com>
2019-06-14 11:40:12 +0700

    Add button pay.

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowServices.IUC.COTF/IucSerialPortInterface.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowServices.IUC.COTF/IucSerialPortInterface.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf94a54be360b830204a86dfcfa669bce8a28cf7b3a470997a500fd6a7eb0727
-size 38356
+oid sha256:27331c70a8a99383086a02dedfb117313b26e5a9cb2d127215d6c79fa742c493
+size 39124
