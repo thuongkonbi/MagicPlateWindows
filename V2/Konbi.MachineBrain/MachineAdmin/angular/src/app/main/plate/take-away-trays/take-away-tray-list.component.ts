7d2a226b79a5582e603b2d82050dbb3b76a097b2
TrungPQ <trungpq@konbinisg.com>
2019-06-19 09:32:11 +0700

    Create button add new takeaway.

--- V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/plate/take-away-trays/take-away-tray-list.component.ts
+++ V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/plate/take-away-trays/take-away-tray-list.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6436733e84a0c99848cf9d3d95063c976f06af944a092f0bb8cf5beea0529b67
-size 3430
+oid sha256:8bd68dd15e626539b4db63dc80ec92589578697d781b8225e05bc7f318f0f959
+size 3547
--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Trays/TakeAwayAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Trays/TakeAwayAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e529973a445ee53175e34a0f9fc3c4fa8ae224374b6a90889269981740d9fc7
-size 4281
+oid sha256:9c58cb51498267f053847eef6ca64a537ab7f252d4cb1839efcdcea7e6a9860f
+size 5072
