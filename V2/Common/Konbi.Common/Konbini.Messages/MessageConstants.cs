4aa55c90a3a284579d46a1d4851f48d5823231ae
TrungPQ <trungpq@konbinisg.com>
2019-06-24 10:13:41 +0700

    Update message for CustomerUI.

--- V2/Common/Konbi.Common/Konbini.Messages/MessageConstants.cs
+++ V2/Common/Konbi.Common/Konbini.Messages/MessageConstants.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f83b3717a19a93866109b934fcf228077ef44401aeb9172cf480ddf0f255cd9d
-size 1864
+oid sha256:981b76e46b8eb1b25f8cebf5d8631e8865c44b24fbd1678334decd9ec923456b
+size 2078
--- V2/Konbi.MachineBrain/CustomerUI/RFIDTableNew/js/abp.signalr-client.js
+++ V2/Konbi.MachineBrain/CustomerUI/RFIDTableNew/js/abp.signalr-client.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc0fbbf2d05ff2d9a0112557d1c95bf968bf48712ec5984182b302fee61433f4
-size 12935
+oid sha256:d76cb53ae79907e16f6ba94f1fe7bb43e57f588f8731d788addcc78d019ca6a4
+size 12953
