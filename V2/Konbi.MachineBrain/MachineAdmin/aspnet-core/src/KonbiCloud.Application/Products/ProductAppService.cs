eb522989d1a99d6b37ff1e0db9c32baade2afb6f
TrungPQ <trungpq@konbinisg.com>
2019-06-27 11:13:47 +0700

    Fix bug on TrungPQ_Product_Manager_(18h).

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Products/ProductAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Products/ProductAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf581354a810ce44a0983dc8a606f32642a57220c2c01f1e610c77176fd07c63
-size 15489
+oid sha256:3d1e2b8fd667ea04f528e0d001db60144eb88e9cb11f5abb5d2dcc655bcd7f0d
+size 15510
