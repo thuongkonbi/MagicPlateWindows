20505f30dc1fcfa6a31f971f759141de753fd07b
TrungPQ <trungpq@konbinisg.com>
2019-06-28 15:24:51 +0700

    Edit scanning product with plate model.

--- V2/Konbi.MachineBrain/CustomerUI/RFIDTableNew/js/abp.signalr-client.js
+++ V2/Konbi.MachineBrain/CustomerUI/RFIDTableNew/js/abp.signalr-client.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d76cb53ae79907e16f6ba94f1fe7bb43e57f588f8731d788addcc78d019ca6a4
-size 12953
+oid sha256:43a23997ceae1a89c5d01ce91e0c7961e62e54e5c5e8bd3db1dcb432f9b56d86
+size 12981
