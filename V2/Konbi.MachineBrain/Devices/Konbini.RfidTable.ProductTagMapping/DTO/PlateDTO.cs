4886314c851c499f7783586f087bf5569b74fc55
TuDN <tu@konbinisg.com>
2019-06-28 15:25:13 +0700

    Fix bug on tag writer app

--- V2/Konbi.MachineBrain/Devices/Konbini.RfidTable.ProductTagMapping/DTO/PlateDTO.cs
+++ V2/Konbi.MachineBrain/Devices/Konbini.RfidTable.ProductTagMapping/DTO/PlateDTO.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b843ce4b81e68eff7961684bfdaf99870b0fdda01ecd4e5704d6c6d1374e310b
-size 340
+oid sha256:7629a09733ec878730650ddb808c96a4540936cab2fafa6793dd0b7224089e5e
+size 501
