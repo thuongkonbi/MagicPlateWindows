fce984c21e4250b770e2cc1f9b2d092067a20a8e
TrungPQ <trungpq@konbinisg.com>
2019-06-28 14:35:07 +0700

    Edit Transaction Infor.

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98cbbc804c901b7d2ec073c32417b0bfaa2341a08e7116d85b8cb25bcdeadd0d
-size 14401
+oid sha256:487487b27af5af93596507d054a75caa632b2e0bb6f55170276f3207b92816a3
+size 15581
