8021473352fc22056e71da88650d10a645753370
TrungPQ <trungpq@konbinisg.com>
2019-07-01 18:32:01 +0700

    Add show Transaction with product but not detail.

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Transactions/TransactionAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Transactions/TransactionAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d8e7b8f7d1024c7aad049cdc2deb06dc45af1d7564a53ffd879466dd4cb58ef
-size 7840
+oid sha256:ecaf6911c5669b88f053e5a1d900390de3730c2a0927cc3c69e6803a5759378f
+size 7977
