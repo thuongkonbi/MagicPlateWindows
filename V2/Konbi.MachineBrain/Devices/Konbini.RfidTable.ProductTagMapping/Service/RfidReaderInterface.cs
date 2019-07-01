a08f3dac5b4e7f43f7cc5bcda124678bcd0ec85a
TuDN <tu@konbinisg.com>
2019-07-01 16:28:06 +0700

    Edit Tag writer, change disc check logic

--- V2/Konbi.MachineBrain/Devices/Konbini.RfidTable.ProductTagMapping/Service/RfidReaderInterface.cs
+++ V2/Konbi.MachineBrain/Devices/Konbini.RfidTable.ProductTagMapping/Service/RfidReaderInterface.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a60de6afc45020343f522aaff578c3d4223e8a6509a34eb6232c3a5431dbd65
-size 14266
+oid sha256:ba884f68c3cc59c00696999ad640f44148ce3a9abdd5be56e8602f4f0ce1679f
+size 14181
--- V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/plate/disc-check/disc-check-modal/disc-check-modal.component.ts
+++ V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/plate/disc-check/disc-check-modal/disc-check-modal.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96579ab35e24e2d04cbbb7dff8163587e342686b8f64c756d42aaa5d0e501082
-size 7234
+oid sha256:d157642b86f2e11779b10a2c0c6c6cc858677effae077475fd23a98cdc9829de
+size 7395
