6b43ca4a5740d4f2a0edd83033102ac70125b026
Bành Cao Quyền <quyen@konbinisg.com>
2019-07-03 21:37:22 +0700

    + Refactoring Process transaction code + Modify ManualPaymentInput class

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/ITableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/ITableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1ad248032762d4e26254d0d49485e2e245a556cb763bbbfc532e120e0ede4c1
-size 981
+oid sha256:2f0412d892d5cd5ee256fa48fb54d7b56eae41c54b08b5ed30b1b4d4f3485a5c
+size 966
--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44038c25645f9ff47e4ca2e81ccb0b628b821c8c0f8363b595bad14fd333e69c
-size 49990
+oid sha256:0e5efec0aee5480053bfb18fdc17c1e667c8d747dfe3a785d2ee112f75c35c8d
+size 47068
