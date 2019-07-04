71b483a5268b6f769ff4de4e6283d6a235dfaa49
TuDN <tu@konbinisg.com>
2019-07-04 16:29:46 +0700

    Reset customer price after transaction

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowServices.CotfPad/Hardware/HardwareUtil.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowServices.CotfPad/Hardware/HardwareUtil.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:141827e6576b2fb70a0ac8a7ab0abd3af66614f3e54bb1ac1efc66b7d82d59e6
-size 21693
+oid sha256:77478003e9f64eccbb77b7b90cc264314eac3ced5bbdceb66c946e18a9a636d2
+size 22799
--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:270f776fe9b6aebcb0e803a4246d95d34762883fc305cf707a2e3d01c5b9c030
-size 56614
+oid sha256:13b6d153d6fa87f69ad93a3ff93c810de4d6ab90c8a7b050235d66625ad983b9
+size 56790
