603a23953785c025a6d195f9e8ab58ba49d5258e
TrungPQ <trungpq@konbinisg.com>
2019-07-04 15:01:34 +0700

    Fixbug when do not choise plate.

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e51b5b76b0d645a23188432dd4c872ad92be9fd319cf8449a9ca476a4682c0d6
-size 54951
+oid sha256:404e851899ded2b8e9426194ceaad7a72ef0a73f569783f071a7226247fc62a2
+size 55013
