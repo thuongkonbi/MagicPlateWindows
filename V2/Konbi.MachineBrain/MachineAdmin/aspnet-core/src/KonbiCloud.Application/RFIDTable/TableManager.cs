c28c1d05e27265c5e28342c83fb894b77c6f947a
TrungPQ <trungpq@konbinisg.com>
2019-07-04 16:36:39 +0700

    Fix check empty ProductId.

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:270f776fe9b6aebcb0e803a4246d95d34762883fc305cf707a2e3d01c5b9c030
-size 56614
+oid sha256:4aa9a12b85097fdc65e47d3ec7f100aee9a1b8c3f1dc5bb83b84738b9dbceaed
+size 56643
