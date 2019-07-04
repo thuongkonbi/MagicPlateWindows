c39f355c03680efec77c5a9de642b3c2ea248734
TuDN <tu@konbinisg.com>
2019-07-04 15:15:56 +0700

    Fix bug on Custom price transaction

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7b9ce7b78f9caf07dad051b1c062fbf6b50e74e29cac30fe70115f602ec8b88
-size 56397
+oid sha256:4b1be242d97d160fdfb1033b176fa1bce65a7ba94a484df1d0335b2d9dd92e9f
+size 56445
