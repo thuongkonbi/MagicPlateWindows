141dda58aa560d5a29c597b497643c0d5429dc6f
TrungPQ <trungpq@konbinisg.com>
2019-07-04 13:47:16 +0700

    show milliseconds in list transaction.

--- V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/transactions/transactions.component.ts
+++ V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/transactions/transactions.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e265073aa03c5aafc7a27b63550383da5ff7359e0abadfb71f435f06665514c6
-size 6098
+oid sha256:30d02d06791924b4c8028fe006bd76dc438c88c327d52569a404be418dfd5c49
+size 6101
