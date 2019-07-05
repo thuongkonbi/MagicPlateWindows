7f3cfb73952f0153531c968a69063cdb23f1f7f0
TuDN <tu@konbinisg.com>
2019-07-05 17:09:23 +0700

    Fix case new plate with custom price

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:551479014a4489d64a392b90e039090e754442366dd8eff86e5abd07d6536309
-size 57469
+oid sha256:854ad70ec88ed2ade56b91e1cc3eca97208da1c436df422945f84823abf2f7e7
+size 57551
