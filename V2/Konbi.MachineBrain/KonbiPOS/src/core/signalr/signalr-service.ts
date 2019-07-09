5da13801555d4c96e35b800d325a864db9b0685d
Bành Cao Quyền <quyen@konbinisg.com>
2019-07-09 15:13:17 +0700

    ok

--- V2/Konbi.MachineBrain/KonbiPOS/src/core/signalr/signalr-service.ts
+++ V2/Konbi.MachineBrain/KonbiPOS/src/core/signalr/signalr-service.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55b1763b54dbd3b716153bb6832b516b38e714dcb0bc8441f2b7e5ef529f5c01
-size 5431
+oid sha256:f81bf59b93d5d0ebe3a5b0389c58fd46abe8c3d514c8aa32baab22946b102a9d
+size 5348
