cc69fd9fb391055b86f540060f6b0cacbbb031a8
TuDN <tu@konbinisg.com>
2019-07-11 01:27:42 +0700

    Fix sync product menu

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/CloudSync/RestApiPlateMenuSyncService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/CloudSync/RestApiPlateMenuSyncService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a58ffbcdda3718ac6b448c7b700dafaaeb01408abed154fb5ceca73680a357db
-size 2295
+oid sha256:2c1879c6fb89cb8982f4b901f964f7094a0d9883385e1f1f52b8dd83aae79ad9
+size 2297
--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/CloudSync/SaveFromCloudService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/CloudSync/SaveFromCloudService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e20677f1ba2681994147083690c4967fad8e1ceea1060a8970fa62df21ed45c
-size 27217
+oid sha256:a251ca34b1f256a569ad350f256b1bc7a21e8ac65ac39e579c996ccc16873c52
+size 27094
