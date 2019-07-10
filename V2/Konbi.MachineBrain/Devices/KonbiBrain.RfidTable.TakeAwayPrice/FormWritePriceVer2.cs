3cedd80bc6ab617ec9edcfebf94f9e1aedc2c69d
TuDN <tu@konbinisg.com>
2019-07-10 11:19:39 +0700

    Fix bug cannot write price after delete price by DEL button on Keypad

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePriceVer2.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePriceVer2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a31dad0c6206b0ac683f1f7f2eb2968cd6d4062d3382287ae6525be3f689e07e
-size 43189
+oid sha256:f1dcd26585f7f3fa8e1fde914b4f33478672cd9d7df803947347c6c20050a2d8
+size 43191
