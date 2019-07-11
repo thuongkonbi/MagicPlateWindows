4200f2c96be42f233cb84538e08318c1cb053a1d
TrungPQ <trungpq@konbinisg.com>
2019-07-11 12:38:03 +0700

    Fixbug Product _ Plate model do not auto sync.

--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Products/ProductAppService.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Products/ProductAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:451844b79453a2569fac0f148ef5342f4fcdd2e485ac4f32c635ac0ce9aedd12
-size 17277
+oid sha256:d6add24241831f3874a820303cc9ac48e678ad688e6edfc28388564b99aa10ea
+size 17785
