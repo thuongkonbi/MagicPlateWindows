dc677a542d0f5a3cbcc37210fcaf40cd3ddf09b0
TuDN <tu@konbinisg.com>
2019-07-12 14:59:05 +0700

    Reset price in cash pay

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9edbb9f538d2dbd91cfde3e4b0e71c736ec8a7d54484c5250edbee8eb1fb3040
-size 60369
+oid sha256:45fbcd2acee5fecc650e516f5797c40a94fd64cf4e47296823afd38bf2c222a5
+size 60477
