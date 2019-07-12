38578f765b7fd10f146b6b4e4869fb9e0f5e9cc6
Long Le Hoang <long@konbi.cloud>
2019-07-12 10:18:50 +0700

    commit to resolve conflict

--- V2/Konbi.MachineBrain/CustomerUI/RFIDTableNew/js/abp.signalr-client.js
+++ V2/Konbi.MachineBrain/CustomerUI/RFIDTableNew/js/abp.signalr-client.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cba9c5e29af760b864ac3971194537b9dfc518ada1ca54a25a05d1342971636d
-size 13429
+oid sha256:71c30182c1466d5cbd9a25aa2cded1b9f57ba1f8786df07bba62d2a0b0da0a06
+size 13982
