7c810c3f6ee68b6345c6e752d28bada0181b69b4
TrungPQ <trungpq@konbinisg.com>
2019-07-12 16:18:09 +0700

    Reset cache after sync from cloud.

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Plate/PlateCategoriesAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Plate/PlateCategoriesAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f9383c1a0abb8ed6a0f5b0df2c71f6cb4dea54d50296dd4fff9c9d2bf49e467
-size 7594
+oid sha256:c6b079f5bd56c37ffcfa94cb9a2f57440a0eb48e2a9b030983aba52e2a211084
+size 7866
--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Products/ProductAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Products/ProductAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19e1d92a08ea0ffcdb0faf56e03d5fc0e30f989c7d68cbf4e97709dc6b29b2a0
-size 18435
+oid sha256:7f8f2d9cc0a81f77c286b07f1bf5225f3890e3afe5406a35a117e42e091b220f
+size 18526
