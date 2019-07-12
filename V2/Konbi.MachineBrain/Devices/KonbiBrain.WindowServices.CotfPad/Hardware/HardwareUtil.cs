e087ef71d8fa0198102e7767f10e13a197482e30
TuDN <tu@konbinisg.com>
2019-07-12 16:39:00 +0700

    Fix bug on cotf, TableManager

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowServices.CotfPad/Hardware/HardwareUtil.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowServices.CotfPad/Hardware/HardwareUtil.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c1abd006ef068f49fbd3096e71a5d3498875b184b1de184d7fe6474b6ab3737
-size 23299
+oid sha256:e6c7c61182b500fd8727bd8618eb6cc8ada0edb431df792f40cb147f68d378c2
+size 23344
--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e713ac63502764c2dc06d488d76169a3b3da2a5e88426089a4e240864f3d43da
-size 60134
+oid sha256:80c30fdb59d4e16fbb02eb32302b15e363b7707ce0ee4c3da60b4dfa9fa5cf76
+size 60455
