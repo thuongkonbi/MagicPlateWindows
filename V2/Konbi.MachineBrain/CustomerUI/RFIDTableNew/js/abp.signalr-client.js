daccf1bfa93af24ee002d283cfdc570ec586e43b
Long Le <Long@konbi.cloud>
2019-07-13 14:01:54 +0700

    imporve alarmlight

--- V2/Konbi.MachineBrain/CustomerUI/RFIDTableNew/js/abp.signalr-client.js
+++ V2/Konbi.MachineBrain/CustomerUI/RFIDTableNew/js/abp.signalr-client.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ff7d0f8b7fd1da8d6050bc9db3adf140faab9d9296625684d46c05b5628b1f1
-size 14118
+oid sha256:ea6397c50138d7a597e9e5059e345e532a49fe52a3f2bf95b899100df1ef16de
+size 14415
--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d972bc2c1fddc1d93c50a97db1f0328345b5da3cf756abb61208ecdec36d0a6
-size 60656
+oid sha256:de3221acfe591c0c70027e102fa4d4a350999018fe76662027e4c4e1aa8c1cdf
+size 63755
