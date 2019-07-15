300583f66efa380cd4c46ff04df46b510dbb749d
Bành Cao Quyền <quyen@konbinisg.com>
2019-07-15 08:52:12 +0700

    ok

--- V2/Konbi.MachineBrain/Devices/BarcodeScanner/RawInputBrain/ViewModels/ShellViewModel.cs
+++ V2/Konbi.MachineBrain/Devices/BarcodeScanner/RawInputBrain/ViewModels/ShellViewModel.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a132b7778d740744ab31005f8585084b671f0291ef40919b79ee984bd74da7d4
-size 2804
+oid sha256:dc602798c9edaa5f7e96dddfadf7f76ab3d145b5c9e7ee20494773a2011bfea2
+size 2577
