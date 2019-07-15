bad352b006d41788f0d98b567d51e992da03db70
Bành Cao Quyền <quyen@konbinisg.com>
2019-07-15 15:42:39 +0700

    Add Display Order to cloud

--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Products/Dtos/CreateOrEditProductDto.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Products/Dtos/CreateOrEditProductDto.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1249e57a064baa7a4264930e6ba8d35e797ffb7a71d9e2d0bd6fc6a30f9f14e8
-size 618
+oid sha256:71a8121c676507dbbc73ac7d44e126d01a723fecfd9beff4434d4d44102deed6
+size 666
