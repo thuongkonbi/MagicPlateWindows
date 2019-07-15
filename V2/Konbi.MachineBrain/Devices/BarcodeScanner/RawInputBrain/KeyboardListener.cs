5e9a95a36286a3a0d632ad2c48404710f48314e9
Bành Cao Quyền <quyen@konbinisg.com>
2019-07-15 13:56:52 +0700

    update barcode listener

--- V2/Konbi.MachineBrain/Devices/BarcodeScanner/RawInputBrain/KeyboardListener.cs
+++ V2/Konbi.MachineBrain/Devices/BarcodeScanner/RawInputBrain/KeyboardListener.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9e9edd306151870a8e055d6bac4da971313f2c66963733c3e602d6ca1bc2c63
-size 11586
+oid sha256:2a4d9d35f43e55c6dd9e7345c572d36e9b635baf87f2a06a3d3a6bf88e9e7e52
+size 10264
