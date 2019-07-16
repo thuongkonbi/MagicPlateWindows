256070e78ac46bae6efab35b371cd012993f91dd
Bành Cao Quyền <quyen@konbinisg.com>
2019-07-16 09:08:25 +0700

    Refactoring barcode scanner

--- V2/Konbi.MachineBrain/Devices/BarcodeScanner/BarcodeScanner/AppBootstrapper.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:65a1efce3f70fdef26ad65362450fc18f0cdb1a0d25bb991be59db4fd1a3a12c
-size 1388
--- V2/Konbi.MachineBrain/Devices/BarcodeScanner/BarcodeScanner/IShell.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1d99fc06cc3b185312ce771bf4e4eca36534dba30cb15106193d7956b97892ab
-size 59
