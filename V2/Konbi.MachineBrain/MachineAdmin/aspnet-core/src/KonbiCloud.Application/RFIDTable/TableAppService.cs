030345309b318d3e18a5a1cf4b6ca810ac8f632c
TrungPQ <trungpq@konbinisg.com>
2019-07-16 15:18:33 +0700

    Fix delete product category.

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87a3b5bb2b81c32d7e23a38cd6195d39aa09739cb82ae78ecaa846e1f2b23c4c
-size 17945
+oid sha256:87cf47f89cc1be15dda5afaa5d887995b36790412d612fc78cecd0e44c89b2f7
+size 17811
