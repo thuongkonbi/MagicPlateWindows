65f4e7e40e7334a824b3e1c25a68e9ed5285872b
TrungPQ <trungpq@konbinisg.com>
2019-07-17 16:51:05 +0700

    Add filter on product.

--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Products/Dtos/GetAllProductsInput.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Products/Dtos/GetAllProductsInput.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4c34d4dd3cb5c68e992cc9925b95f4d31c3db9d9878f8df172438f72580c613
-size 475
+oid sha256:3d145fb68ddfcb935ff85ff8963703b6b4df2fa7d0928843a8f6f73f7158a60b
+size 473
--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Products/ProductAppService.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Products/ProductAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42d2b73704a5892a6cc5a56bd95e1cabc4037ecc1c375ee522eb137eaab238bd
-size 27602
+oid sha256:df79f30a608df7948306063cdb8586049a4818a48e2926341f2065faa451cf6d
+size 27610
