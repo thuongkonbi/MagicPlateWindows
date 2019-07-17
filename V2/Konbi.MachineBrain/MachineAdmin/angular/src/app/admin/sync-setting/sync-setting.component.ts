e0dd1a312c79710d776e3840353eed155ea09bbd
TuDN <tu@konbinisg.com>
2019-07-17 09:01:05 +0700

    Fix bug sync product, make Plate name required

--- V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/admin/sync-setting/sync-setting.component.ts
+++ V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/admin/sync-setting/sync-setting.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89d4666a0eb66f9382bf66195f2172d884327a624fc7793ada100a255bb2a078
-size 5096
+oid sha256:0c92adaa104dac6aaa92eeaf6dbbcbaa31b1c5c73a2bdcf3ba70e73fa5eefafb
+size 5107
