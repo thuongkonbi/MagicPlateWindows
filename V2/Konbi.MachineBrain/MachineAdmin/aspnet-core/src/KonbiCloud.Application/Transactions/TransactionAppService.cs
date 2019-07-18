8182a76038d131893efaa0c9ef7fcfe177684afa
TuDN <tu@konbinisg.com>
2019-07-18 17:11:28 +0700

    Fix UI on Machine

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Transactions/TransactionAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Transactions/TransactionAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89c10597ad81e0d88c9965ba990bc06418ce7a56e2b5e946fa9e61c9403619ce
-size 8318
+oid sha256:7ab71a015aa0ea4c4db2c5e7ad1a9818098d2dd4cb12e2599fc5cbca0431d112
+size 8289
