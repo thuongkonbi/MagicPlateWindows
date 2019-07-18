5daa6d3557dba40b2b10680927a610f697480a95
TrungPQ <trungpq@konbinisg.com>
2019-07-18 17:05:10 +0700

    Add SaveChangesAsync when sync.

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/CloudSync/SaveFromCloudService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/CloudSync/SaveFromCloudService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:adf3f64df7d27064de705a27e46d2b0fbecfd7c9b29aed54605c4fcaaadfba4d
-size 26515
+oid sha256:0d6f73e97117d8bf59e247594a1c210f6eadea6712ced58e99c0f3ec8afa4f0c
+size 27022
