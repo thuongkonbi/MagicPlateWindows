292ca39510c754416f4487d74a3953426c6e96a8
Bành Cao Quyền <quyen@konbinisg.com>
2019-07-19 14:50:47 +0700

    Fix Display Order issues

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/CloudSync/SaveFromCloudService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/CloudSync/SaveFromCloudService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d6f73e97117d8bf59e247594a1c210f6eadea6712ced58e99c0f3ec8afa4f0c
-size 27022
+oid sha256:bb33e0d80aae7f0cdc35392ea683fd9a025775adbb6d78cb4ea8d5849360d9c5
+size 27091
--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/ProductMenu/ProductMenusAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/ProductMenu/ProductMenusAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b01bd221751e1590c1d02b6abb21b9179baec85edc215876fcb6553c0e185b7
-size 50629
+oid sha256:3f7ab4038a9b63cf2fd871807dceea4525f265e3528a0fb6658249b71944a5d4
+size 49475
