2de402534bb2cb2e4fe830470516ae95cf32ca95
Long Le <Long@konbi.cloud>
2019-07-20 13:14:50 +0700

    fix sync nullException & 100% CPU of scanning

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/CloudSync/SaveFromCloudService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/CloudSync/SaveFromCloudService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68464c1f030073b203115d71bfe45603170fe9c491f4918a4b3d459d2b65ee8d
-size 27095
+oid sha256:4cf53552d8cec374302a727b3db9d80acfa008653bd729c0d878a8a5103dbd30
+size 26794
--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4172c1c6712cc7ca2735f85ce5f44cf0d68e017a449674d143511f1dacd4edca
-size 71459
+oid sha256:3e7b7c0e63f3c56cc708e22e98463e39ac95b90beb1daebb53b0cae65b20bb17
+size 71500
