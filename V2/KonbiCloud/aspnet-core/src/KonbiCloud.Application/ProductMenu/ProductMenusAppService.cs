df41af35df8eeee86b9f178fc92caa4b7a8eb881
Bành Cao Quyền <quyen@konbinisg.com>
2019-07-22 14:21:55 +0700

    Replicate Display Order Issue

--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/ProductMenu/ProductMenusAppService.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/ProductMenu/ProductMenusAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9268a5d85dbe528e340f621274010f183bd3e5e88e00e8477dc663e32e9bf087
-size 43130
+oid sha256:b0c91626041f90f95c3fcbbce7f9ee84572859bd631b007e2bba45ffac5737aa
+size 43246
