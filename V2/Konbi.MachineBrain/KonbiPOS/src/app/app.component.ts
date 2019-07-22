cecb0ed58a1615a90926b9cda843874f35646ea1
Bành Cao Quyền <quyen@konbinisg.com>
2019-07-22 10:39:06 +0700

    ok

--- V2/Konbi.MachineBrain/KonbiPOS/src/app/app.component.ts
+++ V2/Konbi.MachineBrain/KonbiPOS/src/app/app.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24d4238ad5bc209aefc17f938bada19501e80a61479ea66f6eba936ce6adcb99
-size 13870
+oid sha256:a1a52f50d7cb1ffa020a91f49c40156dabfed40546bbf3af6c7f797679d91d6a
+size 13888
