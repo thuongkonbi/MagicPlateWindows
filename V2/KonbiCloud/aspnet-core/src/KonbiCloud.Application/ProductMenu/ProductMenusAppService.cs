27a6959452eed7d621ca59abdff392d222c1b3ba
TrungPQ <trungpq@konbinisg.com>
2019-07-22 14:11:40 +0700

    Change index display order and contractor price.

--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/ProductMenu/ProductMenusAppService.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/ProductMenu/ProductMenusAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9268a5d85dbe528e340f621274010f183bd3e5e88e00e8477dc663e32e9bf087
-size 43130
+oid sha256:e53354c8dd26886936988028d85eb50dd4b2a69dfb0f2a88d1d3e7860fcaf31c
+size 42920
