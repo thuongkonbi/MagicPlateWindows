18e1a87103b68ea394275970015910a156b62bd1
TrungPQ <trungpq@konbinisg.com>
2019-07-23 17:14:04 +0700

    Fix show data in Dashboard.

--- V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/transactions/transactions.component.ts
+++ V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/transactions/transactions.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30d02d06791924b4c8028fe006bd76dc438c88c327d52569a404be418dfd5c49
-size 6101
+oid sha256:6b2bef4868b106b291308f68c82eae3a43d4e1f8adecbcdf03ab21c36c3183a0
+size 6135
--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Dashboard/DashboardAppService.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Dashboard/DashboardAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:692dbca0c2079f8c9b12425a6073cd6e2bbdef3d898261ec87578b0e7c563bba
-size 37236
+oid sha256:7c5bb0d7322d7d298ecd2527964ce072ee3ad8b9815380c14174bad8ac97ea25
+size 37393
