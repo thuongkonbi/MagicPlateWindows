b54ccee4c24b69fa6a4ef56bd49c6985aac13058
TrungPQ <trungpq@konbinisg.com>
2019-07-30 17:43:33 +0700

    Add shortcuts for customerUI.

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePriceVer2.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePriceVer2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4b03189d761c1c8670c308e8b73902192396ac3b79574db8de4a5ca142962f8
-size 48509
+oid sha256:f6586dea102de5dec3596155fb475b97d2058e176ebbf68af860cc934782dab2
+size 50936
