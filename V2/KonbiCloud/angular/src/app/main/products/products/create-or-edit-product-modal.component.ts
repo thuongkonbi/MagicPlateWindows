1a5d223e7f0911de9021b32bef47413bde7e36d3
TrungPQ <trungpq@konbinisg.com>
2019-09-04 09:40:18 +0700

    Fix duplicate barcode.

--- V2/KonbiCloud/angular/src/app/main/products/products/create-or-edit-product-modal.component.ts
+++ V2/KonbiCloud/angular/src/app/main/products/products/create-or-edit-product-modal.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9bfc32da23fd6bbb98dad4e3ac94583b0a06f79c83fa7d7e37e775a2d6534b50
-size 7783
+oid sha256:243db92edca1cb66a6f6192f0693c4e2b538dbf72f6009867a72d5c56c27448b
+size 7928
