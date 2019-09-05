9c1229e0ce67602df360220b53f68ff669933e2e
TuDN <tu@konbinisg.com>
2019-09-05 09:43:28 +0700

    Show message about what is written to tags

--- V2/Konbi.MachineBrain/Devices/Konbini.RfidTable.ProductTagMapping/ViewModels/MainViewModel.cs
+++ V2/Konbi.MachineBrain/Devices/Konbini.RfidTable.ProductTagMapping/ViewModels/MainViewModel.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d885a817f1f4a6ab8b529b1d58514df360688ff816f971300e5c071b5a258c37
-size 10310
+oid sha256:a83df8afa8fc9715d774d699aac39f4cf98f6406f7b8b2c0476e14bec03b0c4d
+size 10735
