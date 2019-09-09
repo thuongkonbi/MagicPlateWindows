4c05b4a0eede485869cb218c5a744d3e422d6966
Bành Cao Quyền <quyen@konbinisg.com>
2019-09-09 11:21:51 +0700

    ok

--- V2/Konbi.MachineBrain/KonbiPOS/src/app/app.module.ts
+++ V2/Konbi.MachineBrain/KonbiPOS/src/app/app.module.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ba51a9f7f9136658135936b8f37eb59a3d33a8ad76abf25aadd8bc3ee7dd4cb
-size 939
+oid sha256:597a96034ce7de855001b9e7f4644cec7e83422d668755f1f4d06e57361563a2
+size 1009
--- V2/Konbi.MachineBrain/KonbiPOS/src/core/api-services/api-service-proxies.ts
+++ V2/Konbi.MachineBrain/KonbiPOS/src/core/api-services/api-service-proxies.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f8ec465c93fb3bd980e4baad9652b038aacecde544fa2da140d46fd1533fcff
-size 562928
+oid sha256:f9d343e49efa56f6f45024ff95077d7e924fbd62e9b24d16bdc955a929f09d6d
+size 562876
