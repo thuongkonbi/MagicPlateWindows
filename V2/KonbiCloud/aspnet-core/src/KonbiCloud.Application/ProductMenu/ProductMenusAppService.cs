43783de9954d5b5098b74d7cc0be47e7576fb098
TrungPQ <trungpq@konbinisg.com>
2019-09-10 13:34:17 +0700

    Show category in menu schedule.

--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/ProductMenu/ProductMenusAppService.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/ProductMenu/ProductMenusAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee22b4ca965a8e21239f79a7c769d71f23ade7a6eed1cb2de8c1c6d64e2c8d76
-size 43146
+oid sha256:bfbd711652377fd7ca36a7126b0e40ab95912a22a1d61f598937143a6d590232
+size 43486
