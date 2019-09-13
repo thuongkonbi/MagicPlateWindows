922de34aaf9e07957124c19ceb0faf895bb4a42b
TuDN <tu@konbinisg.com>
2019-09-13 15:34:41 +0700

    Check Plate name when import

--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Products/ProductAppService.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Products/ProductAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99b2abb8cf5a000117ca6399cc20277b252189fc4ab5262eb8b1d0ad83069af6
-size 33846
+oid sha256:1341cc411531d997a651cf996d6533e9ad38415340a8a9af3c17df1deab0db92
+size 34027
