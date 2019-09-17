d9fd8c6e391730015aa08c7688cae5b0b896db7f
anhhangxom0610 <tungltc0812l@gmail.com>
2019-09-17 21:42:06 +0700

    Add code to make Machine run as window service

--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Web.Host/Startup/Startup.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Web.Host/Startup/Startup.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:beec46345d2c3570736a858eb1e8f5795449514af6ef1e7d92597e029ce17d44
-size 9958
+oid sha256:5c23417b54e99e01b0e3a7a502ba8eeb4afd0dc807c88784f6280ac64bb0dd8b
+size 9960
