80283f7ef62205de4a07d40e7520c078b499a995
TuDN <tudn@gmail.com>
2019-09-18 14:23:33 +0700

    Close Cotf, TakeAway automatically when TagMapping start

--- V2/Konbi.MachineBrain/Devices/Konbini.RfidTable.ProductTagMapping/Service/MbCloudService.cs
+++ V2/Konbi.MachineBrain/Devices/Konbini.RfidTable.ProductTagMapping/Service/MbCloudService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:784ead7effab616ed74b5c40b3d6ff452e3d39ac1da325392fcd73cbcb797840
-size 4290
+oid sha256:cd21eb624448aa97001d71d126d1384958c26648385d7c4e829c5f47344ed110
+size 4851
--- V2/Konbi.MachineBrain/Devices/Konbini.RfidTable.ProductTagMapping/ViewModels/ShellViewModel.cs
+++ V2/Konbi.MachineBrain/Devices/Konbini.RfidTable.ProductTagMapping/ViewModels/ShellViewModel.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe710fb1162692173c6bbca92087bd243fe1b2a40353d9ba13fd3fa8f2481729
-size 6468
+oid sha256:87d169bbafe1efa027ac8724f406b0926d09515a433d125108f80d106c77f810
+size 7887
