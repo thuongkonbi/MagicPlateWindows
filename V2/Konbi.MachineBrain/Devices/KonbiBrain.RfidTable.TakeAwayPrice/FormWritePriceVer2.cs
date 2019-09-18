8be839d8b620ce3b425624f60ea03ec1df22b957
TuDN <tudn@gmail.com>
2019-09-18 15:55:02 +0700

    Shutdown another app which use tag reader

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePriceVer2.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePriceVer2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f6586dea102de5dec3596155fb475b97d2058e176ebbf68af860cc934782dab2
-size 50936
+oid sha256:631baeb0ca11188293695aac02eb70fa543955d67719440fcb455d4c0440a43b
+size 52587
--- V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowServices.CotfPad/Program.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowServices.CotfPad/Program.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d3edde2e63688b5ee983e9ad10d429c718daea09ca0e2a17c4fe2b1d69e4c26
-size 1308
+oid sha256:fe2f9634b1ff0d8a7d5a901363abda9018373460c97b7d29248b54ba5cdabd84
+size 3359
