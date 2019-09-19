681a46170547e896d1c4eeacbc826dc5992ba4c1
TrungPQ <trungpq@konbinisg.com>
2019-09-19 13:56:45 +0700

    Fix GetSaleSessionCacheItemAsync

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e7b7c0e63f3c56cc708e22e98463e39ac95b90beb1daebb53b0cae65b20bb17
-size 71500
+oid sha256:32196d2475847d19ab279fb1fae13f57621f8812de207ccad3349f43c0b91732
+size 71517
