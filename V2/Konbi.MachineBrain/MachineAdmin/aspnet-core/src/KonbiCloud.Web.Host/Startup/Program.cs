1a35882f35980e84aeb14af8848a6f3c8696456a
TrungPQ <trungpq@konbinisg.com>
2019-09-19 12:53:27 +0700

    Set run service for local machine.

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Web.Host/Startup/Program.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Web.Host/Startup/Program.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:175fe1841c3e007c31b89b20a594aeaca8edfd5980ad5503a5fea430df9b498e
+oid sha256:6765372d2aa56ba6664d211b3bfee6155f146294e0770ffa86bfe25b04017213
 size 1608
--- V2/ScriptsServices/Pack.ps1
+++ V2/ScriptsServices/Pack.ps1
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cec6257d2a04fbcb1c676c1d1217e1241448ee67ba90a6b1fbf8c0ad4cade265
-size 8376
+oid sha256:63ff0766d65456f7b65147353b75146699aa5e2d961b0864e9b1c6bfd861a75e
+size 8395
