d1b3f4933c27adecacd2f45541a428faa17eb790
TrungPQ <trungpq@konbinisg.com>
2019-09-19 23:33:07 +0700

    Fix not show menu schedule.

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/ProductMenu/ProductMenusAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/ProductMenu/ProductMenusAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9884705f8fa72b06e175cdfa002d3cfe7e507a397973c94e9e5e0dd4ab00afad
-size 45405
+oid sha256:976d564b3fbbac084d8a4f92ad025c459b514c163f6c88005fc09e30554cd6dd
+size 44971
