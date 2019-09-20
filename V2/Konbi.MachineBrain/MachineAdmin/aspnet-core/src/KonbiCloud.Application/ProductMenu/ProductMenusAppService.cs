d8347fe7cdeedd3db6720f080b265a41b2fdf327
TrungPQ <trungpq@konbinisg.com>
2019-09-20 11:28:15 +0700

    Add order by DisplayOrder on POS.

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/ProductMenu/ProductMenusAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/ProductMenu/ProductMenusAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2581c8248e1754dc538180804cdc35d01a87296552cef1b9c8232a5151ceba2b
-size 45252
+oid sha256:ba4e03cc27a1cc2ea2656aad021dc53fe17451d873c3352c7d1db5d3aa12639f
+size 45040
