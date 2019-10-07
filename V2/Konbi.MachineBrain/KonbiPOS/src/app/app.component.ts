bbe80aa9753c4b9dbca9523dc8be1d6c74864598
TrungPQ <trungpq@konbinisg.com>
2019-10-07 11:22:53 +0700

    Fix performance load product menu in POS.

--- V2/Konbi.MachineBrain/KonbiPOS/src/app/app.component.ts
+++ V2/Konbi.MachineBrain/KonbiPOS/src/app/app.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c855303f1205d9b9caf266e28a08d8175406909ebbb8d59376ac6dc66c7ddea3
-size 13863
+oid sha256:3fb1adc63d7ca691111587ab27618271ab698d0456194abd3febff1fa76ca401
+size 14777
