c8c61ca55adb6c5ef903f213f356674fd4e0d3be
TrungPQ <trungpq@konbinisg.com>
2019-11-20 18:01:01 +0700

    Edit slow loading page and edit when scanning plate error.

--- V2/Konbi.MachineBrain/CustomerUI/RFIDTableNew/js/abp.signalr-client.js
+++ V2/Konbi.MachineBrain/CustomerUI/RFIDTableNew/js/abp.signalr-client.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d64c4c29319c66cf6f53e94bade8608adfeeb0ce4fb315fe7649ea777e93b704
-size 14686
+oid sha256:c094f897e6e3da88396e280f9204d4564166647742efc536943b669a026c45cb
+size 15015
--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c39aa32abe7fd6beb0f980197aef3d52fe537254c6d33ce5d5f2fd8f8b3a0f9b
-size 71469
+oid sha256:2be2ed4472185cb25ff6f911b0f2a44e36356425a1546195add6cbb44a770898
+size 71473
