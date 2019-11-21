a35e31de82f504f930f3515d87c97ebf5a91de4e
TrungPQ <trungpq@konbinisg.com>
2019-11-21 10:53:08 +0700

    Add logic Processing Payment.

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowServices.AlarmLight/Program.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowServices.AlarmLight/Program.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e92206cf49c2b43c80761dcfaf251552e30cfd99d58a9505b4adc3fc476429f
+oid sha256:5afd7e722b3878cbbf275a06de811d3702cdfa5131c4186a5dfc02f1dc98cf41
 size 1198
