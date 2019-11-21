9a772f0daa299b90d5d17229f576e9af5ca11c37
TrungPQ <trungpq@konbinisg.com>
2019-11-21 16:47:31 +0700

    Edit load product list in cache.

--- V2/Konbi.MachineBrain/KonbiPOS/src/app/app.component.ts
+++ V2/Konbi.MachineBrain/KonbiPOS/src/app/app.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bafa143b9db57b96ec0aaf8972f8e763373fd2f975c69e583f260b2d1f3c95e7
-size 15113
+oid sha256:c47a6e0dc5917c4592e9cd37f17ad258fe0003893f662c1c9ebca112e10abdec
+size 15304
