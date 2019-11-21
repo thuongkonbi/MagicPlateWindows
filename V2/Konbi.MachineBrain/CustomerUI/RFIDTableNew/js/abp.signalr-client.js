0c8e15af23299145c27b93462731e7eed38b2bce
TrungPQ <trungpq@konbinisg.com>
2019-11-21 11:15:28 +0700

    edit status payment.

--- V2/Konbi.MachineBrain/CustomerUI/RFIDTableNew/js/abp.signalr-client.js
+++ V2/Konbi.MachineBrain/CustomerUI/RFIDTableNew/js/abp.signalr-client.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3fb1278b260ff4689fabc930575089d80962b66301dbafdbf99833345ce30ad2
-size 15520
+oid sha256:f798b8814313b58de2027f10e0cdd358357aaf98437795bfde6a06b796037cb1
+size 15724
