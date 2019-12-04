925eafab49be7a920f75af5d8bc9deba2689495c
Long Le Hoang <long@konbi.cloud>
2019-12-04 10:52:20 +0700

    fix the fucking stupid bug

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ead5c5cf5df668661bf3fafd68ec6a94a60794d33bb1eec94b426b0fa2beb746
-size 71657
+oid sha256:b01bcaf48dce63773c7d8dae5f6e4f6a3126cbe13f4c179fcf1a1c9bac07eb14
+size 71656
