708d8a163858951611103738465f611e403b315f
Hung <hung@konbi.cloud>
2019-12-05 09:41:01 +0700

    Add functions to check service status

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Services/ServiceStatusAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Services/ServiceStatusAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff50b613c864839c0f7a227198217d4368ea3d193b3e26b089e23f6207d44167
-size 1702
+oid sha256:167d075202cddcc5ef21e7a7e5498ea17ba187bde1a25f4a8eb6971c4bdaaac9
+size 3212
