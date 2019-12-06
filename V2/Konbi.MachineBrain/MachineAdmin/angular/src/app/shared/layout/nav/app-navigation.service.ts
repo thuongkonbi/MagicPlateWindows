7b143678e7e6f354fc904ee730814807c98f2b71
Hung <hung@konbi.cloud>
2019-12-06 15:49:50 +0700

    Refactor code - update UI

--- V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/shared/layout/nav/app-navigation.service.ts
+++ V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/shared/layout/nav/app-navigation.service.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a41d6159c34d4389064d119baae524af6c399f4145937baf28d09f27a441680
-size 8341
+oid sha256:926268d500d1c595c55f57d7eceac045506987d4e4365b119bf5acfc6a77a183
+size 8306
