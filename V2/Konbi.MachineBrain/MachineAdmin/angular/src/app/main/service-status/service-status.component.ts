02ebb2fc480ea2208c4013713e49069d85246d11
Hung <hung@konbi.cloud>
2019-12-09 14:18:36 +0700

    Refactoring code

--- V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/service-status/service-status.component.ts
+++ V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/service-status/service-status.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49a480fa9261590b8f1abbf56ac9c0a7a1864659d64c5aa9a5e046396dd7bcba
-size 2388
+oid sha256:3f782fb101a6ea424a3ee5ea686bbbd6c360d31a158ba38c71dcc53b2b4208a1
+size 2267
