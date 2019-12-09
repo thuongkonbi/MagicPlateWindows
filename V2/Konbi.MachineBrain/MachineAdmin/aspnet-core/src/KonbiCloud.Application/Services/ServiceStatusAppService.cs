5bde75ecff224ffe8a4f64f026bcf6848050471c
Hung <hung@konbi.cloud>
2019-12-09 14:25:37 +0700

    Fix resolve conflict code mistake

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Services/ServiceStatusAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Services/ServiceStatusAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c67d089474543752db49cbdcf3e0e1303106c1e8d444b541bc812df7708ebff
-size 4516
+oid sha256:d73fd2b869ec8a323183db9c8b5e9eeaf485dbbca2df7a44a34e381f360b48bb
+size 4523
