d9d55f99f27ddd273ab8f52f5a47123559f78956
Long Le Hoang <long@konbi.cloud>
2019-12-09 16:42:45 +0700

    correct service status icon & renamed machine/server backend UI title

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application.Shared/Messaging/IConnectToRabbitMqService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application.Shared/Messaging/IConnectToRabbitMqService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73dadd8f173ac16babe8d3d9f17b245f7633d3aeb9ec604b9b6141df2c1afada
-size 1154
+oid sha256:55e2d609ee9657f81df8db52ddbab4779270bc4b613f2fe9713e5b4ebc6ae864
+size 1188
--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Services/ServiceStatusAppService.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/Services/ServiceStatusAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c559b5ee097a97f5f14459b923168205e34ac17e96fe4dd5ff5ae065cdcefb5e
-size 5351
+oid sha256:6c022deb2bf41406007b808f10100b5087fc25b0cc5944263a9e3d1165301813
+size 5530
