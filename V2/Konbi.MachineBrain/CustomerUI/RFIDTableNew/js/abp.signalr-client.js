1162fd63ac106beb027d86d67ae5778fccc2906c
Long Le Hoang <long@konbi.cloud>
2019-12-13 17:15:28 +0700

    update customer UI with qr dash logo

--- V2/Konbi.MachineBrain/CustomerUI/RFIDTableNew/js/abp.signalr-client.js
+++ V2/Konbi.MachineBrain/CustomerUI/RFIDTableNew/js/abp.signalr-client.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95a7efe7eef36d428c23f5c8bd2d5a2deb311b78d91ad05089fe3cf443b32760
-size 17411
+oid sha256:5891c5c46037708cf0ad3250491af3ec868424c3fdc0eec524ec8bb310eefea2
+size 17409
