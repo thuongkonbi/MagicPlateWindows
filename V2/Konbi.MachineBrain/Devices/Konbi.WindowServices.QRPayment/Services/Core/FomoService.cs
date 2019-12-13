48ba492bbc49bb5031f2b25c244bba7af4dec67c
Manh Sam <manh@konbinisg.com>
2019-12-13 14:16:34 +0800

    Cancel request

--- V2/Konbi.MachineBrain/Devices/Konbi.WindowServices.QRPayment/Services/Core/FomoService.cs
+++ V2/Konbi.MachineBrain/Devices/Konbi.WindowServices.QRPayment/Services/Core/FomoService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e07f79df9c287c4d1325c0045ee45620d3630006e79fff1871903d7d96f12f6
-size 21027
+oid sha256:80bbaf9daf65d30703afd6e58a680db3dc8eccb0d2f6212ad3f17ab3dc8aa765
+size 24224
