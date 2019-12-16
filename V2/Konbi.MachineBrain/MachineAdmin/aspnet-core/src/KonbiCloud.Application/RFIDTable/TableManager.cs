bf1073bf99c5c1fd3f81247fb47a1328ab5901ec
Long Le Hoang <long@konbi.cloud>
2019-12-16 11:54:08 +0700

    hide discount on CustomerUI initially. Fixed issue that cannot revert custom price

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7d4106c028bb7eac46a792dea3af09a01802c50b87e3b3f5dded21cb6a35210
-size 76798
+oid sha256:e5f60c48ce42d036bebcb0109b325740ffe437944fee8851ce6cbec2a77117b7
+size 76844
