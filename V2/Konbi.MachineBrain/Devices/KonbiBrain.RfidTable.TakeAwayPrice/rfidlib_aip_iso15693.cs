871ad8166eb5da93602da9fa1c332fd6419b022f
TrungPQ <trungpq@konbinisg.com>
2019-12-16 10:11:48 +0700

    Convert TakeAwayPrice project to x86.

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/rfidlib_aip_iso15693.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/rfidlib_aip_iso15693.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e041bb318934fcd3b063e9ad59bc9993e5fc5fa10a292b523d65b66a3870402d
-size 30566
+oid sha256:a3c653b0e7c01df2c2428dad3622951e7259f4f14ac514b312ed44276929cb7d
+size 49281
