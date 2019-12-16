929cc602066465792a4713d40bf4ab957ed88cfd
Long Le Hoang <long@konbi.cloud>
2019-12-16 10:34:13 +0700

    hardcoded 5% discount on Dash QR

--- V2/Konbi.MachineBrain/CustomerUI/RFIDTableNew/js/abp.signalr-client.js
+++ V2/Konbi.MachineBrain/CustomerUI/RFIDTableNew/js/abp.signalr-client.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5891c5c46037708cf0ad3250491af3ec868424c3fdc0eec524ec8bb310eefea2
-size 17409
+oid sha256:27966708362a34acca7ed678ce1d9f6b3621c255d6c23dbf9494db88e4b9cc92
+size 17665
--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TransactionInfo.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TransactionInfo.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f09b30fc64aa3875dc05f11e85317c1bffe35da9ee3b6fd4156cea9ad0bf6049
-size 1656
+oid sha256:976b737f836abba86b6c67adfef005834265d8627276328a124bc96e7152c231
+size 1758
