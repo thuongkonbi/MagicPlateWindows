e0c37ba66de7a2dc3fbb2293f0790a2c684b8692
Long Le Hoang <long@konbi.cloud>
2019-12-16 16:50:39 +0700

    enhance alarmlight, add scan QR audio

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5f60c48ce42d036bebcb0109b325740ffe437944fee8851ce6cbec2a77117b7
-size 76844
+oid sha256:5427e796ae6cda330e7b783b7c28549433a7acdb0b55bf8de7d8e05f1456d4b3
+size 77809
