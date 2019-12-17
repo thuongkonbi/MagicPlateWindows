efc5c572d2ed24a993ab7fb8e804fccc01809efd
Long Le Hoang <long@konbi.cloud>
2019-12-17 16:39:28 +0700

    fix traffice light - solid green light after payment successful

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1a2fe50a8f66713fbb68d6b9ba14ebf3f3fa8527eacf86f3599dcbf1a0d3cc3
-size 77947
+oid sha256:4258743ad6bf21d79d7d0e4e56bd7e738acbc5c9e92ec29f52d4612a327e8ad4
+size 77945
