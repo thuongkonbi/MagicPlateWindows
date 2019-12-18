efbb5330c283c26e4a845a3c42ec03bbadb9721b
Long Le Hoang <long@konbi.cloud>
2019-12-18 15:55:00 +0700

    fix bug populate from hour in editing session wrongly

--- V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/machines/sessions/create-or-edit-session-modal.component.ts
+++ V2/Konbi.MachineBrain/MachineAdmin/angular/src/app/main/machines/sessions/create-or-edit-session-modal.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34aec644dd952941b02102920822aeae7209f8a8ddd184f03fa8918e7522bc26
-size 1911
+oid sha256:3c4147b47791d255bc7e18aca72a999c452c60f4ae2dcb7f1c49d462c8fa212d
+size 2112
