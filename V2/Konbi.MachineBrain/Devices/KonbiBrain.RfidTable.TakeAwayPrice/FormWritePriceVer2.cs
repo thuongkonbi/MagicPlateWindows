a46484cec01a1bd7bfe0ccf8a8152f67a9d4cd7b
Manh Sam <manh@konbinisg.com>
2019-12-19 13:42:04 +0800

    fixed CPU issue

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePriceVer2.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.RfidTable.TakeAwayPrice/FormWritePriceVer2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c337a63fbfe00c4865ff03fbaaafcd9520c1fafe1d05d979e8f8e9282616b6d
-size 52589
+oid sha256:aff4917054068696b8c6c75189b3206a515e2f6bb632edde5a0ba6363e062dce
+size 52276
