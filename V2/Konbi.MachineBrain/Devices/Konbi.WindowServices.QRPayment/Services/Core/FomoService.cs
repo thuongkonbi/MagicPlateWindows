f084b149b5f0a57d0c0da05080d973a14549e45c
Long Le <Long@konbi.cloud>
2019-12-25 02:48:25 +0700

    fix issue that conflict NSQ between FomoService of Application & WebApi instance

--- V2/Konbi.MachineBrain/Devices/Konbi.WindowServices.QRPayment/Services/Core/FomoService.cs
+++ V2/Konbi.MachineBrain/Devices/Konbi.WindowServices.QRPayment/Services/Core/FomoService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:189b4e6b6985b890e340cc16a0fa6ab7c4364839b3058ebc8f2839d726f148c5
-size 30851
+oid sha256:c9c0d8178cd6cd1713155a12e0103fbec4012b4f5e39adfea389501b5becf93f
+size 31141
