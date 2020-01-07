cd602554b3e73ae646cee9c29e0ad583d6ede5b9
Long Le Hoang <long@konbi.cloud>
2020-01-07 13:48:48 +0700

    adjustment for transaction export for NCS

--- V2/KonbiCloud/angular/src/app/main/transactions/transactions.component.ts
+++ V2/KonbiCloud/angular/src/app/main/transactions/transactions.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99a30f697e64601818c933db2ac2fe055ab977fd4bbee155552c81b1a684fbb8
-size 6734
+oid sha256:e1b069100ea8edabff478d2935476bdf98c501009f228075b2270c8c3c856d63
+size 7294
--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Transactions/TransactionAppService.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Transactions/TransactionAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b76055ad2e9301bb2db74297244498eaa3c95168040683e3516916b749e5976
-size 14892
+oid sha256:833efc2355fd8046ccacaa7f3b254cc7cc8a37f088d3b53dafda437ce76b71ec
+size 14949
