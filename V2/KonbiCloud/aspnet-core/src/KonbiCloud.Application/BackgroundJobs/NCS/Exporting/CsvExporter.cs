9048c662669944c23079cdd39026b2b23f8e6d7e
Long Le Hoang <long@konbi.cloud>
2020-08-31 16:57:03 +0700

    daily report - POSSH - ordering mode fixed to DI(Dine In)

--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/BackgroundJobs/NCS/Exporting/CsvExporter.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/BackgroundJobs/NCS/Exporting/CsvExporter.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4cc81347658081d163a5bdb956e725422359ce4d6430a31d647b69ba07104237
-size 7490
+oid sha256:2d0d1b5bae3162fcf7670f25f3484af1f7e3e24eb10719b000e42dfad14e9692
+size 7492
