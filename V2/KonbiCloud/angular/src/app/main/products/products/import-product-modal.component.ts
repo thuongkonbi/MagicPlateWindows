3c15190922daf77de8376ed280585ec93b939fda
Long Le Hoang <long@konbi.cloud>
2020-09-16 14:58:39 +0700

    add SKU to import function

--- V2/KonbiCloud/angular/src/app/main/products/products/import-product-modal.component.ts
+++ V2/KonbiCloud/angular/src/app/main/products/products/import-product-modal.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49c4113dbb401d156ab3628245341a30eff2b80124ec9714d0d8e5a7ebfb7d01
-size 11767
+oid sha256:4c53f9584838717ed9f90f75b41f6daddeae6de25dd673f3f46ef0a8fb70af44
+size 12807
--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Products/ProductAppService.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/Products/ProductAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51965da4e303e39a34c010fe9b68340d7b4c46bf6b67e512106257f51bbe922b
-size 32464
+oid sha256:71b59e583bd8f28bf15df98490330b768f85dcc9c4b0d5af503fb98f79a764f2
+size 33589
