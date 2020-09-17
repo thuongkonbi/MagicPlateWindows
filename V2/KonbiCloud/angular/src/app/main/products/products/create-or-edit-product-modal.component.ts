04e7811531e8abf33e14f7dedbef31c804e79eed
Long Le Hoang <long@konbi.cloud>
2020-09-17 10:59:31 +0700

    Validate SKU

--- V2/KonbiCloud/angular/src/app/main/products/products/create-or-edit-product-modal.component.ts
+++ V2/KonbiCloud/angular/src/app/main/products/products/create-or-edit-product-modal.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4554d60657ac3d28c9b82894e394bc761788eb1d2ac6452bc281af7636810c70
-size 8090
+oid sha256:6fa5e11c4cbce459692c5bc15ee3d97cd9465948a791f78176b3a3a33ffeec50
+size 8361
