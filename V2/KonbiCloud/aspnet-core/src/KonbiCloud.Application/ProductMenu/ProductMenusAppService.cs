1cc93f7bb43f7f949ada16e87602d35d585cc2e4
Long Le Hoang <long@konbi.cloud>
2020-09-18 13:53:24 +0700

    rework on miniprofier + optimize menu scheduler

--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/ProductMenu/ProductMenusAppService.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/ProductMenu/ProductMenusAppService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1e96fb700418ee029b6c4659f8366e9f42321434798ba48763d08ddcb82034a
-size 39160
+oid sha256:1ac2d5d247cf0ff0c58f8608526219d6f7a8552abf1c12cc6e17c565c757bd83
+size 39139
--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Web.Host/Startup/Startup.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Web.Host/Startup/Startup.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e91bb3b28bdffd60f9d08f1f7df52662c7dd7500fe1ff098ead6fdb377b97e7
-size 10377
+oid sha256:e9e1bf6d058fe7e83369ba7c3c25ca4f67ba1fda1aefc48fa73e7a3b8ca8839f
+size 11825
