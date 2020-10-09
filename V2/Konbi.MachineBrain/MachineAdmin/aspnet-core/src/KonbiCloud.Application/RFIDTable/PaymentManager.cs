9d0e129bbdaf5e306b0534ee1d71b3dd3e8e969f
Long Le Hoang <long@konbi.cloud>
2020-10-09 09:45:56 +0700

    fix sending wrong amount to payment terminal

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/PaymentManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/PaymentManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bfb321382df588fd0c7fdff4f53d4488c7ba29d8034b49345535d035b1b40e1c
+oid sha256:5f74096ec3065009c9c02b51f62fcb88a360b1396755d55642533cbc4e6d4f68
 size 16820
