5337170002f056372a91f5f72152cf7e31cdd08f
Long Le Hoang <long@konbi.cloud>
2020-10-12 13:14:08 +0700

    NCS daily reporting file with suffix of yyMMdd

--- V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/BackgroundJobs/NCS/SendDailyReportBackgroundJob.cs
+++ V2/KonbiCloud/aspnet-core/src/KonbiCloud.Application/BackgroundJobs/NCS/SendDailyReportBackgroundJob.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff36d4b4a357957d3560dc3caf668c23510cd5af555be09fc3dfdaadbf4c2601
-size 8814
+oid sha256:ddaa7233eb28f5ce9599edda263c1c5f4fcc2be6b1853c8f70091f21c221d726
+size 8902
