21e1096e14500a1205cbf8ff72a10f72ca8da085
Ebi San <nnthuong106@gmail.com>
2020-12-25 14:09:18 +0700

    Display all the PlateModel in the Menu Scheduler to attach to Product

--- V2/KonbiCloud/angular/src/app/main/plate/plate-menus/plate-menus.component.ts
+++ V2/KonbiCloud/angular/src/app/main/plate/plate-menus/plate-menus.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1aa0c8f9d3e019ed945d82e9b0454a3f097b1cd26ef4810522bcd05a732da097
-size 20652
+oid sha256:7e0b33275dfbc38551736b51d038c02b289c44b07753a1b3df276c9600bbed85
+size 20645
