e07140d99eb8f850908f735cfbdb612836815d21
Daniel Tang Yi Hao <danieltang3434@gmail.com>
2021-01-06 23:44:54 +0800

    Update Cotf Pad app

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowServices.CotfPad/Hardware/HardwareUtil.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowServices.CotfPad/Hardware/HardwareUtil.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f02cc87b38232ba105778babafee540b24af3dcb90aa37c9fa9236379f48bb1c
-size 25410
+oid sha256:7ac2d8a1900510ad8a7f8f8d0a7251eb0c3f14b50be72a93aebe44ed33ca2308
+size 25350
