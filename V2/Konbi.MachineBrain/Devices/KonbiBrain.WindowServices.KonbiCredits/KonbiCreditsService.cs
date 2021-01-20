61d5c625434dc1c76b9c155a64dbe13cf8e8929f
Daniel Tang Yi Hao <danieltang3434@gmail.com>
2021-01-20 09:54:56 +0800

    Include error detections in Konbi Credits Windows Service

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowServices.KonbiCredits/KonbiCreditsService.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowServices.KonbiCredits/KonbiCreditsService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:126f8b989125e6aa588e7f8ae11005bb8fbd5fd8c714d42683acdf11436e7384
-size 12335
+oid sha256:d85225072a252eaf76a3b78d9f1303adb124a28b2f6dbc5d42a2b2dcd75b74e1
+size 12539
