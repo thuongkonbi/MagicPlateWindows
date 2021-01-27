d757b172ca49fa16ea8ec8c866f55d6d01a29000
Daniel Tang Yi Hao <danieltang3434@gmail.com>
2021-01-27 15:02:48 +0800

    Konbi Credits Card Reader Listener app

--- V2/Konbi.MachineBrain/Devices/KonbiCreditsCardReaderListener/ViewModels/ShellViewModel.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiCreditsCardReaderListener/ViewModels/ShellViewModel.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f062402c3182d46ef417f89a08171f5cfb3507cc78783b790c57eca116364c1b
-size 13232
+oid sha256:d4732108aa0f1ab0cae62b3c5421451baf881ac69ceeff58ae52b697bc732a57
+size 14636
--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8dfd14ed1ee958dd441c4b4961501f96049f62260938ec6b6b1e57c94d736cd
-size 82471
+oid sha256:168108aa700afc48da56198bc600b6785d828a2c4c22bb2cf4b47276b5fa285b
+size 82473
