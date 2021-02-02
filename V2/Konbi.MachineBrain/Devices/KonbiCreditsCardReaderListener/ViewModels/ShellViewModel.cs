ea298434ea34c14e44dd17eb45b2563ef809a566
Daniel Tang Yi Hao <danieltang3434@gmail.com>
2021-02-02 12:39:48 +0800

    See description
    - Delete guide-video.mp4 from customer ui
    - update the konbi credits card reader listener app

--- V2/Konbi.MachineBrain/Devices/KonbiCreditsCardReaderListener/ViewModels/ShellViewModel.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiCreditsCardReaderListener/ViewModels/ShellViewModel.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68525a95337a4e7887839303c8c4d01f1ef9c6eb2fa767fdd8b5334ea692146d
-size 15955
+oid sha256:842cf5436b4a5dcad1542ccac26705cfdb99a4cb990ed735b8ef25407949c5a6
+size 16127
