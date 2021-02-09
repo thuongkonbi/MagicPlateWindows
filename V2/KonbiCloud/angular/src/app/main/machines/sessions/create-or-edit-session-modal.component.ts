b18a74fe82c6d4a24eb4f4e92bbe39689b1eafc6
Daniel Tang Yi Hao <danieltang3434@gmail.com>
2021-02-09 19:04:46 +0800

    Prevent session config page from accept starting time later than ending time on Server Admin UI, Asana Task Url: https://app.asana.com/0/1192638138735659/1199570183178154/f

--- V2/KonbiCloud/angular/src/app/main/machines/sessions/create-or-edit-session-modal.component.ts
+++ V2/KonbiCloud/angular/src/app/main/machines/sessions/create-or-edit-session-modal.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc705a27cb6763b14345ea4c03384f10bedc75acb6a1b373b3fd2f3afa6bd923
-size 2311
+oid sha256:4e406fe41246b140fba83b91d22894a504eed811e137b67176f369147a1b9ca6
+size 2827
