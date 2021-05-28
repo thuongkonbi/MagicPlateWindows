286c35afd774f9604a9c28235c121e7a40ceb596
Ebi San <nnthuong106@gmail.com>
2021-05-28 10:30:23 +0700

    update dll

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowService.FacialRecognition/Database.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowService.FacialRecognition/Database.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f5d89d4f9c8d6ce6bb16919b389dfdc70dd52f83ae5038d4b76a9d6464e4827
-size 4038
+oid sha256:d0581887bca97a32fa5e99b7a00c3615d982eed69b012be2f85ba4a82f35902f
+size 4109
--- V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowService.FacialRecognition/RealSenseID/readme.txt
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowService.FacialRecognition/RealSenseID/readme.txt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce03711ea223a4d879d5adddb37f37021e97ae0cf2214efd1d42a7f7f3ed4b9f
-size 277
+oid sha256:a99656b072cd8575e7edd7231c70f10e2dd7f1e96a33f650fab72965b9cd45ae
+size 350
