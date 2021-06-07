57ae48f5339c56e04c135d47786864742c7db083
Ebi San <nnthuong106@gmail.com>
2021-06-07 17:45:21 +0700

    pull faceprint from server

--- V2/Konbi.MachineBrain/Devices/Konbi.RealsenseID/RealsenseID.cs
+++ V2/Konbi.MachineBrain/Devices/Konbi.RealsenseID/RealsenseID.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8edbf3be275aa8ecf0a0e460fbf49532565f9a2f1439cfca2450903cbeae9c80
-size 17157
+oid sha256:56cec996cb4d1acbf2692c6744d9d9a7b6d10ef1b63432cf19383ccab3b588d3
+size 16187
---
+++ V2/Konbi.MachineBrain/Devices/Konbi.RealsenseID/Services/RealsenseService.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b96319c82072c8abb10372cc2c527400b1fc59ec27899f932d7746785ec15561
+size 2495
