b7a7f79fa69f5c4699bf7f15a037e7f0550f8153
Ebi San <nnthuong106@gmail.com>
2021-06-21 12:55:01 +0700

    done payment with loop facial detect

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowService.FacialRecognition/FacialRecognitionService.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowService.FacialRecognition/FacialRecognitionService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30a02fe138fae0227e2297a9702295e09b985839e3cb18c5956e99f67d9ff766
-size 13414
+oid sha256:756eb1fb43922b0e6d5baed265c04265664c7846cca37ba7aa68efbc852c6a0a
+size 13516
--- V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowService.FacialRecognition/RealSenseID.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowService.FacialRecognition/RealSenseID.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed9385df773745d27f1dab04d2a624f1d79c28beae7d9b84b11ee02b3e8ae4fd
-size 16101
+oid sha256:5925ae8d43ef391a0988d695abf2effbc3879f49d3e6e08c2558cfea37c7f164
+size 16205
