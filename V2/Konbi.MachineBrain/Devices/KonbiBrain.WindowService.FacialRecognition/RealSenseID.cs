536bd7c1a6d5b7c9a572a561e58b9755773de62e
Ebi San <nnthuong106@gmail.com>
2021-06-21 12:15:55 +0700

    update new facial

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowService.FacialRecognition/RealSenseID.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowService.FacialRecognition/RealSenseID.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec86ef2076d2a942e492020dca19d590d58bcb2327e392cd4cd612dc7a7e227e
-size 16248
+oid sha256:ed9385df773745d27f1dab04d2a624f1d79c28beae7d9b84b11ee02b3e8ae4fd
+size 16101
--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:453fb00188f86a15d2d9a5e12bc3ccbdcbeb01bd011069dd1b1d7e4a1cc51cdf
-size 84401
+oid sha256:acec464d0ea20737e5874870562471b73f1c9b1b9a6d91f1ebc35675ccee3e95
+size 85434
