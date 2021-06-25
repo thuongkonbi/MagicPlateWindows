bda51da7b6a37f2a6f5bb8b126e9dd5b93723877
Ebi San <nnthuong106@gmail.com>
2021-06-25 15:41:08 +0700

    update title for facial recognition on cashierUI

--- V2/Konbi.MachineBrain/KonbiPOS/src/app/app.component.ts
+++ V2/Konbi.MachineBrain/KonbiPOS/src/app/app.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bffc4a419d6719fef5391c8e3cdc702b4c0e0c5b0dbf8b8f6eae65e6ab0f8d9d
-size 16758
+oid sha256:0fa5f25e7500c9bcc9a652e5e8816cc2eeca2fedc64987231fbda9a00f1afafe
+size 16892
