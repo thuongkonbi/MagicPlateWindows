3d575d8f844f3ab3e7c0bce3b3518c7a987c37e3
Ebi San <nnthuong106@gmail.com>
2021-07-08 09:36:28 +0700

    fixed check all and uncheck when finish task

--- V2/Konbi.MachineBrain/Devices/Konbi.RealsenseID/MainWindow.xaml.cs
+++ V2/Konbi.MachineBrain/Devices/Konbi.RealsenseID/MainWindow.xaml.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fff6b46e04f1518c37034d1951a94859c86af302428ee86171b2dbbe2dc61b85
-size 49085
+oid sha256:2df0f5d7828d014740ddbe0bda5f8614cd778e1bb336ed096bb8a87e6ee21ed6
+size 49301
