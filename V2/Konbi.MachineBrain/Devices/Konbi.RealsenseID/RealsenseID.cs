ab3c0e7941356fcbf8dfcee5a210c074ae3d3a29
Ebi San <nnthuong106@gmail.com>
2021-07-08 16:39:35 +0700

    update new dll for facial service

--- V2/Konbi.MachineBrain/Devices/Konbi.RealsenseID/RealsenseID.cs
+++ V2/Konbi.MachineBrain/Devices/Konbi.RealsenseID/RealsenseID.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b3b843dea0133c80ea920cf0aaa70abe39f784f7e32f9dfb615f3e5b4f4bfaa
-size 18350
+oid sha256:bae47defa65ff718cd3358d547d453ae7362b0ce668c0461e292fc830131d1ea
+size 18337
--- V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowService.FacialRecognition/RealSenseID.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowService.FacialRecognition/RealSenseID.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80d1006092ad248f6af342104d33ffa871ba7b1853efcb64a57db9ae088bb7cb
-size 17455
+oid sha256:e8751998e18854df60964eed2ba059bcaae68fb00b0f986eecfbaa57e9b6a4fd
+size 17262
