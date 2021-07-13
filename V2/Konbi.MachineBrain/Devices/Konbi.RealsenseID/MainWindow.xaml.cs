875e2ff6263b65c8c0d088cbef9577324b09651c
Ebi San <nnthuong106@gmail.com>
2021-07-13 08:41:24 +0700

    done delete flow without reload data from local device

--- V2/Konbi.MachineBrain/Devices/Konbi.RealsenseID/MainWindow.xaml.cs
+++ V2/Konbi.MachineBrain/Devices/Konbi.RealsenseID/MainWindow.xaml.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5fc0277785a06db35a54bb05f5c9294150037d9f36a435962460e06beec3c3e
-size 57329
+oid sha256:bb30ec54eef245efb644b91068086925e7e2e65fa0f67f7849950b3836ace99b
+size 59174
--- V2/Konbi.MachineBrain/Devices/Konbi.RealsenseID/RealsenseID.cs
+++ V2/Konbi.MachineBrain/Devices/Konbi.RealsenseID/RealsenseID.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba61e4704bb2b2eec9d37eb810442d7ab560a5fb731895b6c31bb6cf5cf5c0d4
-size 18898
+oid sha256:464712a8ed80177d234aae1e9578e336ca33f958e0a6dd0503a179796a68c987
+size 19007
