cac423e146618ef0b484afe5426d95a3ce942b86
Ebi San <nnthuong106@gmail.com>
2021-07-13 18:24:56 +0700

    implement override faceprints that updated

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowService.FacialRecognition/FacialRecognitionService.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowService.FacialRecognition/FacialRecognitionService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3bf5482c2e1e2cbdd8f17f7902c238031b2df87238e3fe95bdf5257d7c499c43
-size 22171
+oid sha256:4a05ccabd5f63389bababca95fba09c588ad0eb0871de2a538b16cead0fd010f
+size 24459
--- V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowService.FacialRecognition/Services/MqttClient.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowService.FacialRecognition/Services/MqttClient.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e10418a3d22488a9407e1e98bb42013e22d0b792f77f7841893134c15afdf63f
-size 2803
+oid sha256:4fb472a149bb9380d490b92803deb41f34fedb0e5ffff03a4df6a4264e178e36
+size 3007
