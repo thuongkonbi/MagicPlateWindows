a968cbe5b298d0faaaac48d533ffb5154efb2d8d
Ebi San <nnthuong106@gmail.com>
2021-07-14 12:36:56 +0700

    change logic to delete Faceprints out of device that data get from Server has faceprints is less than 20 charecters

--- V2/Konbi.MachineBrain/Devices/Konbi.RealsenseID/MainWindow.xaml.cs
+++ V2/Konbi.MachineBrain/Devices/Konbi.RealsenseID/MainWindow.xaml.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:644fd128c4717cf184eb34b3aa31cae6d7a47fcda549c788a02999640635fbe8
-size 65144
+oid sha256:08a97df86ba12bfef58f606c2930d5729419bbab3bd90d64bb027ee246949657
+size 66161
