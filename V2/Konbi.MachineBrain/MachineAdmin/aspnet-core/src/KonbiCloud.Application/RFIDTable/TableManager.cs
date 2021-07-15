c1074edb91c5bf3f30fc5d7e0886cf2422235131
Ebi San <nnthuong106@gmail.com>
2021-07-15 17:44:14 +0700

    done fix all Message when payment by FacialPayment service

--- V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
+++ V2/Konbi.MachineBrain/MachineAdmin/aspnet-core/src/KonbiCloud.Application/RFIDTable/TableManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69321c943418d640d8ef8773e89888d5d7843ae27ad9a7bc1d61b74b8242ac44
-size 85516
+oid sha256:a7c52e58ccbda8ab1b68acf2eafcb0f48dfc95dcc77065629c2d68dfdbbddacb
+size 86802
