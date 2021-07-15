e926f71d7b013f477917d861bac3bf9de8c59f39
Ebi San <nnthuong106@gmail.com>
2021-07-15 17:57:43 +0700

    add log faild when call Enroll API to server

--- V2/Konbi.MachineBrain/Devices/Konbi.RealsenseID/MainWindow.xaml.cs
+++ V2/Konbi.MachineBrain/Devices/Konbi.RealsenseID/MainWindow.xaml.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6582f7e7f30f4a3e56ea165fe5bd198d20e1448527cb5a4bef116434661f1ccc
-size 66150
+oid sha256:b84e3cce716cd6969a48011d5a7932accd579303528636ed1727e578fa83694a
+size 66147
