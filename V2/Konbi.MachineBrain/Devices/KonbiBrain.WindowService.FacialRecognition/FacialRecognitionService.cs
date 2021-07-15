b71e71d8328f795f5570d65e0d5189b7bdd1505b
Ebi San <nnthuong106@gmail.com>
2021-07-15 11:42:01 +0700

    edit log

--- V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowService.FacialRecognition/FacialRecognitionService.cs
+++ V2/Konbi.MachineBrain/Devices/KonbiBrain.WindowService.FacialRecognition/FacialRecognitionService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a6f575c7c78f3beb0a40e51f8515263ffe28ec8a161d3789e10c600ac6f9d56
-size 32408
+oid sha256:c8653c2911c9f087c09139d49a7df6ada59eac0f69bd5055225e88c41dfd09f3
+size 32416
